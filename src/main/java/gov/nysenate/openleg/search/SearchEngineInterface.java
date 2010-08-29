c37a787f17d9befc847bbfafa1f0e5e12eff237f
Graylin Kim <graylin.kim@gmail.com>
2010-08-28 21:02:10 -0400

    Removed unused interface

--- src/main/java/gov/nysenate/openleg/search/SearchEngineInterface.java
+++
@@ -1,9 +0,0 @@
-package gov.nysenate.openleg.search;
-
-import java.io.IOException;
-import org.apache.lucene.queryParser.ParseException;
-
-public interface SearchEngineInterface {
-	public Object get(String format, String otype, String oid, String sortField, int start, int numberOfResults, boolean reverseSort);
-	public Object search(String searchText, String format,int start, int max, String sortField, boolean reverseSort) throws ParseException, IOException;
-}
