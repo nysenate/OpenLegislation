32cd377af62f9f9940f2da2ce5998285eaf8a32d
nathanfreitas <nathan@freitas.net>
2010-12-29 17:18:26 -0500

    - Senators servlet now loads JSON data in the init method and sends to /senators/index.jsp view

--- src/main/java/gov/nysenate/openleg/SenatorsServlet.java
+++ src/main/java/gov/nysenate/openleg/SenatorsServlet.java
@@ -1,10 +1,19 @@
 package gov.nysenate.openleg;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
 
 /**
  * Servlet implementation class SenatorsServlet
@@ -12,7 +21,15 @@ import javax.servlet.http.HttpServletResponse;
 public class SenatorsServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
+	private static final String DISTRICT_JSON_FOLDER_PATH = "WEB-INF/classes/data/districts/";
+   
+	private static final String VIEW_PATH = "/senators/index.jsp";
+	
+	private static Logger logger = Logger.getLogger(SenatorsServlet.class);	
+
+	private static ArrayList<JSONObject> districts = null;
+	/**
+     * 
      * @see HttpServlet#HttpServlet()
      */
     public SenatorsServlet() {
@@ -24,14 +41,109 @@ public class SenatorsServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		request.setAttribute("districts", districts);
+		getServletContext().getRequestDispatcher(VIEW_PATH).forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		doGet(request,response);
 	}
 
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+			
+		try
+		{
+		
+			districts = new ArrayList<JSONObject>();
+			
+			for (int i = 1; i <= 62; i++)
+			{
+				String jsonPath = DISTRICT_JSON_FOLDER_PATH + "sd" + i + ".json";
+				logger.info("loading: " + jsonPath);
+				
+				InputStream is = getServletContext().getResourceAsStream(jsonPath);
+			//	InputStream is = ClassLoader  
+				//.getSystemResourceAsStream(DISTRICT_JSON_FOLDER_PATH + "sd" + i + ".json");  
+				
+				BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+				
+				String line = null;
+				StringBuffer jsonb = new StringBuffer();
+				while ((line = reader.readLine())!=null)
+				{	
+					jsonb.append(line);
+					
+				}
+				
+				
+				JSONObject jsono = new JSONObject(jsonb.toString());
+
+				JSONObject jSenator = jsono.getJSONObject("senator");
+				
+				String senatorName = jSenator.getString("name");
+				String senatorKey = jSenator.getString("url");
+				senatorKey = senatorKey.substring(senatorKey.lastIndexOf('/')+1);
+				
+				senatorKey = senatorKey.replace("-jr", ""); 
+				senatorKey = senatorKey.replace("-sr", ""); 
+				String[] senatorKeyParts =senatorKey.split("-");
+
+				if (senatorName.contains("-"))
+				{
+					
+					senatorKey = senatorKeyParts[senatorKeyParts.length-2] + '-' + senatorKeyParts[senatorKeyParts.length-1];
+				}
+				else
+				{
+					senatorKey = senatorKeyParts[senatorKeyParts.length-1];
+				}
+				
+				jSenator.put("key", senatorKey);
+				
+				districts.add(jsono);
+				
+				
+				logger.info(jsono.get("district"));
+				logger.info(jsono.getJSONObject("senator").get("name"));
+				
+			}
+			
+			 Collections.sort(districts, new byLastName());
+		}
+		catch (Exception e)
+		{
+			logger.error("error loading json district files",e);
+		}
+	}
+	
+	
+	 class byLastName implements java.util.Comparator {
+		 public int compare(Object districtA, Object districtB) {
+			 int sdif = 0;
+			 
+			 try
+			 {
+				 JSONObject senatorA = ((JSONObject)districtA).getJSONObject("senator");
+				 JSONObject senatorB = ((JSONObject)districtB).getJSONObject("senator");
+				 
+				 sdif = senatorA.getString("key").compareTo(senatorB.getString("key"));
+				 
+			//	 logger.info("sort value: " + senatorA.getString("key") + " vs " +  senatorB.getString("key") + sdif);
+			 }
+			catch (Exception e)
+			{
+				logger.error("error sorting districts",e);
+			}
+			
+		  
+		  return sdif;
+		 }
+		} 
 }
--- src/main/webapp/senators/index.jsp
+++ src/main/webapp/senators/index.jsp
@@ -1,78 +1,61 @@
-<%@ page language="java" import="java.util.*, java.text.*,java.io.*,gov.nysenate.openleg.*,gov.nysenate.openleg.model.*" pageEncoding="UTF-8"%>
+<%@ page language="java" import="org.json.*,java.util.*, java.text.*,java.io.*,gov.nysenate.openleg.*,gov.nysenate.openleg.model.*" pageEncoding="UTF-8"%>
 <%
 
-String appPath = request.getContextPath();
-session.removeAttribute("term");
-
-String searchPath = appPath + "/sponsor";
-
-Bill bill = null;
-String last = null;
-DateFormat df = SimpleDateFormat.getDateInstance(SimpleDateFormat.MEDIUM);
-
-
-String legTypeFilter = request.getParameter("type");
-
-String stringEventSearch = request.getParameter("action");
-
-if (stringEventSearch == null)
-	stringEventSearch = "Passed";
-	
-Date startDate = null;
-Date endDate = null;
-
-if (request.getParameter("year") != null)
-{
-	int year = Integer.parseInt(request.getParameter("year"));
-	int month = Integer.parseInt(request.getParameter("month"));
-	int day = Integer.parseInt(request.getParameter("day"));
-	
-	Calendar now = Calendar.getInstance();
-	now.clear();
-	now.set(year,month-1,day,0,0,0);
-	startDate = now.getTime();
-	
-	now.set(year,month-1,day,11,59,59);
-	endDate = now.getTime();
-}
-
-long start = 0;
-long end = 25;
-
-String actionSearchKey = "actionSearchKeyFoo";
 
 %>
 
-<jsp:include page="../header.jsp">
+<jsp:include page="/header.jsp">
 	<jsp:param name="title" value="Senators"/>
 </jsp:include>
 <style>
-.views-row
+.senatorBlock
 {
+width:300px;
+height:100px;
 float:left;
-margin:6px;
-width:140px;
-height:130px;
-font-size:9pt;
-line-height:13px;
-}
-
-.contact, .social_buttons
-{
-display:none;
+font-size:14px;
+line-height:16px;
+margin:3px;
 }
 </style>
 
 	<h2>Senators (2011-2012 Session)</h2>
  <div id="content">
   
- <em>Currently being updated / You can view the <a href="2009">2009-2010 Session Senators</a></em>
-  
-</div>
-
-<br style="clear:both;"/>
-
  
+ <%
+ ArrayList<JSONObject> districts =  (ArrayList<JSONObject>)request.getAttribute("districts");
+ 
+ for (JSONObject jDistrict : districts)
+ {
+	 JSONObject jSenator = jDistrict.getJSONObject("senator");
+	 
+	 String senatorKey = jSenator.getString("key");
+	 
+	 
+	 String searchUrl = "/legislation/search/?term=" + java.net.URLEncoder.encode("sponsor:\"" + senatorKey + "\"", "UTF-8");
+	 %>
+	 <div class="senatorBlock">
+	 	
+	 	<img src="<%=jSenator.get("imageUrl")%>" style="float:left;margin-right:3px"/>
+	 	
+	 	<%=jSenator.get("name")%><br/>
+	 	<%=jDistrict.get("district")%><br/>
+	 	<a href="<%=searchUrl%>">View Legislation</a><br/>
+	 	<a href="<%=jSenator.get("url")%>">Visit Website</a><br/>
+	 </div>
+	 <%
+ }
+ 
+ %> 
+  <br style="clear:both;"/>
+  
+
+
+<hr/>
+ <em>You can also view the archived <a href="2009">2009-2010 Session Senators</a></em>
+
+ </div>
    
  <jsp:include page="/footer.jsp"/>
    
