6d22d9d31bd5ec33b0ef8c0c37d175f5b5e4d3db
Jared Williams <williams@nysenate.gov>
2011-06-17 16:19:46 -0400

    trimming APIServlet code to serve only default search views

--- src/main/java/gov/nysenate/openleg/APIServlet.java
+++ src/main/java/gov/nysenate/openleg/APIServlet.java
@@ -1,25 +1,15 @@
 package gov.nysenate.openleg;
 
 import gov.nysenate.openleg.api.ApiHelper;
-import gov.nysenate.openleg.api.ApiType;
-import gov.nysenate.openleg.lucene.ILuceneObject;
-import gov.nysenate.openleg.model.bill.Bill;
-import gov.nysenate.openleg.model.bill.BillEvent;
-import gov.nysenate.openleg.search.Result;
 import gov.nysenate.openleg.search.SearchEngine;
 import gov.nysenate.openleg.search.SenateResponse;
 import gov.nysenate.openleg.util.SessionYear;
 import gov.nysenate.openleg.util.TextFormatter;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Hashtable;
 import java.util.NoSuchElementException;
 import java.util.StringTokenizer;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -27,463 +17,152 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
 
-
 public class APIServlet extends HttpServlet implements OpenLegConstants {
 
-	private static long DATE_START = SessionYear.getSessionStart();
-	private static long DATE_END = SessionYear.getSessionEnd();
-	
 	private static final long serialVersionUID = -7567155903739799800L;
 
-	private static Logger logger = Logger.getLogger(APIServlet.class);	
+	private static Logger logger = Logger.getLogger(APIServlet.class);
 
 	private static final String SRV_DELIM = "/";
-	
-	@Override
-	public void init() throws ServletException {
-		super.init();
-	
-	}
 
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-		super.init(config);
-	}
-	
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
-		
-		if (req.getParameter("reset")!=null) {
+
+		if (req.getParameter("reset") != null) {
 			SearchEngine.getInstance().closeSearcher();
 			return;
 		}
-		
-		/* /legislation/<service> */
-		String service;
-		
-		/* /legislation/<service>/<version> */
-		String version = "";
-		
-		/* accepted formats:
-		 * 		bill:		html, json, mobile, xml		csv, html-print, lrs-print
-		 * 		calendar:	html, json, mobile, xml
-		 * 		meeting:	html, json, mobile, xml
-		 * 		search:		html, json, mobile, xml		csv, html-list, rss
-		 * 		transcript:	html, json, mobile, xml		csv
-		 */
+
+		/* bills, calendars, transcripts, meetings, sponsor, committee */
 		String format = null;
-		
-		/* accepted types: bill, calendar, meeting, transcript
-		 * 		as well as bills, calendars, meetings, transcripts, actions, votes
+
+		/*
+		 * accepted types: bill, calendar, meeting, transcript as well as bills,
+		 * calendars, meetings, transcripts, actions, votes
 		 */
 		String type = null;
-		
-		/* unique id */
-		String key = "";
-		
-		String uri = java.net.URLDecoder.decode(req.getRequestURI(),OpenLegConstants.ENCODING);
-		req.setAttribute(KEY_PATH,uri);
-			
-		logger.info(TextFormatter.append("request: ", uri, " (" + req.getRequestURI(),")"));
-		
-		
-		StringTokenizer st = new StringTokenizer (uri,SRV_DELIM);
-		st.nextToken(); //legislation
-		service = st.nextToken();
-		
-		if (st.hasMoreTokens())
-			version = st.nextToken(); //1.0
+
+		String uri = java.net.URLDecoder.decode(req.getRequestURI(),
+				OpenLegConstants.ENCODING);
+		req.setAttribute(KEY_PATH, uri);
+
+		logger.info(TextFormatter.append("request: ", uri,
+				" (" + req.getRequestURI(), ")"));
+
+		StringTokenizer st = new StringTokenizer(uri, SRV_DELIM);
+		st.nextToken(); // legislation
+		type = st.nextToken();
 
 		int pageIdx = DEFAULT_START_PAGE;
 		int pageSize = DEFAULT_PAGE_SIZE;
-				
-		try
-		{	
-			/*
-			 * /legislation/api/1.0/
-			 * 					<format>/
-			 * 					<type>/
-			 * 					<id>/
-			 * 	
-			 * ex. /legislation/api/1.0/html/bill/s1234-2011
-			 */
-			if (version.equals("1.0")) {				
-				format = st.nextToken().toLowerCase();	
-				type = st.nextToken().toLowerCase();
-				
-				if (st.hasMoreTokens())
-					key = st.nextToken();
-				
-				if (st.hasMoreTokens()) {
-					pageIdx = Integer.parseInt(st.nextToken());
-						
-					if (st.hasMoreTokens())
-						pageSize = Integer.parseInt(st.nextToken());
-							
-				}
-				else if (format.equals(FORMAT_XML)) //for now with XML
-					pageSize = DEFAULT_API_PAGE_SIZE;
-				
-				handleAPIv1(format, type, key, pageIdx, pageSize, req, resp);
+
+		try {
+			format = "html";
+
+			if (req.getSession().getAttribute("mobile") != null)
+				format = "mobile";
+
+			if (st.hasMoreTokens()) {
+				pageIdx = Integer.parseInt(st.nextToken());
 			}
-			/*
-			 * /legislation/api/
-			 * 				<format>/
-			 * 				<type>/
-			 * 				<id>/
-			 * 
-			 * ex. /legislation/api/html/bill/s1234-2011
-			 */
-			else if (service.equals("api")) {
-				format = version;
-				type = st.nextToken();
-				key = st.nextToken();
-								
-				if (st.hasMoreTokens()) {
-					pageIdx = Integer.parseInt(st.nextToken());
-						
-					if (st.hasMoreTokens())
-						pageSize = Integer.parseInt(st.nextToken());
-							
-				}
-				else if (format.equals(FORMAT_XML)) //for now with XML
-					pageSize = DEFAULT_API_PAGE_SIZE;
-					
-				handleAPIv1(format, type, key, pageIdx, pageSize, req, resp);
-			}
-			/*
-			 * /legislation/
-			 * 			<type>/
-			 * 			<id>/
-			 * 
-			 * ex. /legislation/bill/s1234-2011
-			 * 
-			 * /legislation/
-			 * 			<type>/
-			 * 			?<id>
-			 * 
-			 * ex. /legislation/bill/?s1234-2011
-			 */
-			else 
-			{
-				format = "html";
-				
-				if (req.getSession().getAttribute("mobile")!=null)
-					format = "mobile";
-				
-				type = service;
-				key = version;
-				
-				if(type != null) {
-					if(type.endsWith("s")) {
-						pageIdx = (key != null && !key.equals("")) ? new Integer(key) : pageIdx;
-						key = "";
-					}
-					else if(type.matches("(sponsor|committee)")) {
-						if(st.hasMoreTokens()) {
-							pageIdx = Integer.parseInt(st.nextToken());
-						}
-					}
-				}
-				
-				if (st.hasMoreTokens()) {
-						pageSize = Integer.parseInt(st.nextToken());
-				}
-				else if (format.equals(FORMAT_XML)) //for now with XML
-					pageSize = DEFAULT_API_PAGE_SIZE;
-				
-				handleAPIv1(format, type, key, pageIdx, pageSize, req, resp);
-			}
+			if (st.hasMoreTokens()) {
+				pageSize = Integer.parseInt(st.nextToken());
+			} else if (format.equals(FORMAT_XML)) // for now with XML
+				pageSize = DEFAULT_API_PAGE_SIZE;
+
+			handleAPIv1(format, type, pageIdx, pageSize, req, resp);
+		} catch (NumberFormatException nfe) {
+			logger.warn("Invalid API call", nfe);
+		} catch (NoSuchElementException nse) {
+			logger.warn("Invalid API call", nse);
+		} catch (NullPointerException npe) {
+			logger.warn("Invalid API call", npe);
+		} catch (Exception e) {
+			logger.warn("Invalid API call", e);
 		}
-		catch (NumberFormatException nfe) {
-			logger.warn ("Invalid API call", nfe);
-		}
-		catch (NoSuchElementException nse) {
-			logger.warn ("Invalid API call", nse);
-		}
-		catch (NullPointerException npe) {
-			logger.warn ("Invalid API call", npe);
-		}
-		catch (Exception e)	{
-			logger.warn ("Invalid API call", e);
-		}
-		
+
 	}
-	
-	public void handleAPIv1 (String format, String type, String key, int pageIdx, int pageSize, HttpServletRequest req, HttpServletResponse resp) 
-		throws IOException, ServletException {
-		
-		String urlPath = TextFormatter.append("/legislation/", type, "/", ((key != null && !key.matches("\\s*")) ? key + "/" :""));
-		
-		if (type.equalsIgnoreCase("sponsor")) {
-			String filter = req.getParameter("filter");
-			key = TextFormatter.append("sponsor:\"", key, "\"");
-			if(filter != null) {
-				req.setAttribute("filter", filter);
-				key = TextFormatter.append(key, filter != null ? " AND " + filter : "");
-			}
-			type = "bills";
-		}
-		else if (type.equalsIgnoreCase("committee")) {
-			key = TextFormatter.append("committee:\"", key, "\"");
-			type = "bills";
-		}
-		
-		String originalType = type;
+
+	public void handleAPIv1(String format, String type, int pageIdx,
+			int pageSize, HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
+
+		String urlPath = TextFormatter.append("/legislation/", type, "/");
+
 		String viewPath = "";
 		String searchString = "";
 		String sFormat = "json";
 		String sortField = "when";
 		boolean sortOrder = true;
-		
 
-		if(type != null) {
-			if(type.contains("bill")) {
-				sortField = "sortindex";
-				sortOrder = false;
-			}
-			else if(type.contains("meeting")) {
-				sortField = "sortindex";
-			}
+		if (type.contains("bill")) {
+			sortField = "sortindex";
+			sortOrder = false;
+		} else if (type.contains("meeting")) {
+			sortField = "sortindex";
 		}
-		
-		
+
 		SenateResponse sr = null;
-		
-		key = key.trim();
-		
+
 		if (pageSize > MAX_PAGE_SIZE)
-			throw new ServletException ("The maximum page size is " + MAX_PAGE_SIZE);
-		//now calculate start, end idx based on pageIdx and pageSize
+			throw new ServletException("The maximum page size is "
+					+ MAX_PAGE_SIZE);
+
+		// now calculate start, end idx based on pageIdx and pageSize
 		int start = (pageIdx - 1) * pageSize;
 		int end = start + pageSize;
-						
-		logger.info(TextFormatter.append("request: key=", key, ";type=", type, ";format=", format, ";paging=", start, "/", end));
-		try	{
+
+		logger.info(TextFormatter.append("request: type=", type, ";format=",
+				format, ";paging=", start, "/", end));
+		try {
+			type = type.substring(0, type.length() - 1);
+
+			searchString = TextFormatter.append("otype:", type);
+
 			/*
-			 * construct query with "otype:<type>" if type present
+			 * generate default views query, filter by session year, documents
+			 * outside of this range can be manually searched
 			 */
-			if (type != null) {
-				
-				/*
-				 * for searches
-				 * 
-				 * applicable to: bills, calendars, meetings, transcripts, actions, votes
-				 */
-				if (type.endsWith("s"))	{
-					type = type.substring(0,type.length()-1);
-					
-					searchString = TextFormatter.append("otype:", type);
-					
-					if (key != null && key.length() > 0)
-						searchString = TextFormatter.append(searchString," AND ", key);
-				}
-				/*
-				 * for individual documents
-				 * 
-				 * applicable to: bill, calendar, meeting, transcript
-				 */
-				else {
-					searchString = TextFormatter.append("otype:", type);
-	
-					if (key != null && key.length() > 0) {
-						if (type.equals("bill") && key.indexOf("-") == -1)
-							key += TextFormatter.append(key,"-", DEFAULT_SESSION_YEAR);
-	
-						key = key.replace(" ","+");
-						searchString = TextFormatter.append(searchString," AND oid:", key);
-					}
-				}
-			}
-			else
-			{
-				searchString = key;
-			}
-							
-			/* 
-			 * applicable to: bills view
-			 * 
-			 * only return bills with "year:<currentSessionYear>"
-			 */
-			if(originalType.equals("bills")) {
-				req.setAttribute("urlPath", urlPath);
-				searchString = TextFormatter.append(ApiHelper.dateReplace(searchString),
-					" AND year:", SessionYear.getSessionYear(),
-					" AND active:", LUCENE_ACTIVE);
-			}
-			/*
-			 * applicable to: calendars, meetings, and transcripts views
-			 * 
-			 * only want current session documents as defined by the time
-			 * frame between DATE_START and DATE_END
-			 */
-			else if(originalType.endsWith("s")) {
-				req.setAttribute("urlPath", urlPath);
-				searchString = TextFormatter.append(ApiHelper.dateReplace(searchString),
-					" AND when:[",DATE_START," TO ",DATE_END,"]"," AND active:", LUCENE_ACTIVE);
-			}
-			/*
-			 * applicable to: individual documents
-			 * 
-			 * attempting to access a document by id doesn't
-			 * doesn't require any special formatting
-			 */
-			else {
-				searchString = ApiHelper.dateReplace(searchString);
-			}
-						
-			req.setAttribute("sortField", sortField);
-			req.setAttribute("sortOrder", Boolean.toString(sortOrder));
-			req.setAttribute("type", type);
-			req.setAttribute("term", searchString);
-			req.setAttribute("format", format);
-			req.setAttribute("key", key);
-			req.setAttribute(PAGE_IDX,pageIdx+"");
-			req.setAttribute(PAGE_SIZE,pageSize+"");
+			searchString = TextFormatter.append(
+					ApiHelper.dateReplace(searchString), " AND (", "year:",
+					SessionYear.getSessionYear(), " OR when:[",
+					SessionYear.getSessionStart(), " TO ",
+					SessionYear.getSessionEnd(), "]", ")", " AND active:",
+					LUCENE_ACTIVE);
 			
-			sr = SearchEngine.getInstance().search(searchString,sFormat,start,pageSize,sortField,sortOrder);
-						
+			sr = SearchEngine.getInstance().search(searchString, sFormat,
+					start, pageSize, sortField, sortOrder);
+
 			logger.info(TextFormatter.append("got search results: ", sr.getResults().size()));
-			
-			if(key != null && !key.equals("") && sr.getResults().size() >= 1) {
-				if(sr.getResults().get(0).getOid().equalsIgnoreCase(key)) {
-					sr.setResults(new ArrayList<Result>(Arrays.asList(sr.getResults().get(0))));
-				}
-			}
-						
-			if (sr.getResults().size()==0) {
+
+			if (sr.getResults().size() == 0) {
 				resp.sendError(404);
 				return;
-			}
-			else if (sr.getResults().size()==1 && format.matches("(html(\\-print)?|mobile|lrs\\-print)"))	{
-				
-				Result result = sr.getResults().get(0);
-				
-				/* if not an exact match on the oid it's likely 
-				 * the correct id, just the wrong case */
-				if (!result.getOid().equals(key)) {
-					if(format.equals("html"))
-						resp.sendRedirect(TextFormatter.append("/legislation/", result.getOtype(), "/", result.getOid()));
-					else
-						resp.sendRedirect(TextFormatter.append("/legislation/api/1.0/",
-								format,"/",result.getOtype(),"/",result.getOid()));
-					return;
-				}
-				
-				String jsonData = result.getData();
-				req.setAttribute("active", result.isActive());
-				
-				jsonData = ApiHelper.unwrapJson(jsonData);
-				
-				/* Jackson ObjectMaper will deserialize our json in to an object,
-				 * we need to know what sort of an object that is
-				 */
-				ApiType apiType = ApiHelper.getApiType(type);
-				Class<? extends ILuceneObject> clazz = apiType.clazz();
-				
-				/* 
-				 * for bills we populate other relevant data, such as
-				 * 		votes, calendars, meetings, sameas bills, older versions, etc.
-				 */
-				if (type.equals("bill")) {
-					String billQueryId = key;
-					String sessionYear = DEFAULT_SESSION_YEAR;
-					
-					/* default behavior to maintain previous permalinks
-					 * is if key=S1234 to transform to S1234-2009
-					 * in line with our new bill oid format <billno>-<sessYear> */
-					String[] billParts = billQueryId.split("-");
-					billQueryId = billParts[0];
-					if (billParts.length > 1)
-						sessionYear = billParts[1];
-				
-					/* turns S1234A in to S1234 */
-					String billWildcard = billQueryId.replaceAll("[a-zA-Z]$", "");
-						
-					//get BillEvents for this 
-					//otype:action AND billno:((S1234-2011 OR [S1234A-2011 TO S1234Z-2011) AND S1234*-2011)
-					String rType = "action";
-					String rQuery = null;
-					rQuery = ApiHelper.buildBillWildCardQuery("billno", billWildcard, sessionYear);
-					
-					ArrayList<Result> relatedActions = ApiHelper.getRelatedSenateObjects(rType,rQuery);
-					Hashtable<String,Result> uniqResults = new Hashtable<String,Result>();
-					for (Result rResult: relatedActions) {
-						BillEvent rAction = (BillEvent)rResult.getObject();
-						uniqResults.put(rAction.getEventDate().getTime()+'-'+rResult.getTitle().toUpperCase(), rResult);
-					}
-					ArrayList<Result> list = Collections.list(uniqResults.elements());
-					req.setAttribute("related-" + rType, list);
-
-					//get sameas bills (e.g. for S1234A get S1234)
-					//otype:bill AND oid:((S1234-2011 OR [S1234A-2011 TO S1234Z-2011) AND S1234*-2011)
-					rType = "bill";
-					rQuery = ApiHelper.buildBillWildCardQuery("oid", billWildcard, sessionYear);
-					req.setAttribute("related-" + rType, ApiHelper.getRelatedSenateObjects(rType,rQuery));
-
-					//get Meetings
-					rType = "meeting";
-					rQuery = "bills:\"" + key + "\"";					
-					req.setAttribute("related-" + rType, ApiHelper.getRelatedSenateObjects(rType,rQuery));
-					
-					//get calendars
-					rType = "calendar";
-					rQuery = "bills:\"" + key + "\"";
-					req.setAttribute("related-" + rType, ApiHelper.getRelatedSenateObjects(rType,rQuery));
-					
-					//get votes
-					rType = "vote";
-					rQuery = "billno:\"" + key + "\"";
-					req.setAttribute("related-" + rType, ApiHelper.getRelatedSenateObjects(rType,rQuery));
-				}
-				
-				Object resultObj = null;
-				
-				try	{
-					resultObj = ApiHelper.getMapper().readValue(jsonData, clazz);
-				}
-				catch (Exception e) {
-					logger.warn("error binding className", e);
-				}
-				
-				req.setAttribute(type, resultObj);
-				viewPath = TextFormatter.append("/views/", type, "-", format, ".jsp");
-			}
+			} 
 			else {
-				/* all non html/print/mobile bill views go here */
-				if (type.equals("bill") && !format.equals("html")) {
-					viewPath = TextFormatter.append("/views/bills-", format, ".jsp");
-					
-					ArrayList<Result> searchResults = ApiHelper.buildSearchResultList(sr);
-					ArrayList<Bill> bills = new ArrayList<Bill>();
-					
-					for (Result result : searchResults) {
-						bills.add((Bill)result.getObject());
-					}
-										
-					req.setAttribute("bills", bills);
-				}
-				/* all calendar, meeting, transcript multi views go here */
-				else {
-					viewPath = TextFormatter.append("/views/", "search", "-", format, ".jsp");
-					
-					sr.setResults(ApiHelper.buildSearchResultList(sr));
-					
-					req.setAttribute("results", sr);
-				}
+				viewPath = TextFormatter.append("/views/", "search", "-", format, ".jsp");
+
+				sr.setResults(ApiHelper.buildSearchResultList(sr));
+				
+				req.setAttribute("sortField", sortField);
+				req.setAttribute("sortOrder", Boolean.toString(sortOrder));
+				req.setAttribute("type", type);
+				req.setAttribute("term", searchString);
+				req.setAttribute("format", format);
+				req.setAttribute(PAGE_IDX, pageIdx + "");
+				req.setAttribute(PAGE_SIZE, pageSize + "");
+				req.setAttribute("urlPath", urlPath);
+				req.setAttribute("results", sr);
 			}
-		}
-		catch (Exception e) {
-			logger.warn("search controller didn't work for: " + req.getRequestURI(),e);
-			e.printStackTrace();
+		} catch (Exception e) {
+			logger.warn("search controller didn't work for: " + req.getRequestURI(), e);
 		}
 
 		try {
 			logger.info("routing to search controller:" + viewPath);
 			getServletContext().getRequestDispatcher(viewPath).forward(req, resp);
-		}
-		catch (Exception e)	{
-			logger.warn("search controller didn't work for: " + req.getRequestURI(),e);
+		} catch (Exception e) {
+			logger.warn("search controller didn't work for: " + req.getRequestURI(), e);
 		}
 	}
 }
