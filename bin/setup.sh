6d5d8e785a413886837f7bc985bc954e579a24f8
Graylin Kim <graylin.kim@gmail.com>
2012-03-13 13:22:26 -0400

    Remove unsupported non-maven dep management plugin.

---
+++ bin/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+root_dir=`dirname $0`/../
+lib_dir=$root_dir/src/main/resources/lib
+
+function mvn_install {
+    mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar -DgeneratePom=true
+}
+
+mvn_install gov.nysenate    SenateServices         0.0.1-SNAPSHOT     $lib_dir/SenateServices-0.0.1-SNAPSHOT.jar
