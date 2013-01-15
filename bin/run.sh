4f67e9f0e1f06c60e8fb87989cf67bf2400bc563
Graylin Kim <graylin.kim@gmail.com>
2013-01-15 15:08:43 -0500

    Update run.sh to allow running of anything in the scripts package.

--- bin/run.sh
+++ bin/run.sh
@@ -2,14 +2,12 @@
 source $(dirname "$0")/utils.sh
 
 BASE="$ROOTDIR/target/legislation-$VERSION/WEB-INF"
+SCRIPT=$1; shift
 
-case $1 in
-    ingest) SCRIPT=gov.nysenate.openleg.scripts.Ingest; shift;;
-    collate) SCRIPT=gov.nysenate.openleg.scripts.Collate; shift;;
-    push) SCRIPT=gov.nysenate.openleg.scripts.Push; shift;;
-    --help | -h | help) echo "USAGE: `basename $0` ingest|help <args>"; exit;;
-esac
+if [ ! $SCRIPT ]; then
+    echo "Script name is a required argument.";
+fi
 
 # TODO: This memory size should be an adjustable parameter
-java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/* $SCRIPT $@
+java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/* gov.nysenate.openleg.scripts.$SCRIPT $@
 
