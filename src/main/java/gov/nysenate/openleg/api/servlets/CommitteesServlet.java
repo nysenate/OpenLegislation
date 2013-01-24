ca7d23db9ae2f601705c478d794b638e386bec52
Graylin Kim <graylin.kim@gmail.com>
2013-01-24 18:55:29 -0500

    Redo the Committees servlet in the style of the SenatorsServlet.

--- src/main/java/gov/nysenate/openleg/api/servlets/CommitteesServlet.java
+++ src/main/java/gov/nysenate/openleg/api/servlets/CommitteesServlet.java
@@ -1,66 +1,77 @@
 package gov.nysenate.openleg.api.servlets;
 
-import gov.nysenate.openleg.api.ApiHelper;
-import gov.nysenate.openleg.util.CommitteeWriter;
+import gov.nysenate.openleg.util.RequestUtils;
+import gov.nysenate.openleg.util.RequestUtils.FORMAT;
+import gov.nysenate.openleg.util.SessionYear;
 import gov.nysenate.services.model.Committee;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.JsonGenerationException;
-import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 
 public class CommitteesServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    private static final String COMMITTEE_JSON_FOLDER_PATH = "WEB-INF/classes/data/committees/committees.json";
-
-    private static final String VIEW_PATH = "/committees/index.jsp";
-
     private static Logger logger = Logger.getLogger(CommitteesServlet.class);
 
-    ArrayList<Committee> committees;
+    private static Pattern pathPattern = Pattern.compile("/([0-9]{4}).*");
 
     @Override
-    protected void doGet(HttpServletRequest request,
-            HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String format = request.getParameter("format");
-        String uri = request.getRequestURI();
-
-        if (uri.indexOf(".") != -1)
-            format = uri.substring(uri.indexOf(".") + 1);
-
-        if (format != null) {
-            if (format.equals("json")) {
-                displayJSON(request, response);
+        int sessionYear = SessionYear.getSessionYear();
+        String pathInfo = request.getPathInfo();
+        if (pathInfo != null) {
+            Matcher pathMatcher = pathPattern.matcher(pathInfo);
+            if (pathMatcher.find()) {
+                sessionYear = SessionYear.getSessionYear(pathMatcher.group(1));
             }
-        } else {
-            request.setAttribute("committees", committees);
-            getServletContext().getRequestDispatcher(VIEW_PATH).forward(
-                    request, response);
         }
-    }
 
-    private void displayJSON(HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType("text/plain");
-        response.setCharacterEncoding("ISO-8859-1");
+        ObjectMapper mapper = new ObjectMapper();
+        File committeesBase = new File(SenatorsServlet.class.getClassLoader().getResource("data/committees/").getPath());
+        File committeesDir = new File(committeesBase, String.valueOf(sessionYear));
+        if (!committeesDir.exists()) committeesDir.mkdirs();
 
-        try {
-            response.getWriter().write(ApiHelper.getMapper().writeValueAsString(committees));
-        } catch (JsonGenerationException e) {
-            logger.error(e);
-        } catch (JsonMappingException e) {
-            logger.error(e);
-        } catch (IOException e) {
-            logger.error(e);
+        ArrayList<Committee> committees = new ArrayList<Committee>();
+        for (File committeeFile : FileUtils.listFiles(committeesDir, null, false)) {
+            committees.add(mapper.readValue(committeeFile, Committee.class));
+        }
+
+        Collections.sort(committees, new Comparator<Committee>() {
+            public int compare(Committee a, Committee b) {
+                return a.getName().compareToIgnoreCase(b.getName());
+            }
+        });
+
+        FORMAT format = RequestUtils.getFormat(request, FORMAT.HTML);
+
+        switch (format) {
+        case JSON:
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            mapper.writeValue(response.getOutputStream(), committees);
+            break;
+        case XML:
+            // TODO: write out the xml;
+            break;
+        case HTML:
+            request.setAttribute("committees", committees);
+            request.setAttribute("sessionStart", sessionYear);
+            request.setAttribute("sessionEnd", sessionYear+1);
+            request.getRequestDispatcher("/views/committees.jsp").forward(request, response);
+            break;
         }
     }
 
@@ -73,10 +84,5 @@ public class CommitteesServlet extends HttpServlet {
     @Override
     public void init() throws ServletException {
         super.init();
-
-        CommitteeWriter cw = new CommitteeWriter();
-        File file = new File(getServletContext().getRealPath(COMMITTEE_JSON_FOLDER_PATH));
-        committees = cw.getCommitteesFromJson(file, ApiHelper.getMapper());
     }
-
 }
