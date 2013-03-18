16d53ea36d6d35c3ad003ffc165bbb049b8cf9f3
Kevin Caseiras <kcaseiras@gmail.com>
2013-03-18 11:44:26 -0400

    Improvements to vote testing.

--- src/test/java/gov/nysenate/openleg/tests/BillS16182011.java
+++ src/test/java/gov/nysenate/openleg/tests/BillS16182011.java
@@ -1,10 +1,15 @@
 package gov.nysenate.openleg.tests;
 
-import org.junit.Test;
+import java.util.Arrays;
+
+import gov.nysenate.openleg.model.Vote;
+
+import org.junit.*;
+
 
 public class BillS16182011 extends TestSetup
 {
-	private static final String billKey = "2011/bill/S1618-2011";
+	private static final String billKey = "2011/bill/S1618-2011"; // Directory and name of expected json file within testing environment.
 	private static final String initialSenateSobi = "SOBI.D110110.T154119.TXT";
 	private static final String senateSponsor = "Sampson";
 	private static final String meetingKey2011 = "2011/meeting/meeting-Codes-7-2011-2011";
@@ -18,8 +23,7 @@ public class BillS16182011 extends TestSetup
 	private static final long senateVoteDate2011 = 1301544000000L; // March 31, 2011.
 	private static final long senateVoteDate2012 = 1327899600000L; // Jan 20, 2012.
 	private static final long committeeVoteDate2011 = 1299598200000L; // March 8, 2011.
-	private static final long committeeVoteDate2012 = 1326906000000L; // Jan 18, 2012.
-
+	private static final long committeeVoteDate2012 = 1326906000000L; // Jan 18, 2012.	
 	// Json max of 14 characters for name -- SOBI gives 15 characters
 	private static final String[] senateAyeVotes2011 = { "Adams", "Addabbo", "Alesi", "Avella", "Ball", "Bonacic",
 		"Breslin", "Carlucci", "DeFrancisco", "Diaz", "Dilan", "Duane", "Farley", "Flanagan", "Fuschillo", "Gallivan", 
@@ -29,8 +33,6 @@ public class BillS16182011 extends TestSetup
 		"Rivera", "Robach", "Saland", "Sampson", "Savino", "Serrano", "Seward", "Skelos", "Smith", "Squadron", "Stavisky", 
 		"Stewart-Cousin", "Valesky", "Young", "Zeldin"};
 	private static final String[] senateAbsVotes2011 = { "Espaillat", "Huntley" };
-	private static final String[] committeeAyeVotes2011 = {"Saland", "DeFrancisco", "Flanagan", "Fuschillo", "Gallivan", 
-		"Golden", "Lanza", "Nozzolio", "O'Mara", "Gianaris", "Duane", "Huntley", "Parker", "Perkins", "Squadron", "Espaillat"};
 	private static final String[] senateAyeVotes2012 = { "Adams", "Addabbo", "Alesi", "Avella", "Ball", "Bonacic", 
 		"Breslin", "Carlucci", "DeFrancisco", "Diaz", "Dilan", "Duane", "Espaillat", "Farley", "Flanagan", "Fuschillo", 
 		"Gallivan", "Gianaris", "Golden", "Griffo", "Grisanti", "Hannon", "Hassell-Thomps", "Johnson", "Kennedy", 
@@ -39,146 +41,58 @@ public class BillS16182011 extends TestSetup
 		"Robach", "Saland", "Sampson", "Savino", "Serrano", "Seward", "Skelos", "Smith", "Squadron", "Stavisky", "Stewart-Cousin", 
 		"Valesky", "Young", "Zeldin"};
 	private static final String[] senateExcusedVotes2012 = {"Huntley"};
+	private static final String[] committeeAyeVotes2011 = {"Saland", "DeFrancisco", "Flanagan", "Fuschillo", "Gallivan", 
+		"Golden", "Lanza", "Nozzolio", "O'Mara", "Gianaris", "Duane", "Huntley", "Parker", "Perkins", "Squadron", "Espaillat"};
 	private static final String[] committeeAyeVotes2012 = {"Saland", "DeFrancisco", "Flanagan", "Fuschillo", "Gallivan", "Golden",
 		"Lanza", "Nozzolio", "O'Mara", "Gianaris", "Huntley", "Parker", "Perkins", "Squadron", "Espaillat"};	
 	private static final String[] committeeAyeWRVotes2012 = {"Duane"};
 
-	
-	// TODO test SOBI.D110331.T112352.TXT:2011S01618 --> 100000Pen L. sex. abuse 2nd degree     00000
 
-	
+	// TODO test SOBI.D110331.T112352.TXT:2011S01618 --> 100000Pen L. sex. abuse 2nd degree     00000	
+
+
 	/*
-	 * ------- Senate vote tests -------
+	 * ------- Senate Vote Tests -------
 	 */
-	@Test
-	public void testSenateVoteDate2012()
-	{
-		VoteTests.isVoteDateCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2012, senateVoteDate2012);
-	}
-	
 	@Test
 	public void testSenateVoteDate2011()
 	{
 		VoteTests.isVoteDateCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2011, senateVoteDate2011);
 	}
-	
+
 	@Test
-	public void testSenateAyeVotes2011()
+	public void testSenateVoteDate2012()
 	{
-		VoteTests.areSenateAyeVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2011, senateAyeVotes2011);
+		VoteTests.isVoteDateCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2012, senateVoteDate2012);
 	}
 
 	@Test
-	public void testSenateAbsVotes2011()
+	public void testSenateVote2011()
 	{
-		VoteTests.areSenateAbsVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2011, senateAbsVotes2011);
-	}
-	
-	@Test
-	public void testSenateAyeWRVotes2011()
-	{
-		VoteTests.areSenateAyeWRVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2011);
-	}
-	
-	@Test
-	public void testSenateAbstainVotes2011()
-	{
-		VoteTests.areSenateAbstainVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2011);
-	}
-	
-	@Test
-	public void testSenateExcusedVotes2011()
-	{
-		VoteTests.areSenateExcusedVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2011);
-	}
-	
-	@Test
-	public void testSenateNayVotes2011()
-	{
-		VoteTests.areSenateNayVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2011);
-	}
-	
-	@Test
-	public void testSenateAyeVotes2012()
-	{
-		VoteTests.areSenateAyeVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2012, senateAyeVotes2012);
+		Vote expected = new Vote();
+		expected.setAyes(Arrays.asList(senateAyeVotes2011));
+		expected.setAbsent(Arrays.asList(senateAbsVotes2011));
+		VoteTests.areSenateVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2011, expected);
 	}
 
 	@Test
-	public void testSenateExcusedVotes2012()
+	public void testSenateVote2012()
 	{
-		VoteTests.areSenateExcusedVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2012, senateExcusedVotes2012);
-	}
-	
-	@Test
-	public void testSenateAyeWRVotes2012()
-	{
-		VoteTests.areSenateAyeWRVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2012);
-	}
-	
-	@Test
-	public void testSenateAbsVotes2012()
-	{
-		VoteTests.areSenateAbsVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2012);
-	}
-	
-	@Test
-	public void testSenateAbstainVotes2012()
-	{
-		VoteTests.areSenateAbstainVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2012);
-	}
-	
-	@Test
-	public void testSenateNayVotes2012()
-	{
-		VoteTests.areSenateNayVotesNull(env, sobiDirectory, storage, billKey, senateVoteSobi2012);
+		Vote expected = new Vote();
+		expected.setAyes(Arrays.asList(senateAyeVotes2012));
+		expected.setExcused(Arrays.asList(senateExcusedVotes2012));
+		VoteTests.areSenateVotesCorrect(env, sobiDirectory, storage, billKey, senateVoteSobi2012, expected);
 	}
 
 	/*
-	 * ------- Committee vote tests -------
+	 * ------- Committee Vote Tests -------
 	 */
 	@Test
 	public void testCommitteeVoteDate2011()
 	{
 		VoteTests.isVoteDateCorrect(env, sobiDirectory, storage, billKey, committeeVoteSobi2011, committeeVoteDate2011);
 	}
-	
-	@Test
-	public void testCommitteeAyeVotes2011()
-	{
-		VoteTests.areCommitteeAyeVotesCorrect(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011, committeeAyeVotes2011);
-	}
-	
-	@Test
-	public void testCommitteeAyeWRVotes2011()
-	{
-		VoteTests.areCommitteeAyeWRVotesNull(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011);
-	}
-	
-	@Test
-	public void testCommitteeAbsVotes2011()
-	{
-		VoteTests.areCommitteeAbsVotesNull(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011);
-	}
-	
-	@Test
-	public void testCommitteeAbstainVotes2011()
-	{
-		VoteTests.areCommitteeAbstainVotesNull(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011);
-	}
-	
-	@Test
-	public void testCommitteeAExcusedVotes2011()
-	{
-		VoteTests.areCommitteeExcusedVotesNull(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011);
-	}
-	
-	@Test
-	public void testCommitteeNayVotes2011()
-	{
-		VoteTests.areCommitteeNayVotesNull(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011);
-	}
-	
+
 	@Test
 	public void testCommitteeVoteDate2012()
 	{
@@ -186,17 +100,22 @@ public class BillS16182011 extends TestSetup
 	}
 
 	@Test
-	public void testCommitteeAyeVotes2012()
+	public void testCommitteeVote2011()
 	{
-		VoteTests.areCommitteeAyeVotesCorrect(env, sobiDirectory, storage, meetingKey2012, billName, committeeVoteSobi2012, committeeAyeVotes2012);
+		Vote expected = new Vote();
+		expected.setAyes(Arrays.asList(committeeAyeVotes2011));
+		VoteTests.areCommitteeVotesCorrect(env, sobiDirectory, storage, meetingKey2011, billName, committeeVoteSobi2011, expected);
 	}
 
 	@Test
-	public void testCommitteeAyeWRVotes2012()
+	public void testCommitteeVote2012()
 	{
-		VoteTests.areCommitteeAyeWRVotesCorrect(env, sobiDirectory, storage, meetingKey2012, billName, committeeVoteSobi2012, committeeAyeWRVotes2012);
+		Vote expected = new Vote();
+		expected.setAyes(Arrays.asList(committeeAyeVotes2012));
+		expected.setAyeswr(Arrays.asList(committeeAyeWRVotes2012));
+		VoteTests.areCommitteeVotesCorrect(env, sobiDirectory, storage, meetingKey2012, billName, committeeVoteSobi2012, expected);
 	}
-	
+
 	/*
 	 * ------- Other Tests -------
 	 */
@@ -205,19 +124,19 @@ public class BillS16182011 extends TestSetup
 	{
 		BillTests.isBillInitiallyNull(storage, billKey);
 	}
-	
+
 	@Test
 	public void testProcessingSenate()
 	{
 		BillTests.doesBillExistsAfterProcessing(env, sobiDirectory, storage, billKey, initialSenateSobi);
 	}
-	
+
 	@Test
 	public void testProcessingCommittee()
 	{
 		BillTests.doesBillExistsAfterProcessing(env, sobiDirectory, storage, billKey, committeeVoteSobi2012);
 	}
-	
+
 	@Test
 	public void testSenateSponsor()
 	{
--- src/test/java/gov/nysenate/openleg/tests/VoteTests.java
+++ src/test/java/gov/nysenate/openleg/tests/VoteTests.java
@@ -23,247 +23,39 @@ public class VoteTests {
 		Bill bill = TestHelper.getBill(storage, billKey);
 		Vote vote = bill.getVotes().get(0);
 		assertThat(vote.getVoteDate(), is(new Date(expectedDate)));
-		System.out.println(vote.getVoteDate().toString());
 	}
 
-	// TODO All Vote Tests can only test single votes, cannot handle multiple votes in a bill
-	// TODO Allow more than 1 sobi to be sent for vote information.
-	// 	 -what if a sobi contains more than 1 vote? ( can a sobi have more than 1 vote on the same bill?? )
-	
-	/*
-	 * ------- Senate Vote Tests -------
-	 */
-	public static void areSenateAyeVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedAyes)
+	public static void areSenateVotesCorrect(Environment env, File sobiDirectory,
+			Storage storage, String billKey, String voteSobi, Vote expected)
 	{
 		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
 		TestHelper.processFile(env, voteSobiFile);
 		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyes(), containsInAnyOrder(expectedAyes));
-	}
-	
-	public static void areSenateAyeWRVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedAyeWR)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyeswr(), containsInAnyOrder(expectedAyeWR));
+		Vote testVote = bill.getVotes().get(0);
+		// Look into ListAssert.assertEquals if we don't care about the order of the lists.
+		assertThat(testVote.getAyes(), is(expected.getAyes()));
+		assertThat(testVote.getAyeswr(), is(expected.getAyeswr()));
+		assertThat(testVote.getAbsent(), is(expected.getAbsent()));
+		assertThat(testVote.getAbstains(), is(expected.getAbstains()));
+		assertThat(testVote.getExcused(), is(expected.getExcused()));
+		assertThat(testVote.getNays(), is(expected.getNays()));
 	}
 
-	public static void areSenateAbsVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedAbs)
+	public static void areCommitteeVotesCorrect(Environment env, File sobiDirectory, Storage storage,
+			String meetingKey, String billName, String committeeVoteSobi, Vote expected)
 	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
+		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
 		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbsent(), containsInAnyOrder(expectedAbs));
+		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
+		List<Bill> bills = meeting.getBills();
+		Bill bill = TestHelper.getBillByName(bills, billName);
+		Vote testVote = bill.getVotes().get(0);
+		assertThat(testVote.getAyes(), is(expected.getAyes()));
+		assertThat(testVote.getAyeswr(), is(expected.getAyeswr()));
+		assertThat(testVote.getAbsent(), is(expected.getAbsent()));
+		assertThat(testVote.getAbstains(), is(expected.getAbstains()));
+		assertThat(testVote.getExcused(), is(expected.getExcused()));
+		assertThat(testVote.getNays(), is(expected.getNays()));
 	}
 
-	public static void areSenateExcusedVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedExcused)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getExcused(), containsInAnyOrder(expectedExcused));
-	}
-
-	public static void areSenateNayVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedNay)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getNays(), containsInAnyOrder(expectedNay));
-	}
-
-	public static void areSenateAbstainVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi, String[] expectedAbstain)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbstains(), containsInAnyOrder(expectedAbstain));
-	}
-	
-	public static void areSenateAyeVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyes(), is(empty()));
-	}
-	
-	public static void areSenateAyeWRVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyeswr(), is(empty()));
-	}
-	
-	public static void areSenateAbsVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbsent(), is(empty()));
-	}
-	
-	public static void areSenateExcusedVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getExcused(), is(empty()));
-	}
-	
-	public static void areSenateNayVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getNays(), is(empty()));
-	}
-	
-	public static void areSenateAbstainVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String billKey, String voteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, voteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Bill bill = TestHelper.getBill(storage, billKey);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbstains(), is(empty()));
-	}
-
-	
-	/*
-	 * ------- Committee Vote Tests -------
-	 */
-	public static void areCommitteeAyeVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi, String[] expectedAyeVotes)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyes(), containsInAnyOrder(expectedAyeVotes));
-	}
-
-	public static void areCommitteeAyeWRVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi, String[] expectedAyeWRVotes)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyeswr(), containsInAnyOrder(expectedAyeWRVotes));
-	}
-
-	public static void areCommitteeNayVotesCorrect(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi, String[] expectedNayVotes)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getNays(), containsInAnyOrder(expectedNayVotes));
-	}
-	
-	// TODO finish the rest of committee vote tests
-	
-	public static void areCommitteeAyeVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyes(), is(empty()));
-	}
-	
-	public static void areCommitteeAyeWRVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAyeswr(), is(empty()));
-	}
-	
-	public static void areCommitteeAbsVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbsent(), is(empty()));
-	}
-	
-	public static void areCommitteeExcusedVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getExcused(), is(empty()));
-	}
-	
-	public static void areCommitteeNayVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getNays(), is(empty()));
-	}
-	
-	public static void areCommitteeAbstainVotesNull(Environment env, File sobiDirectory,
-			Storage storage, String meetingKey, String billName, String committeeVoteSobi)
-	{
-		File[] voteSobiFile = TestHelper.getFilesByName(sobiDirectory, committeeVoteSobi);
-		TestHelper.processFile(env, voteSobiFile);
-		Meeting meeting = TestHelper.getMeeting(storage, meetingKey);
-		List<Bill> bills = meeting.getBills();
-		Bill bill = TestHelper.getBillByName(bills, billName);
-		Vote vote = bill.getVotes().get(0);
-		assertThat(vote.getAbstains(), is(empty()));
-	}
 }
