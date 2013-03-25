bcba72512dd73c16e5d5b2ed9cce10a2cbca1d6b
Kevin Caseiras <kcaseiras@gmail.com>
2013-03-25 10:00:48 -0400

    Improvements to the scope of variables
    Also added tests for bill actions and bill law

--- src/test/java/gov/nysenate/openleg/tests/BillA83542011.java
+++ src/test/java/gov/nysenate/openleg/tests/BillA83542011.java
@@ -1,50 +1,44 @@
 package gov.nysenate.openleg.tests;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+
 import gov.nysenate.openleg.model.Vote;
 import org.junit.Test;
 
 public class BillA83542011 extends TestSetup
 {
 	public static final String billKey = "2011/bill/A8354-2011";
-	public static final String billSponsor = "O'Donnell";
 	public static final String billTitle = "Enacts the Marriage Equality Act relating to ability of individuals to marry";
-	public static final String voteDate = "6/24/11";
-
 	public static final String initialSobi = "SOBI.D110614.T162232.TXT";
 	public static final String billTextSobi = "SOBI.D110614.T162734.TXT";
 	public static final String voteSobi = "SOBI.D110624.T230028.TXT";
+	public static final String actionsSobi = "SOBI.D110625.T001531.TXT";
 
-	public static final String[] billCoSponsors = { "Gottfried", "Glick", "Titone", "Kellner", "Bronson", "Rivera J", "Silver", "Farrell",
-		"Sayward", "Lentol", "Nolan", "Weisenberg", "Arroyo", "Brennan", "Dinowitz", "Hoyt", "Lifton", "Millman", "Cahill", "Paulin",
-		"Reilly", "Bing", "Jeffries", "Jaffee", "Rosenthal", "Kavanagh", "DenDekker", "Schimel", "Hevesi", "Benedetto", "Schroeder",
-		"Miller J", "Lavine", "Lancman", "Linares", "Moya", "Roberts", "Simotas", "Abinanti", "Braunstein" };
-	public static final String[] billMultiSponsors = { "Aubry", "Boyland", "Brook-Krasny", "Canestrari", "Cook", "Duprey", "Englebright",
-		"Latimer", "Lopez V", "Lupardo", "Magnarelli", "McEneny", "Morelle", "Ortiz", "Pretlow", "Ramos", "Rivera N", "Rivera P",
-		"Rodriguez", "Russell", "Sweeney", "Thiele", "Titus", "Weprin", "Wright", "Zebrowski" };
-	public static final String[] ayeVotes = { "Adams", "Addabbo", "Alesi", "Avella", "Breslin", "Carlucci", "Dilan", "Duane", "Espaillat",
-		"Gianaris",	"Grisanti", "Hassell-Thomps", "Huntley", "Kennedy", "Klein", "Krueger", "Kruger", "McDonald", "Montgomery", "Oppenheimer",
-		"Parker","Peralta", "Perkins", "Rivera", "Saland", "Sampson", "Savino", "Serrano", "Smith", "Squadron", "Stavisky", "Stewart-Cousin",
-	"Valesky" };
-	public static final String[] nayVotes = {"Ball", "Bonacic", "DeFrancisco", "Diaz", "Farley", "Flanagan", "Fuschillo", "Gallivan",
-		"Golden", "Griffo", "Hannon", "Johnson", "Lanza", "Larkin", "LaValle", "Libous", "Little", "Marcellino", "Martins", "Maziarz",
-		"Nozzolio", "O'Mara", "Ranzenhofer", "Ritchie", "Robach", "Seward", "Skelos", "Young", "Zeldin" };
 
 	@Test
 	public void isSponsorCorrect()
 	{
+		String billSponsor = "O'Donnell";
 		BillTests.isSponserNameCorrect(env, sobiDirectory, storage, billKey, initialSobi, billSponsor);
 	}
 
 	@Test
 	public void testCoSponsors()
 	{
+		String[] billCoSponsors = { "Gottfried", "Glick", "Titone", "Kellner", "Bronson", "Rivera J", "Silver", "Farrell",
+				"Sayward", "Lentol", "Nolan", "Weisenberg", "Arroyo", "Brennan", "Dinowitz", "Hoyt", "Lifton", "Millman", "Cahill", "Paulin",
+				"Reilly", "Bing", "Jeffries", "Jaffee", "Rosenthal", "Kavanagh", "DenDekker", "Schimel", "Hevesi", "Benedetto", "Schroeder",
+				"Miller J", "Lavine", "Lancman", "Linares", "Moya", "Roberts", "Simotas", "Abinanti", "Braunstein" };
 		BillTests.areCoSponsorsCorrect(env, sobiDirectory, storage, billKey, initialSobi, billCoSponsors);
 	}
 
 	@Test
 	public void testMultiSponsors()
 	{
+		String[] billMultiSponsors = { "Aubry", "Boyland", "Brook-Krasny", "Canestrari", "Cook", "Duprey", "Englebright",
+				"Latimer", "Lopez V", "Lupardo", "Magnarelli", "McEneny", "Morelle", "Ortiz", "Pretlow", "Ramos", "Rivera N", "Rivera P",
+				"Rodriguez", "Russell", "Sweeney", "Thiele", "Titus", "Weprin", "Wright", "Zebrowski" };
 		BillTests.areMultiSponsorsCorrect(env, sobiDirectory, storage, billKey, initialSobi, billMultiSponsors);
 	}
 
@@ -63,6 +57,13 @@ public class BillA83542011 extends TestSetup
 	@Test
 	public void testVotes()
 	{
+		String[] ayeVotes = { "Adams", "Addabbo", "Alesi", "Avella", "Breslin", "Carlucci", "Dilan", "Duane", "Espaillat", "Gianaris",
+				"Grisanti", "Hassell-Thomps", "Huntley", "Kennedy", "Klein", "Krueger", "Kruger", "McDonald", "Montgomery", "Oppenheimer",
+				"Parker","Peralta", "Perkins", "Rivera", "Saland", "Sampson", "Savino", "Serrano", "Smith", "Squadron", "Stavisky",
+				"Stewart-Cousin", "Valesky" };
+		String[] nayVotes = {"Ball", "Bonacic", "DeFrancisco", "Diaz", "Farley", "Flanagan", "Fuschillo", "Gallivan", "Golden",
+				"Griffo", "Hannon", "Johnson", "Lanza", "Larkin", "LaValle", "Libous", "Little", "Marcellino", "Martins", "Maziarz",
+				"Nozzolio", "O'Mara", "Ranzenhofer", "Ritchie", "Robach", "Seward", "Skelos", "Young", "Zeldin" };
 		Vote expectedVote = new Vote();
 		// Add any non-null vote types to vote object.
 		expectedVote.setAyes(Arrays.asList(ayeVotes));
@@ -73,7 +74,38 @@ public class BillA83542011 extends TestSetup
 	@Test
 	public void testVoteDate()
 	{
+		String voteDate = "6/24/11";
 		VoteTests.isVoteDateCorrect(env, sobiDirectory, storage, billKey, voteSobi, voteDate);
 	}	
 
+	@Test
+	public void testLawSection()
+	{
+		String expectedLawSection = "Domestic Relations Law";
+		BillTests.testLawSection(env, sobiDirectory, storage, billKey, initialSobi, expectedLawSection);
+	}
+
+	@Test
+	public void testActions()
+	{
+		// Action 1 id = A8354-2011-1308110400000-reported+referred+to+rules
+		String billNumber = "A8354-2011";
+		ArrayList<String[]> actionStrings = new ArrayList<String[]>();
+		actionStrings.add(new String[]{"06/14/11", "referred to judiciary"});
+		actionStrings.add(new String[]{"06/15/11", "reported referred to rules"});
+		actionStrings.add(new String[]{"06/15/11", "reported"});
+		actionStrings.add(new String[]{"06/15/11", "rules report cal.320"});
+		actionStrings.add(new String[]{"06/15/11", "ordered to third reading rules cal.320"});
+		actionStrings.add(new String[]{"06/15/11", "message of necessity - 3 day message"});
+		actionStrings.add(new String[]{"06/15/11", "passed assembly"});
+		actionStrings.add(new String[]{"06/15/11", "delivered to senate"});
+		actionStrings.add(new String[]{"06/24/11", "ORDERED TO THIRD READING CAL.1545"});
+		actionStrings.add(new String[]{"06/24/11", "MESSAGE OF NECESSITY"});
+		actionStrings.add(new String[]{"06/24/11", "PASSED SENATE"});
+		actionStrings.add(new String[]{"06/24/11", "RETURNED TO ASSEMBLY"});
+		actionStrings.add(new String[]{"06/24/11", "delivered to governor"});
+		actionStrings.add(new String[]{"06/24/11", "signed chap.95"});
+
+		BillTests.testBillStatusActions(env, sobiDirectory, storage, billKey, actionsSobi, TestHelper.convertIntoActions(actionStrings, billNumber));
+	}
 }
