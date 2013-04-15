6d33b7e95cf6452e641823408d526d176dbd7d05
shweta22 <shweta.sri2202@gmail.com>
2013-04-15 09:59:17 -0400

    Created tests for SpotCheck.java

--- src/test/java/gov/nysenate/openleg/spotcheck/SpotCheckTest.java
+++ src/test/java/gov/nysenate/openleg/spotcheck/SpotCheckTest.java
@@ -1,6 +1,6 @@
 package gov.nysenate.openleg.spotcheck;
 
-import static org.junit.Assert.*;
+
 import gov.nysenate.openleg.model.Bill;
 import gov.nysenate.openleg.scripts.SpotCheck;
 import gov.nysenate.openleg.scripts.SpotCheck.SpotCheckBill;
@@ -8,23 +8,22 @@ import gov.nysenate.openleg.util.Storage;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
 
-import org.junit.Before;
+import java.util.HashMap;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
 public class SpotCheckTest extends SpotCheckBill
 {
     Bill bill;
-    HashMap<String, SpotCheckBill> bills = new HashMap<String, SpotCheckBill>();
+    static HashMap<String, SpotCheckBill> bills = new HashMap<String, SpotCheckBill>();
+    protected static Storage storage;
+    
    
-    @ Test
-    public void checkFiles() throws IOException{
-        Storage storage = new Storage("/home/shweta/test/processed/lbdc_test/json");
+    @ BeforeClass
+    public static void checkFiles() throws IOException{
+      storage  = new Storage("/home/shweta/test/processed/lbdc_test/json");
        String args[]= { "/home/shweta/test/processed/lbdc_test/files/20130323.assembly.low.html",
                "/home/shweta/test/processed/lbdc_test/files/20130323.assembly.high.html",
                "/home/shweta/test/processed/lbdc_test/files/20130323.senate.low.html",
@@ -33,30 +32,75 @@ public class SpotCheckTest extends SpotCheckBill
         for (String arg : args) {
             bills.putAll(SpotCheck.readDaybreak(new File(arg)));
         }
-        System.out.println(bills.size());
+        System.out.println("Total number of bills: "+bills.size());
+     }
+    
+    
+   @Test
+    public  void testBillTitles(){
         for(String id : bills.keySet()) {
-        String billNo = id+"-2013";
-         bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
-         testBillTitles(bill, bills,id);
-         
-       }
+            String billNo = id+"-2013";
+            bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
+            SpotCheckHelper.testBillTitles(bill, bills,id);
+            }
+       
+          
+    }
+   @Test
+    public void checkSummary()
+    {
+       for(String id : bills.keySet()) {
+           String billNo = id+"-2013";
+           bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
+           SpotCheckHelper.checkSummary(bill, bills,id);
+           }
+     }
+    
+    
+    @Test
+    public void checkSponsors(){
+            for(String id : bills.keySet()) {
+                String billNo = id+"-2013";
+                bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
+                SpotCheckHelper.checkSponsors(bill, bills, id);
+                }   
+    }
+      
+    
+    @Test
+    public void checkCoSponsors(){
+            for(String id : bills.keySet()) {
+                String billNo = id+"-2013";
+                bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
+                SpotCheckHelper.checkCoSponsors(bill, bills, id);
+                }   
     }
     
-   
-    public  void testBillTitles(Bill bill,HashMap<String, SpotCheckBill> bills,String id){
-     // Compare the titles, ignore white space differences
-        String jsonTitle = SpotCheck.unescapeHTML(bill.getTitle());
-        String lbdcTitle = bills.get(id).getTitle();
-         boolean check=SpotCheck.stringEquals(jsonTitle, lbdcTitle, true, true) ;
-         if(check== false)
-         {
-            System.out.println(bill.getSenateBillNo()+" :Title mismatch") ;  
-         }
-            assertEquals(check,true);
-       }
+    @Test
+    public void checkEvents(){
+            for(String id : bills.keySet()) {
+                String billNo = id+"-2013";
+                bill = (Bill)storage.get("2013/bill/"+billNo, Bill.class);
+                SpotCheckHelper.checkEvents(bill, bills, id);
+                }   
+    }
+    
+    @AfterClass
+    public static void errorCount()
+    {
+        int count[]=SpotCheckHelper.getErrorCount();
+        System.out.println("Actual Summary mismatches:Ignoring P/S Encoding :" +count[1]);
+        System.out.println("Summary mismatches:Not ignoring P/S Encoding :" +count[0]);
+        System.out.println("Total Title mismatches: " +count[3]);
+        System.out.println("Total Sponsor Error Count: " +count[2]);
+        System.out.println("Total CoSponsor Error Count :" +count[4]);
+        System.out.println("Total Event Error Count  :" +count[5]);
+        
+     }     
+  }
         
         
     
     
     
-} 
+ 
