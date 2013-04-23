007824bc472d0cf8ef714ff0b2806a6a027640b5
Graylin Kim <graylin.kim@gmail.com>
2013-04-23 17:12:27 -0400

    Make reset also reset files in the work directory.

--- bin/reset_env.sh
+++ bin/reset_env.sh
@@ -11,6 +11,10 @@ if [ `find $env/processed -type f -name SOBI* | wc -l` -ne 0 ]; then
 	mv `find $env/processed -type f -name SOBI*` data/;
 fi
 
+if [ `find $env/work -type f -name SOBI* | wc -l` -ne 0 ]; then
+	mv `find $env/work -type f -name SOBI*` data/;
+fi
+
 rm -fr data/*.xml
 rm -fr processed/*
 rm -fr json/*
