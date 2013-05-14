5076612decd0763acd4cecfab7767a14945625e5
Graylin Kim <graylin.kim@gmail.com>
2013-05-14 15:28:23 -0400

    Integrate nysenate-utils into Kevin's code.

--- src/main/java/gov/nysenate/openleg/api/servlets/UpdateServlet.java
+++ src/main/java/gov/nysenate/openleg/api/servlets/UpdateServlet.java
@@ -1,20 +1,19 @@
 package gov.nysenate.openleg.api.servlets;
 
 import gov.nysenate.openleg.model.Update;
+import gov.nysenate.openleg.util.Application;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,25 +23,28 @@ import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.ResultSetHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 import org.apache.commons.lang3.time.DateUtils;
-import org.apache.tomcat.dbcp.dbcp.BasicDataSource;
 
 /**
  * Servlet implementation class UpdateServlet
  */
 public class UpdateServlet extends HttpServlet {
 
-    private SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd");
+    private final SimpleDateFormat dateFormat = new java.text.SimpleDateFormat("yyyy-MM-dd");
 
     public UpdateServlet() {
         super();
     }
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         List<Update> updates = null;
-        String startDay = (String)request.getParameter("startDay");     // Earliest day we want to see results for.
-        String endDay = (String)request.getParameter("endDay");         // Latest day to see results for, default is today.
-        String otype = (String)request.getParameter("otype");
+        String startDay = request.getParameter("startDay");     // Earliest day we want to see results for.
+        String endDay = request.getParameter("endDay");         // Latest day to see results for, default is today.
+        String otype = request.getParameter("otype");
         if (request.getParameter("bill") != null) {
             // Show all changes for this bill
             String billId = request.getParameter("bill");
@@ -64,6 +66,8 @@ public class UpdateServlet extends HttpServlet {
             request.setAttribute("endDay", endDay);
             request.setAttribute("startDay", startDay);
             try {
+                startDay += " 00:00:00";
+                endDay += " 23:59:59";
                 if (otype != null && !otype.equalsIgnoreCase("all")) {
                     updates = processQueryByType(startDay, endDay, otype);
                 } else {
@@ -81,57 +85,28 @@ public class UpdateServlet extends HttpServlet {
         request.getRequestDispatcher("/updates.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        doGet(request, response);
-    }
-
     private List<Update> processQuery(String startDate, String endDate) throws SQLException
     {
-        BasicDataSource datasource = getDataSource();
-        // Add hh:mm:ss values to dates for the query.
-        startDate += " 00:00:00";
-        endDate += " 23:59:59";
-        QueryRunner run = new QueryRunner(datasource);
+        QueryRunner run = new QueryRunner(Application.getDB().getDataSource());
         ResultSetHandler<List<Update>> handler = new BeanListHandler<Update>(Update.class);
-        List<Update> updates = run.query("SELECT * FROM OpenLegUpdateTracker.Updates WHERE date BETWEEN ? AND ? ORDER BY date", handler, startDate, endDate);
+        List<Update> updates = run.query("SELECT * FROM updates WHERE date BETWEEN ? AND ? ORDER BY date", handler, startDate, endDate);
         return updates;
     }
 
     private List<Update> processQueryByType(String startDate, String endDate, String otype) throws SQLException
     {
-        BasicDataSource datasource = getDataSource();
-        // Add hh:mm:ss values to dates for the query.
-        startDate += " 00:00:00";
-        endDate += " 23:59:59";
-        QueryRunner run = new QueryRunner(datasource);
+        QueryRunner run = new QueryRunner(Application.getDB().getDataSource());
         ResultSetHandler<List<Update>> handler = new BeanListHandler<Update>(Update.class);
-        List<Update> updates = run.query("SELECT * FROM OpenLegUpdateTracker.Updates WHERE otype = ? AND date BETWEEN ? AND ? ORDER BY date", handler, otype, startDate, endDate);
+        List<Update> updates = run.query("SELECT * FROM updates WHERE otype = ? AND date BETWEEN ? AND ? ORDER BY date", handler, otype, startDate, endDate);
         return updates;
     }
 
     private List<Update> getBillHistory(String billId) throws SQLException
     {
-        BasicDataSource datasource = getDataSource();
-        QueryRunner run = new QueryRunner(datasource);
+        QueryRunner run = new QueryRunner(Application.getDB().getDataSource());
         ResultSetHandler<List<Update>> handler = new BeanListHandler<Update>(Update.class);
-        List<Update> updates = run.query("SELECT * FROM OpenLegUpdateTracker.Updates WHERE oid = ?", handler, billId);
+        List<Update> updates = run.query("SELECT * FROM updates WHERE oid = ?", handler, billId);
         return updates;
-    }
-
-    private BasicDataSource getDataSource()
-    {
-        String server = "localhost";
-        String port = "3306";
-        String driver = "com.mysql.jdbc.Driver";
-        String userName = "openleg";
-        String password = "openleg";
-        String url = "jdbc:mysql://" + server +":" + port + "/";
-        BasicDataSource datasource = new BasicDataSource();
-        datasource.setDriverClassName(driver);
-        datasource.setUrl(url);
-        datasource.setUsername(userName);
-        datasource.setPassword(password);
-        return datasource;
     }
 
     /**
