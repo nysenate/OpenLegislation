cfec260cb3dcf4b275a02edfc59470491db01c4a
Graylin Kim <graylin.kim@gmail.com>
2013-05-14 16:24:33 -0400

    Fix nysenate-* distribution and installation.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,9 +4,9 @@ source $(dirname "$0")/utils.sh
 lib_dir=$ROOTDIR/lib
 
 function mvn_install {
-    mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar -DgeneratePom=true
+    mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar 
 }
 
-mvn_install gov.nysenate.services nysenate-java-client 1.0.1 $lib_dir/nysenate-java-client-1.0.1.jar
-mvn_install gov.nysenate nysenate-utils 1.0.0 $lib_dir/nysenate-utils-1.0.0.jar
+mvn install:install-file -Dfile=$lib_dir/nysenate-java-client-1.0.1.jar -DpomFile=$lib_dir/nysenate-java-client-1.0.1.pom
+mvn install:install-file -Dfile=$lib_dir/nysenate-utils-1.0.0.jar -DpomFile=$lib_dir/nysenate-utils-1.0.0.pom
 
