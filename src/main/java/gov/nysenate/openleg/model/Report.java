3d05ac20e33ad32eb88a975c327a09e170f63772
Graylin Kim <graylin.kim@gmail.com>
2013-05-15 15:54:22 -0400

    Reformat the report model.

--- src/main/java/gov/nysenate/openleg/model/Report.java
+++ src/main/java/gov/nysenate/openleg/model/Report.java
@@ -1,28 +1,29 @@
 package gov.nysenate.openleg.model;
 
-import java.sql.Timestamp;
 import java.util.Date;
+
 public class Report
 {
-public int getReportId()
+    private int reportId;
+    private Date date;
+
+    public int getReportId()
     {
         return reportId;
     }
+
     public void setReportId(int reportId)
     {
         this.reportId = reportId;
     }
+
     public Date getDate()
     {
         return date;
     }
+
     public void setDate(Date date)
     {
         this.date = date;
     }
-int reportId;
-Date date;
-
-    
-    
 }
