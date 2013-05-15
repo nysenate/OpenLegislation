4db39cc32ef1fc8c9ee7346518fa5d0cbb20ab92
Graylin Kim <graylin.kim@gmail.com>
2013-05-15 15:05:59 -0400

    Rename nysenate-utils to nysenate-java-utils.

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,10 +3,6 @@ source $(dirname "$0")/utils.sh
 
 lib_dir=$ROOTDIR/lib
 
-function mvn_install {
-    mvn install:install-file -DgroupId=$1 -DartifactId=$2 -Dversion=$3 -Dfile=$4 -Dpackaging=jar 
-}
-
 mvn install:install-file -Dfile=$lib_dir/nysenate-java-client-1.0.1.jar -DpomFile=$lib_dir/nysenate-java-client-1.0.1.pom
-mvn install:install-file -Dfile=$lib_dir/nysenate-utils-1.0.0.jar -DpomFile=$lib_dir/nysenate-utils-1.0.0.pom
+mvn install:install-file -Dfile=$lib_dir/nysenate-java-utils-1.0.0.jar -DpomFile=$lib_dir/nysenate-java-utils-1.0.0.pom
 
