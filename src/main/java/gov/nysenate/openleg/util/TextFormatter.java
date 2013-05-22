42c00d87acad7f0f00486dd2ee36621d6bd69717
Graylin Kim <graylin.kim@gmail.com>
2013-05-22 12:16:50 -0400

    Readd the accidentally reverted Avoid inserting page breaks into bill headers.

--- src/main/java/gov/nysenate/openleg/util/TextFormatter.java
+++ src/main/java/gov/nysenate/openleg/util/TextFormatter.java
@@ -38,8 +38,9 @@ public class TextFormatter {
         int end = -1;
 
         ArrayList<TextPoint> points;
-
+        int linenum = 0;
         while(st.hasMoreTokens()) {
+            linenum++;
             String line = st.nextToken();
 
             Pattern pagePattern = Pattern.compile("(^\\s+\\w\\.\\s\\d+(--\\w)?\\s+\\d+(\\s+\\w\\.\\s\\d+(--\\w)?)?$|^\\s+\\d+\\s+\\d+\\-\\d+\\-\\d$|^\\s+\\d{1,4}$)");
@@ -134,7 +135,8 @@ public class TextFormatter {
                 capCount = 0;
             }
             else {
-                if(pageMatcher.find()) {
+                // We need to wait till we hit the 10th line to avoid breaking on the bill header
+                if(pageMatcher.find() && linenum > 10) {
                     out.append("<div style=\"page-break-after:always\"></div>"+line + "\n");
                 }
                 else {
