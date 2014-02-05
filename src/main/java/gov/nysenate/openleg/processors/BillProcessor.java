54dd043134ebe283694ff8313742bcdc164560a4
Graylin Kim <graylin.kim@gmail.com>
2014-02-04 21:56:13 -0500

    Add override for J3307.

--- src/main/java/gov/nysenate/openleg/processors/BillProcessor.java
+++ src/main/java/gov/nysenate/openleg/processors/BillProcessor.java
@@ -367,6 +367,9 @@ public class BillProcessor
         else if (bill.getBillId().equals("J2885-2013")) {
             bill.setOtherSponsors(Arrays.asList(new Person("KLEIN"), new Person("STEWART-COUSINS")));
         }
+        else if (bill.getBillId().equals("J3307-2013")) {
+            bill.setOtherSponsors(Arrays.asList(new Person("KLEIN"), new Person("SKELOS")));
+        }
 
         // An old bug with the assembly sponsors field needs to be corrected, NYSS 7215
         if (bill.getSponsor() != null && bill.getSponsor().getFullname().startsWith("RULES ")) {
