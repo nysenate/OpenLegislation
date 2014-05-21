33d7572139b3219636cc9dd1117350047454fc68
Kevin Caseiras <kcaseiras@gmail.com>
2014-05-21 16:15:03 -0400

    Display Transcripts back to 1993 in PDF format.

--- src/main/java/gov/nysenate/openleg/converter/PDFConverter.java
+++ src/main/java/gov/nysenate/openleg/converter/PDFConverter.java
@@ -7,9 +7,9 @@ import gov.nysenate.openleg.model.Transcript;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import gov.nysenate.openleg.util.TranscriptLine;
 import org.apache.pdfbox.exceptions.COSVisitorException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
@@ -22,8 +22,13 @@ public class PDFConverter
     // Kirkland started on May 16, 2011
     protected static long KIRKLAND_START_TIME = 1305086400000L;
 
-    // Line feeds started on Feb 7, 2008
-    protected static long LINEFEED_START_TIME = 1202360400000L;
+    private static Float bot = 90f;
+    private static Float right = 575f;
+    private static Float top = 710f;
+    private static Float left = 105f;
+    private static Float fontSize = 12f;
+    private static Float fontWidth = 7f;
+
 
     public static void write(IBaseObject object, OutputStream out) throws IOException, COSVisitorException, ApiRequestException
     {
@@ -41,49 +46,41 @@ public class PDFConverter
         PDFont font = PDType1Font.COURIER;
 
         ArrayList<List<String>> pages;
-        if (transcript.getTimeStamp().getTime() > LINEFEED_START_TIME) {
-            pages = pagifyWithLineFeeds(transcript.getTranscriptText());
-        }
-        else {
-            pages = pagifyByString(transcript.getTranscriptText(), "(518) 371-8910");
-        }
-
-        Float bot = 90f;
-        Float right = 575f;
-        Float top = 710f;
-        Float left = 105f;
-        Float fontSize = 12f;
-        Float fontWidth = 7f;
+        pages = parsePages(transcript.getTranscriptText());
 
         for (List<String> pageLines : pages) {
             PDPage page = new PDPage(PDPage.PAGE_SIZE_LETTER);
             PDPageContentStream contentStream = new PDPageContentStream(doc, page);
-            contentStream.drawLine(left, top, left, bot);
-            contentStream.drawLine(left, top, right, top);
-            contentStream.drawLine(left, bot, right, bot);
-            contentStream.drawLine(right, top, right, bot);
+
+            drawBorder(contentStream);
+
             contentStream.beginText();
             contentStream.setFont(font, fontSize);
 
             int lineCount = 0;
-            for (String line : pageLines) {
-                line = line.trim();
-                String[] parts = line.split("\\s");
-                if (parts.length > 0 && parts[0].matches("[0-9]+")) {
+            TranscriptLine line;
+            for (String aLine : pageLines) {
+                line = new TranscriptLine(aLine);
+
+                if (!line.isEmpty()) {
                     if (lineCount == 0) {
-                        float offset = right-(parts[0].length()+1)*fontWidth;
-                        contentStream.moveTextPositionByAmount(offset, top+fontWidth);
-                        contentStream.drawString(parts[0]);
-                        contentStream.moveTextPositionByAmount(-offset, -fontSize*2);
+                        // If no transcript number on first page, skip it.
+                        if (!line.isTranscriptNumber()) {
+                            contentStream.moveTextPositionByAmount(0, top - fontWidth);
+                        }
+                        else if (line.isTranscriptNumber()) {
+                            drawTranscriptNumber(contentStream, line);
+                        }
+                    }
+                    else if (line.isTranscriptNumber()) {
+                        drawTranscriptNumber(contentStream, line);
                     }
                     else {
-                        float offset = left-(parts[0].length()+1)*fontWidth;
-                        contentStream.moveTextPositionByAmount(offset, -fontSize);
-                        contentStream.drawString(line);
-                        contentStream.moveTextPositionByAmount(-offset, -fontSize);
+                        drawLine(contentStream, line);
                     }
                     lineCount++;
                 }
+
             }
 
             String stenographer;
@@ -107,27 +104,77 @@ public class PDFConverter
         doc.close();
     }
 
-    public static ArrayList<List<String>> pagifyByString(String text, String separator)
-    {
+    private static void drawBorder(PDPageContentStream contentStream) throws IOException {
+        contentStream.drawLine(left, top, left, bot);
+        contentStream.drawLine(left, top, right, top);
+        contentStream.drawLine(left, bot, right, bot);
+        contentStream.drawLine(right, top, right, bot);
+    }
+
+    private static void drawTranscriptNumber(PDPageContentStream contentStream, TranscriptLine line) throws IOException {
+        float offset = right - (line.fullText().trim().length() + 1) * fontWidth;
+        contentStream.moveTextPositionByAmount(offset, top + fontWidth);
+        contentStream.drawString(line.fullText().trim());
+        contentStream.moveTextPositionByAmount(-offset, -fontSize * 2);
+    }
+
+    private static void drawLine(PDPageContentStream contentStream, TranscriptLine line) throws IOException {
+        int indent;
+        if (line.hasLineNumber()) {
+            indent = line.fullText().trim().split("\\s")[0].length() + 1;
+            draw(contentStream, indent, line.fullText().trim());
+        }
+        else {
+            // Shift lines to the left to reduce indent size
+            indent = 11;
+            draw(contentStream, indent, line.fullText());
+
+            // Fix spacing on first page. Spacing info is lost without line numbers marking blank lines.
+            if (line.textTrimmed().equals("NEW YORK STATE SENATE"))
+                contentStream.moveTextPositionByAmount(0, -(fontSize * 4));
+
+            if (line.textTrimmed().contains("STENOGRAPHIC RECORD"))
+                contentStream.moveTextPositionByAmount(0, -(fontSize * 5));
+
+            if (line.isTime())
+                contentStream.moveTextPositionByAmount(0, -(fontSize * 3));
+
+            if (line.isSession())
+                contentStream.moveTextPositionByAmount(0, -(fontSize * 6));
+        }
+    }
+
+    private static void draw(PDPageContentStream contentStream, int indent, String text) throws IOException {
+        float offset = left - indent * fontWidth;
+        contentStream.moveTextPositionByAmount(offset, -fontSize);
+        contentStream.drawString(text);
+        contentStream.moveTextPositionByAmount(-offset, -fontSize);
+    }
+
+    private static ArrayList<List<String>> parsePages(String transcriptText) {
         List<String> page = new ArrayList<String>();
         ArrayList<List<String>> pages = new ArrayList<List<String>>();
-        for (String line : text.split("\n")) {
-            page.add(line);
-            if (line.trim().equals(separator)) {
-                // The separator is always the last line on the old page
-                pages.add(page);
-                page = new ArrayList<String>();
+
+        String[] line = transcriptText.split("\n");
+        TranscriptLine nextLine;
+        for (int i = 0; i < line.length; i++) {
+            page.add(line[i]);
+
+            // Ignore the first transcript number.
+            if (i > 10) {
+                if (i + 1 < line.length) {
+                    nextLine = new TranscriptLine(line[i + 1]);
+
+                    if (nextLine.isTranscriptNumber()) {
+                        pages.add(page);
+                        page = new ArrayList<String>();
+                    }
+                }
             }
-        }
-        return pages;
-    }
-    public static ArrayList<List<String>> pagifyWithLineFeeds(String text)
-    {
-        ArrayList<List<String>> pages = new ArrayList<List<String>>();
-        for (String pageText : text.split((char)12+"")) {
-            String[] lines = pageText.split("\n");
-            if (lines.length > 0) {
-                pages.add(Arrays.asList(lines));
+
+            // add the last page.
+            if (i == line.length - 1) {
+                pages.add(page);
             }
         }
         return pages;
