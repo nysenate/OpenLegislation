fabd167dc9d5589a095200a8d418b6b1cb621faf
Ash Islam <ashislam858@gmail.com>
2014-07-15 12:27:29 -0400

    Added bill vote code enumeration to represent the different voting prefixes

---
+++ src/main/java/gov/nysenate/openleg/model/bill/BillVoteCode.java
@@ -0,0 +1,14 @@
+package gov.nysenate.openleg.model.bill;
+
+/**
+ * Represents the possible voting code prefixes.
+ */
+public enum BillVoteCode
+{
+    AYE,   // Voted 'Yes'
+    NAY,   // Voted 'No'
+    EXC,   // Excluded from Vote
+    ABS,   // Absent from Vote
+    ABD,   // Abstained from Vote
+    AYEWR  // Vote 'Yes, with reservations'
+}
