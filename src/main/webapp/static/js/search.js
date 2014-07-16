ee1ae1d28c5dd3aab9201d312d265937da5ad262
Ken Zalewski <zalewski.senate@gmail.com>
2014-07-16 16:32:25 -0400

    Changed absolute URLs in CSS and JS to be relative, so that virtual host proxying works properly.

--- src/main/webapp/static/js/search.js
+++ src/main/webapp/static/js/search.js
@@ -25,7 +25,7 @@ $(function () {
         else {
             $.ajax({
                 type: "GET",
-                url: "http://"+window.location.host+"/legislation/search/",
+                url: "search/",
                 data: dataString,
                 cache: false,
                 async: true,
