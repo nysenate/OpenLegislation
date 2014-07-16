7d6b6830f3e1d70dc9d59403a6d1482f6e67211b
Ken Zalewski <zalewski.senate@gmail.com>
2014-07-16 16:21:36 -0400

    Temporary kludge to make sure that scripts are using the Tomcat lib JARs.

--- bin/run.sh
+++ bin/run.sh
@@ -2,6 +2,7 @@
 source $(dirname "$0")/utils.sh
 
 BASE="$ROOTDIR/target/legislation-$VERSION/WEB-INF"
+TOMCAT_HOME=/usr/share/tomcat
 SCRIPT=$1; shift
 
 if [ ! $SCRIPT ]; then
@@ -9,5 +10,5 @@ if [ ! $SCRIPT ]; then
 fi
 
 # TODO: This memory size should be an adjustable parameter
-java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/* gov.nysenate.openleg.scripts.$SCRIPT $@
+java -Xmx1024m -Xms16m -cp $BASE/classes/:$BASE/lib/*:$TOMCAT_HOME/lib/* gov.nysenate.openleg.scripts.$SCRIPT $@
 
