6a0757e65412b4167ec8fc63d822ef403ed89bab
Ash Islam <ashislam858@gmail.com>
2014-08-06 15:25:01 -0400

    Add caching to agenda data service.

--- src/main/java/gov/nysenate/openleg/service/agenda/CachedAgendaDataService.java
+++ src/main/java/gov/nysenate/openleg/service/agenda/CachedAgendaDataService.java
@@ -6,29 +6,40 @@ import gov.nysenate.openleg.model.agenda.AgendaId;
 import gov.nysenate.openleg.model.agenda.AgendaNotFoundEx;
 import gov.nysenate.openleg.model.sobi.SobiFragment;
 import gov.nysenate.openleg.service.base.CachingService;
+import net.sf.ehcache.CacheManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class CachedAgendaDataService implements AgendaDataService, CachingService
 {
+    private static final String agendaCache = "agendaData";
+
+    @Autowired
+    private CacheManager cacheManager;
+
     @Autowired
     private AgendaDao agendaDao;
 
     @Override
+    @PostConstruct
     public void setupCaches() {
-
+        cacheManager.addCache(agendaCache);
     }
 
     @Override
-    public void evictCaches() {
-
-    }
+    @CacheEvict(value = agendaCache, allEntries = true)
+    public void evictCaches() {}
 
     /** {@inheritDoc} */
     @Override
+    @Cacheable(value = agendaCache, key = "#agendaId")
     public Agenda getAgenda(AgendaId agendaId) throws AgendaNotFoundEx {
         if (agendaId == null) {
             throw new IllegalArgumentException("AgendaId cannot be null.");
@@ -43,6 +54,7 @@ public class CachedAgendaDataService implements AgendaDataService, CachingServic
 
     /** {@inheritDoc} */
     @Override
+    @CacheEvict(value = agendaCache, key = "#agenda.getId()")
     public void saveAgenda(Agenda agenda, SobiFragment sobiFragment) {
         if (agenda == null) {
             throw new IllegalArgumentException("Agenda cannot be null when saving.");
@@ -52,7 +64,8 @@ public class CachedAgendaDataService implements AgendaDataService, CachingServic
 
     /** {@inheritDoc} */
     @Override
+    @CacheEvict(value = agendaCache, key = "#agendaId")
     public void deleteAgenda(AgendaId agendaId) {
-        //throw new NotImplementedException();
+        agendaDao.deleteAgenda(agendaId);
     }
 }
