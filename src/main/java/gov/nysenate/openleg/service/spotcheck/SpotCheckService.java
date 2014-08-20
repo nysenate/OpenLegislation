1869f657fcb79eb2ac288c0f3df6455c61a97849
Ash Islam <ashislam858@gmail.com>
2014-08-20 16:32:23 -0400

    Added some comments to the SpotCheck interface

--- src/main/java/gov/nysenate/openleg/service/spotcheck/SpotCheckService.java
+++ src/main/java/gov/nysenate/openleg/service/spotcheck/SpotCheckService.java
@@ -17,9 +17,12 @@ import java.time.LocalDateTime;
 public interface SpotCheckService<ContentKey, ContentType>
 {
     /**
+     * Perform a check on 'content' against the latest reference data available. If no
+     * reference data can be matched for the given content, the ReferenceDataNotFoundEx
+     * will be thrown. A SpotCheckObservation will be returned which will contain a
+     * listing of any mismatches.
      *
-     *
-     * @param content ContentType
+     * @param content ContentType - The content to check
      * @return SpotCheckObservation<ContentKey>
      * @throws ReferenceDataNotFoundEx
      */
@@ -27,13 +30,16 @@ public interface SpotCheckService<ContentKey, ContentType>
                                                   throws ReferenceDataNotFoundEx;
 
     /**
+     * Overload of {@link #check(Object)} with an option to specify the latest datetime for
+     * which reference data should be retrieved for. Since reference data can typically
+     * be stored as a snapshot, perform the check such that the reference data was active
+     * prior to the 'latestDateTime'
      *
-     *
-     * @param content ContentType
-     * @param latestDate LocalDateTime
+     * @param content ContentType - The content to check
+     * @param latestDateTime LocalDateTime - Reference data must be active before this datetime
      * @return SpotCheckObservation<ContentKey>
      * @throws ReferenceDataNotFoundEx
      */
-    public SpotCheckObservation<ContentKey> check(ContentType content, LocalDateTime latestDate)
+    public SpotCheckObservation<ContentKey> check(ContentType content, LocalDateTime latestDateTime)
                                                   throws ReferenceDataNotFoundEx;
 }
