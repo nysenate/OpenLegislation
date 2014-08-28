e1b4a773c3069daf2ec629a573a4b508e8ce8c0a
Ash Islam <ashislam858@gmail.com>
2014-08-28 13:02:28 -0400

    push up wip front end stuff

--- src/main/webapp/config.rb
+++ src/main/webapp/config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
-add_import_path "bower_components/foundation/scss"
+add_import_path "static/bower_components/foundation/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
-sass_dir = "scss"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "static/css"
+sass_dir = "WEB-INF/scss"
+images_dir = "static/img"
+javascripts_dir = "static/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
