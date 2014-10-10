749c203dc41b6e92750580dc50fac278a367e35f
Ash Islam <ashislam858@gmail.com>
2014-10-10 15:01:58 -0400

    WIP bill search

---
+++ src/test/java/gov/nysenate/openleg/service/bill/search/ElasticBillSearchServiceTests.java
@@ -0,0 +1,12 @@
+package gov.nysenate.openleg.service.bill.search;
+
+import gov.nysenate.openleg.BaseTests;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ElasticBillSearchServiceTests extends BaseTests
+{
+    private static final Logger logger = LoggerFactory.getLogger(ElasticBillSearchServiceTests.class);
+
+
+}
