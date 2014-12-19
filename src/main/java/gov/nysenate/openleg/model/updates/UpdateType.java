7028ed3dafff4afc4d6106fb9d8ae01463b1763f
Ash Islam <ashislam858@gmail.com>
2014-12-19 10:39:18 -0500

    An update type to specify what the updates are based off of

---
+++ src/main/java/gov/nysenate/openleg/model/updates/UpdateType.java
@@ -0,0 +1,7 @@
+package gov.nysenate.openleg.model.updates;
+
+public enum UpdateType
+{
+    PROCESSED_DATE, // Updates that pertain to when pieces of data were processed by the system.
+    PUBLISHED_DATE  // Updates relating to the date when the source data was initially sent.
+}
