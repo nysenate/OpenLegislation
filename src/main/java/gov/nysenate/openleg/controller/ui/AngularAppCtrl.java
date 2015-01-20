9defd0e2ae4c1ef9f945a3db426e7dc49b9d5853
Ash Islam <ashislam858@gmail.com>
2015-01-20 14:56:21 -0500

    AngularApp Ctrl

---
+++ src/main/java/gov/nysenate/openleg/controller/ui/AngularAppCtrl.java
@@ -0,0 +1,20 @@
+package gov.nysenate.openleg.controller.ui;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AngularAppCtrl
+{
+    private static final Logger logger = LoggerFactory.getLogger(AngularAppCtrl.class);
+
+    @RequestMapping({"/",
+                     "/bills/**",
+                     "/calendars/**",
+                     "/laws/**"})
+    public String home() {
+        return "home";
+    }
+}
