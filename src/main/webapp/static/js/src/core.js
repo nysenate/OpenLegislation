684df87f6f4fc89f223c7515d0c1d74f849c457d
Sam Stouffer <samlthecaml@gmail.com>
2015-01-22 17:21:08 -0500

    amcharts directive + updated bower.json and Gruntfile.js

--- src/main/webapp/static/js/src/core.js
+++ src/main/webapp/static/js/src/core.js
@@ -16,3 +16,90 @@ coreModule.filter('moment', ['$filter', function($filter) {
         }
     };
 }]);
+
+
+
+/** --- Am Charts --- */
+
+commonModule.directive('amChart', function () {
+    return {
+        restrict: 'E',
+        replace:true,
+        scope: {
+            chartId: '@',
+            chartClass: '@',
+            config: '='
+        },
+        template: '<div id="{{chartId}}" class="am-chart {{chartClass}}" style="min-width: 310px; height: 400px; margin: 0 auto"></div>',
+        link: function (scope, element, attrs) {
+
+            var chart = false;
+
+            var initChart = function() {
+                if (chart) chart.destroy();
+                var config = scope.config || {};
+                chart = AmCharts.makeChart("chartdiv", config);
+            };
+            initChart();
+
+
+//            {
+//                "type": "serial",
+//                "theme": "none",
+//                "marginLeft": 20,
+//                "pathToImages": "http://www.amcharts.com/lib/3/images/",
+//                "dataProvider": [
+//                {
+//                    "year": "2000",
+//                    "value": 0.267
+//                }, {
+//                    "year": "2001",
+//                    "value": 0.411
+//                }, {
+//                    "year": "2002",
+//                    "value": 0.462
+//                }, {
+//                    "year": "2003",
+//                    "value": 0.47
+//                }, {
+//                    "year": "2004",
+//                    "value": 0.445
+//                }, {
+//                    "year": "2005",
+//                    "value": 0.47
+//                }],
+//                "valueAxes": [{
+//                "axisAlpha": 0,
+//                "inside": true,
+//                "position": "left",
+//                "ignoreAxisWidth": true
+//            }],
+//                "graphs": [{
+//                "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
+//                "bullet": "round",
+//                "bulletSize": 6,
+//                "lineColor": "#d1655d",
+//                "lineThickness": 2,
+//                "negativeLineColor": "#637bb6",
+//                "type": "smoothedLine",
+//                "valueField": "value"
+//            }],
+//                "chartScrollbar": {},
+//                "chartCursor": {
+//                "categoryBalloonDateFormat": "YYYY",
+//                    "cursorAlpha": 0,
+//                    "cursorPosition": "mouse"
+//            },
+//                "dataDateFormat": "YYYY",
+//                "categoryField": "year",
+//                "categoryAxis": {
+//                "minPeriod": "YYYY",
+//                    "parseDates": true,
+//                    "minorGridAlpha": 0.1,
+//                    "minorGridEnabled": true
+//            }
+//            }
+
+        }//end watch
+    }
+});
