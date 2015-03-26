aa74d6de2f7ec749c8002c7ffbdd5de9b52220f3
Josh McDonald <jmcdonald1193@gmail.com>
2015-03-26 13:35:37 -0400

    Cleaned up Api User Dao

--- src/main/java/gov/nysenate/openleg/dao/auth/ApiUserDao.java
+++ src/main/java/gov/nysenate/openleg/dao/auth/ApiUserDao.java
@@ -3,44 +3,25 @@ package gov.nysenate.openleg.dao.auth;
 import gov.nysenate.openleg.model.auth.ApiUser;
 import org.springframework.dao.DataAccessException;
 
+import java.util.List;
+
 /**
  * DAO Interface for retrieving and persisting ApiUser data
  */
 
-public interface ApiUserDao {
-    public void insertUser(ApiUser user) throws DataAccessException;
-
-    public void updateUser(ApiUser user) throws DataAccessException;
-
+public interface ApiUserDao
+{
     public ApiUser getApiUserFromEmail(String email) throws DataAccessException;
 
     public ApiUser getApiUserFromKey(String apikey) throws DataAccessException;
 
-    public String getApiKey(ApiUser user);
+    public void insertUser(ApiUser user) throws DataAccessException;
 
-    public long getNumRequests(String email) throws DataAccessException;
+    public void updateUser(ApiUser user) throws DataAccessException;
 
-    public long getNumRequestFromEmail(String email) throws DataAccessException;
-
-    public String getApiKeyFromEmail(String email) throws DataAccessException;
+    public List<ApiUser> getAllUsers() throws DataAccessException;
 
     public void deleteApiUser(ApiUser apiuser) throws DataAccessException;
 
-    public void deleteApiUserByEmail(String email) throws DataAccessException;
-
     public ApiUser getApiUserFromToken(String token);
-
-    public String getOrganizationFromEmail(String email_addr) throws DataAccessException;
-
-    public String getEmailFromToken(String regToken) throws DataAccessException;
-
-    public String getRegTokenFromEmail (String email_addr) throws DataAccessException;
-
-    public String getEmailFromKey(String key) throws DataAccessException;
-
-    public String getNameFromEmail(String email) throws DataAccessException;
-
-    public boolean getAuthStatusFromEmail(String email) throws DataAccessException;
-
-    public boolean getAuthStatusFromKey(String key) throws DataAccessException;
 }
