a43d865a2936ec6e20f64f00f98eb41df009672a
Sam Stouffer <samlthecaml@gmail.com>
2016-02-24 15:26:14 -0500

    direct patch to prod: calendar entries now present bill amendment at time of calendar

--- src/main/java/gov/nysenate/openleg/client/view/calendar/CalendarEntryView.java
+++ src/main/java/gov/nysenate/openleg/client/view/calendar/CalendarEntryView.java
@@ -1,25 +1,37 @@
 package gov.nysenate.openleg.client.view.calendar;
 
-import gov.nysenate.openleg.client.view.bill.SimpleBillInfoView;
+import gov.nysenate.openleg.client.view.bill.BillInfoView;
+import gov.nysenate.openleg.model.base.Version;
 import gov.nysenate.openleg.model.bill.BillId;
 import gov.nysenate.openleg.model.calendar.CalendarEntry;
 import gov.nysenate.openleg.service.bill.data.BillDataService;
 
 import java.util.Comparator;
+import java.util.Optional;
 
-public class CalendarEntryView extends SimpleBillInfoView {
-
+public class CalendarEntryView extends BillInfoView
+{
     protected int billCalNo;
+
+    protected String selectedVersion;
 
     public CalendarEntryView(CalendarEntry calendarEntry, BillDataService billDataService) {
         super(calendarEntry != null ? billDataService.getBillInfoSafe(BillId.getBaseId(calendarEntry.getBillId())) : null);
         if (calendarEntry != null) {
             this.billCalNo = calendarEntry.getBillCalNo();
+            this.selectedVersion = Optional.ofNullable(calendarEntry.getBillId())
+                    .map(BillId::getVersion)
+                    .map(Version::getValue)
+                    .orElse(null);
         }
     }
 
     public int getBillCalNo() {
         return billCalNo;
+    }
+
+    public String getSelectedVersion() {
+        return selectedVersion;
     }
 
     public static Comparator<CalendarEntryView> calEntryViewComparator =
