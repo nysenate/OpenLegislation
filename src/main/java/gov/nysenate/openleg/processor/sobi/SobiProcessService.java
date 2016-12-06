e80813be10d58a77d584efb60915eada62a242cb
scalls <scalls@rpi.edu>
2016-12-06 10:49:17 -0500

    created many of the xml processor files (which will probably be merged into the sobi ones)

--- src/main/java/gov/nysenate/openleg/processor/sobi/SobiProcessService.java
+++ src/main/java/gov/nysenate/openleg/processor/sobi/SobiProcessService.java
@@ -1,66 +1,46 @@
-package gov.nysenate.openleg.processor.sobi;
+package gov.nysenate.openleg.processor.xml;
 
 import gov.nysenate.openleg.dao.base.LimitOffset;
 import gov.nysenate.openleg.dao.base.SortOrder;
-import gov.nysenate.openleg.model.process.DataProcessRun;
 import gov.nysenate.openleg.model.sobi.SobiFragment;
 import gov.nysenate.openleg.model.sobi.SobiFragmentNotFoundEx;
 import gov.nysenate.openleg.model.sobi.SobiProcessOptions;
+import gov.nysenate.openleg.model.xml.XmlFile;
 import gov.nysenate.openleg.processor.base.ProcessService;
 
-import javax.xml.crypto.Data;
 import java.util.List;
 
 /**
- * The SobiProcessService interface provides the necessary methods for collating
- * and processing sobi files. These methods should typically be used via a
- * process intended to parse new sobi files.
+ * The XmlProcessService interface provides the necessary methods for collating
+ * and processing xml files. These methods should typically be used via a
+ * process intended to parse new xml files.
  */
-public interface SobiProcessService extends ProcessService
+public interface XmlProcessService extends ProcessService
 {
-    /**
-     * Looks for sobi files that have been placed in the incoming directory and
-     * parses them out into SobiFragments. The sobi files are then placed into an
-     * archive directory and the SobiFragments are recorded in the backing store
-     * as pending processing.
-     *
-     * @return int - The number of sobi files that have been collated.
-     */
-    public int collateSobiFiles();
 
     /**
-     * Retrieves the SobiFragments that are awaiting processing.
+     * Retrieves the XmlFiles that are awaiting processing.
      *
-     * @param sortByPubDate SortOrder - Sort order for the fragment id.
+     * @param sortByPubDate SortOrder - Sort order for the files.
      * @param limitOffset LimitOffset - Restrict the results list.
-     * @return List<SobiFragment>
+     * @return List<XmlFile>
      */
-    public List<SobiFragment> getPendingFragments(SortOrder sortByPubDate, LimitOffset limitOffset);
+    public List<XmlFile> getPendingXmlFiles(SortOrder sortByPubDate, LimitOffset limitOffset);
 
     /**
-     * Process the list of supplied SobiFragments.
-     *  @param fragments List<SobiFragment> - List of fragments to process.
-     * @param options - SobiProcessOptions - Provide custom processing options or
+     * Process the list of supplied XmlFiles.
+     *  @param files - List of fragments to process.
+     * @param options - XmlProcessOptions - Provide custom processing options
      */
-    public int processFragments(List<SobiFragment> fragments, SobiProcessOptions options);
+    public int processXmlFiles(List<XmlFile> files, XmlProcessOptions options);
 
     /**
-     * Retrieves all pending fragments and processes them. This is essentially a shorthand
-     * for invoking {@link #getPendingFragments} and running {@link #processFragments} on
-     * the results.
+     * Toggle the pending processing status of an XmlFile via it's fileId.
      *
-     * @param options - SobiProcessOptions - Provide custom processing options or
-     *                                       set to null to use the default options. TODO
-     */
-    public int processPendingFragments(SobiProcessOptions options);
-
-    /**
-     * Toggle the pending processing status of a SobiFragment via it's fragmentId.
-     *
-     * @param fragmentId String - The fragment id
+     * @param fileId String - The file id
      * @param pendingProcessing boolean - Indicate if fragment is pending processing
-     * @throws SobiFragmentNotFoundEx - If the fragmentId did not match a stored fragment
+     * @throws XmlFragmentNotFoundEx - If the fileId did not match a stored file
      */
-    public void updatePendingProcessing(String fragmentId, boolean pendingProcessing)
-                                        throws SobiFragmentNotFoundEx;
+    public void updatePendingProcessing(String fileId, boolean pendingProcessing)
+            throws XmlFragmentNotFoundEx;
 }
