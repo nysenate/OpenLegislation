94cda2fe887fd51bcb674cd2bae167034dd0151e
scalls <scalls@rpi.edu>
2016-12-06 10:49:17 -0500

    created many of the xml processor files (which will probably be merged into the sobi ones)

--- src/main/java/gov/nysenate/openleg/processor/xml/XmlParseException.java
+++ src/main/java/gov/nysenate/openleg/processor/xml/XmlParseException.java
@@ -1,7 +1,27 @@
 package gov.nysenate.openleg.processor.xml;
 
-/**
- * Created by scally on 11/30/16.
- */
-public class XmlParseException {
+import gov.nysenate.openleg.model.xml.XmlFile;
+import gov.nysenate.openleg.processor.base.ParseError;
+
+import java.time.LocalDateTime;
+
+public class XmlParseException extends ParseError {
+
+    // The file that caused the exception
+    private XmlFile xmlFile;
+
+    // Time datetime at which it occurred
+    private LocalDateTime parseDateTime;
+
+    public XmlParseException(String message, XmlFile xmlFile, LocalDateTime parseDateTime) {
+        super(message);
+        this.xmlFile = xmlFile;
+        this.parseDateTime = parseDateTime;
+    }
+
+    public XmlParseException(String message, Throwable cause, XmlFile xmlFile, LocalDateTime parseDateTime) {
+        super(message, cause);
+        this.xmlFile = xmlFile;
+        this.parseDateTime = parseDateTime;
+    }
 }
