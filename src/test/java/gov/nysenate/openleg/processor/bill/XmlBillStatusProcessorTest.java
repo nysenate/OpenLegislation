15caa1cc04afd9442708121fd3aa1fcf867bce74
Uros Antic <u18anti@siena.edu>
2017-02-14 13:01:39 -0500

    created XmlBillStatusProcessor class and test class

---
+++ src/test/java/gov/nysenate/openleg/processor/bill/XmlBillStatusProcessorTest.java
@@ -0,0 +1,12 @@
+package gov.nysenate.openleg.processor.bill;
+
+import gov.nysenate.openleg.BaseTests;
+
+/**
+ * Created by uros on 2/14/17.
+ */
+public class XmlBillStatusProcessorTest extends BaseTests {
+
+
+
+}
