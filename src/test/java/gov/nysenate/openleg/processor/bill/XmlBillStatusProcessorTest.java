c9cacbdd47b7b3f728acf55e2374c02676c21123
Uros Antic <u18anti@siena.edu>
2017-02-16 10:06:37 -0500

    Added code to XmlBillStatusProcessor.java class

--- src/test/java/gov/nysenate/openleg/processor/bill/XmlBillStatusProcessorTest.java
+++ src/test/java/gov/nysenate/openleg/processor/bill/XmlBillStatusProcessorTest.java
@@ -1,12 +1,22 @@
 package gov.nysenate.openleg.processor.bill;
 
 import gov.nysenate.openleg.BaseTests;
+import gov.nysenate.openleg.model.sobi.SobiFragment;
+import org.junit.Test;
 
 /**
  * Created by uros on 2/14/17.
  */
 public class XmlBillStatusProcessorTest extends BaseTests {
 
+    @Test
+    public void test()  {
+        process("billstatfile.xml");
+    }
 
+    private SobiFragment process(String filename)   {
+
+        return null;
+    }
 
 }
