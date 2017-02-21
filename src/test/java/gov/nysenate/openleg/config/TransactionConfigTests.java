262603eac962e3983c3d479cdd3154a5f2bbc26c
Sam Stouffer <samlthecaml@gmail.com>
2017-02-21 15:37:51 -0500

    Add rollback tests

--- src/test/java/gov/nysenate/openleg/config/TransactionConfigTests.java
+++ src/test/java/gov/nysenate/openleg/config/TransactionConfigTests.java
@@ -1,11 +1,15 @@
 package gov.nysenate.openleg.config;
 
 import gov.nysenate.openleg.BaseTests;
+import gov.nysenate.openleg.dao.base.ImmutableParams;
+import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Propagation;
@@ -18,8 +22,9 @@ public class TransactionConfigTests extends BaseTests
     private static final
     Logger logger = LoggerFactory.getLogger(TransactionConfigTests.class);
 
-    @Autowired
-    private JdbcTemplate jdbc;
+    @Autowired private JdbcTemplate jdbc;
+
+    @Autowired private NamedParameterJdbcTemplate jdbcNamed;
 
     @Transactional(rollbackFor = RuntimeException.class)
     public void testTransaction(int c) {
@@ -33,16 +38,57 @@ public class TransactionConfigTests extends BaseTests
 
     }
 
-    @Test
+    @Test(expected = RuntimeException.class)
     @Transactional
     public void testTransactionMethod() throws Exception {
         testTransaction(3212);
     }
 
-    @Transactional(propagation = Propagation.REQUIRED)
+    @Test
     @Rollback
-    private void hah(int i) {
-        testTransaction(i);
-        moose(i);
+    public void rollbackTest() {
+        rollbackTestMethod();
     }
+
+    @Test
+    @Rollback
+    public void anotherRollbackTest() {
+        rollbackTestMethod();
+    }
+
+    private void rollbackTestMethod() {
+        final int session = 2037;
+        final String printNo = "X5000";
+        final String activeVersion = "Q";
+
+        final ImmutableParams params = ImmutableParams.from(new MapSqlParameterSource()
+                .addValue("session", session)
+                .addValue("printNo", printNo)
+                .addValue("activeVersion", activeVersion)
+        );
+
+        final String querySql =
+                "SELECT COUNT(*)\n" +
+                "FROM master.bill\n" +
+                "WHERE bill_session_year = :session\n" +
+                "  AND bill_print_no = :printNo\n" +
+                "  AND active_version = :activeVersion\n";
+
+        final String insertSql =
+                "INSERT INTO master.bill (bill_session_year, bill_print_no, active_version)\n" +
+                "                 VALUES (:session,          :printNo,      :activeVersion)\n";
+
+        final int preCount = jdbcNamed.queryForObject(querySql, params, Integer.class);
+        Assert.assertEquals(
+                "Bill " + printNo + activeVersion + "-" + session + " should not exist in the database",
+                0, preCount);
+
+        jdbcNamed.update(insertSql, params);
+
+        final int postCount = jdbcNamed.queryForObject(querySql, params, Integer.class);
+        Assert.assertEquals(
+                "Bill " + printNo + activeVersion + "-" + session + " should have been inserted into the database",
+                1, postCount);
+    }
+
 }
