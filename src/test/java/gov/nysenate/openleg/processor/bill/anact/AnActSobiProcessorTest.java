43a28d64fce9bd83edaa2838d85e49260646f7e9
Robert Bebber <robert.l.bebber@gmail.com>
2017-02-21 16:17:27 -0500

    Added AnActSobeProcessorTest with a Corresponding Tester

---
+++ src/test/java/gov/nysenate/openleg/processor/bill/anact/AnActSobiProcessorTest.java
@@ -0,0 +1,16 @@
+package gov.nysenate.openleg.processor.bill;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by robert on 2/15/17.
+ */
+public class AnActSobiProcessorTest {
+    @Test
+    public void process() throws Exception {
+
+    }
+
+}
