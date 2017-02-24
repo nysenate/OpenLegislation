8b1f1d3efa1f3e492c75fa10386c072d6eca5ac5
Sam Stouffer <samlthecaml@gmail.com>
2017-02-24 17:55:31 -0500

    Add base xml processor test class
     - Add BaseXmlProcessorTest with common functionality for testing xml
       sobi processors
     - Refactor AnactSobiProcessorTest to use BaseXmlProcessorTest

--- src/test/java/gov/nysenate/openleg/processor/bill/anact/AnActSobiProcessorTest.java
+++ src/test/java/gov/nysenate/openleg/processor/bill/anact/AnActSobiProcessorTest.java
@@ -1,24 +1,16 @@
 package gov.nysenate.openleg.processor.bill.anact;
 
-import gov.nysenate.openleg.BaseTests;
 import gov.nysenate.openleg.dao.bill.data.BillDao;
-import gov.nysenate.openleg.dao.sobi.SobiDao;
 import gov.nysenate.openleg.model.base.Version;
 import gov.nysenate.openleg.model.bill.Bill;
 import gov.nysenate.openleg.model.bill.BillId;
-import gov.nysenate.openleg.model.sobi.SobiFile;
-import gov.nysenate.openleg.model.sobi.SobiFragment;
-import gov.nysenate.openleg.model.sobi.SobiFragmentType;
-import gov.nysenate.openleg.service.bill.search.BillSearchService;
-import org.apache.commons.io.FileUtils;
+import gov.nysenate.openleg.processor.BaseXmlProcessorTest;
+import gov.nysenate.openleg.processor.sobi.SobiProcessor;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.io.File;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,50 +18,41 @@ import static org.junit.Assert.assertEquals;
  * Created by robert on 2/15/17.
  */
 @Transactional
-public class AnActSobiProcessorTest extends BaseTests{
+public class AnActSobiProcessorTest extends BaseXmlProcessorTest {
+
     @Autowired BillDao billDao;
-    @Autowired SobiDao sobiDao;
     @Autowired AnActSobiProcessor anActSobiProcessor;
+
     private static final Logger logger = LoggerFactory.getLogger(AnActSobiProcessorTest.class);
+
+    @Override
+    protected SobiProcessor getSobiProcessor() {
+        return anActSobiProcessor;
+    }
 
     @Test
     public void processReplaceTest() throws Exception {
-        Bill a=billDao.getBill(new BillId("S08215",2015));
+        final String anactXmlFilePath = "processor/bill/anact/2016-12-02-09.16.10.220257_ANACT_S08215.XML";
 
-        File anactXmlFile = new File(getClass().getClassLoader().getResource(
-                "processor/bill/anact/2016-12-02-09.16.10.220257_ANACT_S08215.XML").getFile());
-        String contents=FileUtils.readFileToString(anactXmlFile);
-        SobiFile sobiFileTester = new SobiFile(anactXmlFile);
-        SobiFragment sobiFragmentTester=new SobiFragment(sobiFileTester, SobiFragmentType.ANACT,contents,1);
-        sobiDao.updateSobiFile(sobiFileTester);
-        sobiDao.updateSobiFragment(sobiFragmentTester);
-        anActSobiProcessor.process(sobiFragmentTester);
-        anActSobiProcessor.postProcess();
+        processXmlFile(anactXmlFilePath);
 
-
-        Bill b=billDao.getBill(new BillId("S08215",2015));
-        String expectedS="AN ACT to amend the executive law, in relation to the appointment of\n" +
+        Bill b = billDao.getBill(new BillId("S08215", 2015));
+        String expectedS = "AN ACT to amend the executive law, in relation to the appointment of\n" +
                 "interpreters to be used in parole board proceedings [altered]";
-        String actualClause=b.getAmendment(Version.DEFAULT).getActClause();
-        assertEquals(expectedS,actualClause);
+        String actualClause = b.getAmendment(Version.DEFAULT).getActClause();
+        assertEquals(expectedS, actualClause);
     }
 
     @Test
     public void processRemoveTest() throws Exception {
-        File anactXmlFile = new File(getClass().getClassLoader().getResource(
-                "processor/bill/anact/2017-02-09-12.36.50.736583_ANACT_A05462.XML").getFile());
-        String contents=FileUtils.readFileToString(anactXmlFile);
-        SobiFile sobiFileTester = new SobiFile(anactXmlFile);
-        SobiFragment sobiFragmentTester=new SobiFragment(sobiFileTester, SobiFragmentType.ANACT,contents,1);
-        sobiDao.updateSobiFile(sobiFileTester);
-        sobiDao.updateSobiFragment(sobiFragmentTester);
-        anActSobiProcessor.process(sobiFragmentTester);
-        anActSobiProcessor.postProcess();
+        final String anactXmlFilePath = "processor/bill/anact/2017-02-09-12.36.50.736583_ANACT_A05462.XML";
 
-        Bill b=billDao.getBill(new BillId("A05462",2017));
-        String expectedS="";
-        String actualClause=b.getAmendment(Version.DEFAULT).getActClause();
-        assertEquals(expectedS,actualClause);
+        processXmlFile(anactXmlFilePath);
+
+        Bill b = billDao.getBill(new BillId("A05462", 2017));
+        String expectedS = "";
+        String actualClause = b.getAmendment(Version.DEFAULT).getActClause();
+        assertEquals(expectedS, actualClause);
     }
 
 }
