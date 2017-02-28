11a7db5543c4590af3372fee119456006088161e
Chenguang He <gaoyike@gmail.com>
2017-02-28 15:01:16 -0500

    fixes #10876
    Fixes exceptiones

--- src/main/java/gov/nysenate/openleg/processor/bill/BillXMLBillDigestProcessor.java
+++ src/main/java/gov/nysenate/openleg/processor/bill/BillXMLBillDigestProcessor.java
@@ -5,8 +5,8 @@ import gov.nysenate.openleg.model.base.Version;
 import gov.nysenate.openleg.model.bill.Bill;
 import gov.nysenate.openleg.model.bill.BillId;
 import gov.nysenate.openleg.model.process.DataProcessUnit;
-import gov.nysenate.openleg.model.sourcefiles.sobi.SobiFragment;
-import gov.nysenate.openleg.model.sourcefiles.sobi.SobiFragmentType;
+import gov.nysenate.openleg.model.sobi.SobiFragment;
+import gov.nysenate.openleg.model.sobi.SobiFragmentType;
 import gov.nysenate.openleg.processor.base.AbstractDataProcessor;
 import gov.nysenate.openleg.processor.base.ParseError;
 import gov.nysenate.openleg.processor.sobi.SobiProcessor;
@@ -32,8 +32,7 @@ public class BillXMLBillDigestProcessor extends AbstractDataProcessor implements
     @Autowired
     private XmlHelper xmlHelper;
 
-    public BillXMLBillDigestProcessor() {
-    }
+    public BillXMLBillDigestProcessor() {}
 
     @Override
     public SobiFragmentType getSupportedType() {
@@ -47,40 +46,31 @@ public class BillXMLBillDigestProcessor extends AbstractDataProcessor implements
         DataProcessUnit unit = createProcessUnit(sobiFragment);
         try {
             final Document doc = xmlHelper.parse(sobiFragment.getText());
-            final Node billTextNode = xmlHelper.getNode("digestsummary", doc);
-            final int sessionYear = xmlHelper.getInteger("@sessyr", billTextNode);
-            final String billhse = xmlHelper.getString("@billhse", billTextNode);
-            final String billno = xmlHelper.getString("@billno", billTextNode);
-            final String action = xmlHelper.getString("@action", billTextNode); //todo wait for LBDC explanation of action
-            final String summary = xmlHelper.getNode("digestsummary/summary", doc) == null ? "" : xmlHelper.getNode("digestsummary/summary", doc).getTextContent();
-            final String amd = xmlHelper.getString("digestsummary/summaryamendment", doc);
+            final Node billTextNode = xmlHelper.getNode("digestsummary",doc);
+            final int sessionYear = xmlHelper.getInteger("@sessyr",billTextNode);
+            final String billhse = xmlHelper.getString("@billhse",billTextNode);
+            final String billno = xmlHelper.getString("@billno",billTextNode);
+            final String action = xmlHelper.getString("@action",billTextNode); //todo wait for LBDC explanation of action
+            final String summary = xmlHelper.getNode("digestsummary/summary",doc).getTextContent();
+            final String amd = "";//todo wait for LBDC explanation
             final Version version = Version.of(amd);
-            final String law = xmlHelper.getString("law", billTextNode);
-            final Bill baseBill = getOrCreateBaseBill(sobiFragment.getPublishedDateTime(), new BillId(billhse + billno, new SessionYear(sessionYear), version), sobiFragment);
+            final Bill baseBill = getOrCreateBaseBill(sobiFragment.getPublishedDateTime(), new BillId(billhse+billno, new SessionYear(sessionYear),version) ,sobiFragment);
             baseBill.setSummary(summary);
-            baseBill.getAmendment(version).setLaw(law);
-            if (action.equals("replace")) { //replace bill
-                /**
-                 * add previous bills
-                 */
-                int totalNumsOfPreBills = xmlHelper.getNodeList("digestsummary/oldbill/oldyear", doc).getLength();
-                for (int i = 1; i <= totalNumsOfPreBills; i++) {
-                    int sess = xmlHelper.getInteger("digestsummary/oldbill/oldyear[" + i + "]", doc);
-                    String oldhse = xmlHelper.getString("digestsummary/oldbill/oldhse[" + i + "]", doc).replaceAll("\n", "");
-                    String oldno = xmlHelper.getString("digestsummary/oldbill/oldno[" + i + "]", doc).replaceAll("\n", "");
-                    String oldamd = xmlHelper.getString("digestsummary/oldbill/oldamd[" + i + "]", doc).replaceAll("\n", "");
-                    baseBill.addDirectPreviousVersion(new BillId(oldhse + oldno, SessionYear.of(sess), Version.of(oldamd)));
-                }
-            } else { //remove bill
-
-                // clear Set<BillID> pre version
-                baseBill.getAllPreviousVersions().clear();
-                baseBill.getDirectPreviousVersions().clear();
+            /**
+             * add previous bills
+             */
+            int totalNumsOfPreBills = xmlHelper.getNodeList("digestsummary/oldbill/oldyear",doc).getLength();
+            for (int i = 1; i <= totalNumsOfPreBills; i++) {
+                int  sess = xmlHelper.getInteger("digestsummary/oldbill/oldyear["+i+"]",doc);
+                String oldhse = xmlHelper.getString("digestsummary/oldbill/oldhse["+i+"]",doc).replaceAll("\n","");
+                String oldno = xmlHelper.getString("digestsummary/oldbill/oldno["+i+"]",doc).replaceAll("\n","");
+                String oldamd = xmlHelper.getString("digestsummary/oldbill/oldamd["+i+"]",doc).replaceAll("\n","");
+                baseBill.addDirectPreviousVersion(new BillId(oldhse+oldno, SessionYear.of(sess),Version.of(oldamd)));
             }
             billIngestCache.set(baseBill.getBaseBillId(), baseBill, sobiFragment);
             logger.info("Put base bill in the ingest cache.");
-        } catch (IOException | SAXException | XPathExpressionException e) {
-            throw new ParseError("Error While Parsing Bill Digest XML : " + sobiFragment.getFragmentId(), e);
+        } catch (IOException | SAXException |XPathExpressionException e) {
+            throw new ParseError("Error While Parsing Bill Digest XML", e);
         }
     }
 
