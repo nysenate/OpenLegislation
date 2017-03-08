4696b8f63a4df27c79e011d07e81ed686ba0e7cc
SenateUser <senateuser@senate-hp-elitedesk-800-g2-dm-35w.nysenate.gov>
2017-03-08 16:58:14 -0500

    This processor will help grab html tags needed for memos and other HTML styled files * Added the abstract class * Added a getText method to build the segments

--- src/main/java/gov/nysenate/openleg/processor/bill/AbstractMemoProcessor.java
+++ src/main/java/gov/nysenate/openleg/processor/bill/AbstractMemoProcessor.java
@@ -10,18 +10,9 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 /**
- * This Class is responsible for taking the context of the Memo files for processing.
- * <p>
- * Created by Robert Bebber on 3/8/17.
+ * Created by senateuser on 3/8/17.
  */
 public abstract class AbstractMemoProcessor extends AbstractDataProcessor {
-
-    /**
-     * This method is responsible for pulling the content tag and if it isn't present will throw a Parsing Exception.
-     *
-     * @param memo The String of the content tag with CSS script
-     * @return
-     */
     protected String parsedMemoHTML(String memo) {
         Document document = Jsoup.parse(memo);
         Elements elements = document.body().select("pre");
@@ -31,21 +22,15 @@ public abstract class AbstractMemoProcessor extends AbstractDataProcessor {
         return getText(elements.get(0));
     }
 
-    /**
-     * This method is responsible for building a string of the entire content tag, as standing version of JSoup had
-     * not supported getWholeText.
-     *
-     * @param cell This the pre tag for the content to be fulled from
-     * @return
-     */
     private String getText(Element cell) {
         StringBuilder textBuilder = new StringBuilder();
         for (Node node : cell.childNodes()) {
             if (node instanceof TextNode) {
-                textBuilder.append(((TextNode) node).getWholeText());
-            } else {
+                textBuilder.append(((TextNode)node).getWholeText());
+            }
+            else {
                 for (Node childNode : node.childNodes()) {
-                    textBuilder.append(getText((Element) childNode));
+                    textBuilder.append(getText((Element)childNode));
                 }
                 textBuilder.append(node.outerHtml());
             }
