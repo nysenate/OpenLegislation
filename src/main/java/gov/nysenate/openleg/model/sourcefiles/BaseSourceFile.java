000766384cd9715bf3365d929e0ba902ebbfd9f2
Hamza Memon <h01memo@siena.edu>
2017-03-29 11:52:42 -0400

    XML file management WIP
    Created new packages and interfaces for the
    Sobi/XML Daos and the Sobi/XML filetypes
    Adjusted the code to work with these new files

---
+++ src/main/java/gov/nysenate/openleg/model/sourcefiles/BaseSourceFile.java
@@ -0,0 +1,131 @@
+package gov.nysenate.openleg.model.sourcefiles;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.google.common.base.MoreObjects;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+import gov.nysenate.openleg.model.sourcefiles.sobi.InvalidSobiNameEx;
+import gov.nysenate.openleg.model.sourcefiles.sobi.SobiFileNotFoundEx;
+import gov.nysenate.openleg.model.sourcefiles.sobi.SourceFile;
+import gov.nysenate.openleg.model.sourcefiles.sobi.UnreadableSobiEx;
+
+public class BaseSourceFile implements SourceFile {
+    
+    /**
+     * SOBI and XML files are (mostly) in a CP850 or similar encoding. This was determined from the
+     * byte mapping of
+     * paragraph/section characters to 244/245. This can't be 100% correct though because the degree symbol
+     * must be 193 in the correct code set. See SOBI.D120612.T125850.TXT.
+     */
+    private static final String DEFAULT_ENCODING = "CP850";
+    private static final Logger logger = LoggerFactory.getLogger(BaseSourceFile.class);
+    
+    /** Reference to the actual file. */
+    private File file;
+    
+    /** The encoding this file was written in. */
+    private String encoding;
+    
+    /** The datetime when the Sobi or XML files was recorded into the backing store. */
+    private LocalDateTime stagedDateTime;
+    
+    /** Indicates if the underlying 'file' reference has been moved into an archive directory. */
+    private boolean archived;
+    
+    /** --- Constructors --- */
+    
+    public BaseSourceFile(File sobiFile) throws IOException, SobiFileNotFoundEx{
+        this(sobiFile, DEFAULT_ENCODING);
+    }
+    
+    public BaseSourceFile(File file, String encoding) throws IOException, SobiFileNotFoundEx{
+        if(file.exists()){
+            this.file = file;
+            this.encoding = encoding;
+            archived = false;
+            // Attempt to parse the file name, raising an exception if the name is invalid
+            getPublishedDateTime();
+        }
+        else{
+            throw new FileNotFoundException(file.getAbsolutePath());
+        }
+    }
+    
+    /** --- Functional Getters/Setters --- */
+    
+    /** The file name serves as the unique identifier for the Sobi or XML files. */
+    @Override
+    public String getFileName(){
+        return file.getName();
+    }
+    
+    /**
+     * Retrieves the text contained within the file. The text is not saved due to the
+     * added memory overhead when retaining references to Sobi or XML files.
+     */
+    @Override
+    @JsonIgnore
+    public String getText(){
+        try{
+            return FileUtils.readFileToString(file, encoding);
+        }
+        catch(IOException e){
+            throw new UnreadableSobiEx(this, e);
+        }
+    }
+    
+    @Override
+    public LocalDateTime getPublishedDateTime() throws InvalidSobiNameEx{
+        return null;
+    }
+    
+    /** --- Override Methods --- */
+    
+    @Override
+    public String toString(){
+        return MoreObjects.toStringHelper(this)
+                .add("file", file)
+                .add("encoding", encoding)
+                .add("stagedDateTime", stagedDateTime)
+                .add("archived", archived)
+                .toString();
+    }
+    
+    /** --- Basic Getters/Setters --- */
+    
+    public File getFile(){
+        return file;
+    }
+    
+    public void setFile(File file){
+        this.file = file;
+    }
+    
+    public String getEncoding(){
+        return encoding;
+    }
+    
+    public LocalDateTime getStagedDateTime(){
+        return stagedDateTime;
+    }
+    
+    public void setStagedDateTime(LocalDateTime stagedDateTime){
+        this.stagedDateTime = stagedDateTime;
+    }
+    
+    public boolean isArchived(){
+        return archived;
+    }
+    
+    public void setArchived(boolean archived){
+        this.archived = archived;
+    }
+}
---
+++ src/main/java/gov/nysenate/openleg/model/sourcefiles/xml/XmlFile.java
@@ -0,0 +1,56 @@
+package gov.nysenate.openleg.model.sourcefiles.xml;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import gov.nysenate.openleg.model.sourcefiles.BaseSourceFile;
+import gov.nysenate.openleg.model.sourcefiles.sobi.InvalidSobiNameEx;
+
+/**
+ * Represents the 'newer' source files that LBDC sends.
+ */
+public class XmlFile extends BaseSourceFile {
+    /** The format for the XML files */
+    private static final String xmlPattern = "yyyy'-'MM'-'dd'-'HH'.'mm'.'ss'.'SSS";
+    
+    private static final Pattern fileNamePattern = Pattern.compile(
+            "(?<date>[0-9-]{10})-(?<time>[0-9.]{15})_(?<type>[A-Z]+)_(?<target>.+)\\.XML");
+    
+    /** --- Constructors --- */
+    
+    public XmlFile(File sobiFile) throws IOException{
+        super(sobiFile);
+    }
+    
+    public XmlFile(File file, String encoding) throws IOException{
+        super(file, encoding);
+    }
+    
+    /** --- Overrides --- */
+    
+    /**
+     * Get the published date time from the file name.
+     *
+     * @return LocalDateTime
+     */
+    @Override
+    public LocalDateTime getPublishedDateTime() throws InvalidSobiNameEx{
+        try{
+            Matcher m = fileNamePattern.matcher(getFileName());
+            if(m.matches()){
+                return LocalDateTime.parse(m.group("date") + "T" + m.group("time"));
+            }
+        }
+        catch(DateTimeParseException ex){
+            throw new IllegalStateException(
+                    "Failed to parse published datetime from Sobi XML: " + ex.getMessage());
+        }
+        throw new IllegalStateException(
+                "Failed to parse published datetime from Sobi XML because the filename" +
+                        " did not match the required format.");
+    }
+}
