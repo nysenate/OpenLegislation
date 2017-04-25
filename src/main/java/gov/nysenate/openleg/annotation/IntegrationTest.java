b01d554c31698034f1cc585909977c3c600ca8b7
Hamza Memon <h01memo@siena.edu>
2017-04-25 14:15:45 -0400

    Created a script to generate Tester files based on the file contents It currently doesn't create the files, but it prints out what the file would look like if it had ran

---
+++ src/main/java/gov/nysenate/openleg/annotation/IntegrationTest.java
@@ -0,0 +1,12 @@
+package gov.nysenate.openleg.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Labels an integration test
+ * Integration tests may test more than one module (unlike a {@link UnitTest})
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface IntegrationTest {
+}
---
+++ src/main/java/gov/nysenate/openleg/annotation/SillyTest.java
@@ -0,0 +1,11 @@
+package gov.nysenate.openleg.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Marks a test case as being silly, i.e. maybe non-reproducible and not really intended to pass.
+ */
+@Retention(RetentionPolicy.SOURCE)
+public @interface SillyTest {
+}
