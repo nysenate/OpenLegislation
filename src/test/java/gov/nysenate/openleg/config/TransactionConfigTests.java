0e703372a43127a179876691eaa1ecfd93ff5462
Sam Stouffer <samlthecaml@gmail.com>
2017-05-11 15:21:59 -0400

    Add test for transaction duplicate key exceptions
    Add a test that demonstrates that duplicate key exceptions will cancel a
    database transaction.

--- src/test/java/gov/nysenate/openleg/config/TransactionConfigTests.java
+++ src/test/java/gov/nysenate/openleg/config/TransactionConfigTests.java
@@ -7,20 +7,35 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @TransactionConfiguration(defaultRollback = false)
 @Transactional
 public class TransactionConfigTests extends BaseTests
 {
-    private static final
-    Logger logger = LoggerFactory.getLogger(TransactionConfigTests.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransactionConfigTests.class);
+
+    private final int session = 2037;
+    private final String printNo = "X5000";
+    private final String activeVersion = "Q";
+
+    private final String querySql =
+            "SELECT COUNT(*)\n" +
+                    "FROM master.bill\n" +
+                    "WHERE bill_session_year = :session\n" +
+                    "  AND bill_print_no = :printNo\n" +
+                    "  AND active_version = :activeVersion\n";
+
+    private final String insertSql =
+            "INSERT INTO master.bill (bill_session_year, bill_print_no, active_version)\n" +
+                    "                 VALUES (:session,          :printNo,      :activeVersion)\n";
 
     @Autowired private JdbcTemplate jdbc;
 
@@ -56,27 +71,40 @@ public class TransactionConfigTests extends BaseTests
         rollbackTestMethod();
     }
 
+    @Test(expected = UncategorizedSQLException.class)
+    public void duplicateKeyTest() {
+        logger.info("first insert...");
+        insertBill();
+        logger.info("second insert...");
+        insertBill();
+    }
+
+    private void insertBill() {
+        final ImmutableParams params = ImmutableParams.from(new MapSqlParameterSource()
+                .addValue("session", session)
+                .addValue("printNo", printNo)
+                .addValue("activeVersion", activeVersion)
+        );
+        try {
+            jdbcNamed.update(insertSql, params);
+
+        } catch (DuplicateKeyException ex) {
+            logger.error("Duplicate KEY!!!");
+        }
+        final int postCount = jdbcNamed.queryForObject(querySql, params, Integer.class);
+        logger.info("bills found: {}", postCount);
+        Assert.assertEquals(
+                "Bill " + printNo + activeVersion + "-" + session + " should have been inserted into the database",
+                1, postCount);
+    }
+
     private void rollbackTestMethod() {
-        final int session = 2037;
-        final String printNo = "X5000";
-        final String activeVersion = "Q";
 
         final ImmutableParams params = ImmutableParams.from(new MapSqlParameterSource()
                 .addValue("session", session)
                 .addValue("printNo", printNo)
                 .addValue("activeVersion", activeVersion)
         );
-
-        final String querySql =
-                "SELECT COUNT(*)\n" +
-                "FROM master.bill\n" +
-                "WHERE bill_session_year = :session\n" +
-                "  AND bill_print_no = :printNo\n" +
-                "  AND active_version = :activeVersion\n";
-
-        final String insertSql =
-                "INSERT INTO master.bill (bill_session_year, bill_print_no, active_version)\n" +
-                "                 VALUES (:session,          :printNo,      :activeVersion)\n";
 
         final int preCount = jdbcNamed.queryForObject(querySql, params, Integer.class);
         Assert.assertEquals(
