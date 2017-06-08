456b840c402b4dbbc95d4bbb7ef251d35fd01225
Kevin Caseiras <kcaseiras@gmail.com>
2017-06-08 11:14:23 -0400

    Fixes to calendar refactor script

--- src/main/resources/sql/qa-redesign/calendar-type-refactor.sql
+++ src/main/resources/sql/qa-redesign/calendar-type-refactor.sql
@@ -1,5 +1,2 @@
-
-UPDATE master.spotcheck_mismatch
-SET key = key || hstore('type', 'ALL')
-WHERE content_type = 'CALENDAR'
-AND NOT exist(key, 'type');
+DELETE FROM master.spotcheck_mismatch WHERE reference_type = 'LBDC_CALENDAR_ALERT'
+DELETE FROM master.spotcheck_report WHERE reference_type = 'LBDC_CALENDAR_ALERT'
