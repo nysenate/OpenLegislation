237643c9d5860e74a006787bfe2db5515b9b1c95
Chenguang He <gaoyike@gmail.com>
2017-06-12 10:43:14 -0400

    fixes #10981 Improve sorting function

--- src/main/java/gov/nysenate/openleg/client/view/spotcheck/MismatchTypeSummaryView.java
+++ src/main/java/gov/nysenate/openleg/client/view/spotcheck/MismatchTypeSummaryView.java
@@ -15,7 +15,11 @@ public class MismatchTypeSummaryView  implements ViewObject {
     protected MapView<SpotCheckMismatchType, Integer> typeCount;
 
     public MismatchTypeSummaryView(MismatchTypeSummary summary) {
-        this.typeCount = MapView.ofIntMap(sortByValues(summary.getSummary()));
+        if (summary.getSummary().get(SpotCheckMismatchType.All) == 0){ // if there is no mismatch, sort by alphabet
+            this.typeCount = MapView.ofIntMap(sortByKeys(summary.getSummary()));
+        }
+        else // else, sort by number of mismatch
+            this.typeCount = MapView.ofIntMap(sortByValues(summary.getSummary()));
     }
 
     private <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
@@ -29,6 +33,21 @@ public class MismatchTypeSummaryView  implements ViewObject {
         Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
         sortedByValues.putAll(map);
         return sortedByValues;
+    }
+    private <K extends Comparable<K>, V> Map<K, V> sortByKeys(final Map<K, V> map) {
+        Comparator<K> keyComparator =  new Comparator<K>() {
+            public int compare(K k1, K k2) {
+                if (k1.equals(SpotCheckMismatchType.All))
+                    return 1;
+                else if (k2.equals(SpotCheckMismatchType.All))
+                    return -1;
+                else
+                    return k2.toString().compareTo(k1.toString());
+            }
+        };
+        Map<K, V> sortedByKeys = new TreeMap<K, V>(keyComparator);
+        sortedByKeys.putAll(map);
+        return sortedByKeys;
     }
 
     public MapView<SpotCheckMismatchType, Integer> getTypeCount(){
