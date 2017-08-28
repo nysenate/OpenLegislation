5d6c17689462c87f1da50625e06ff361501bff8d
Kevin Caseiras <kcaseiras@gmail.com>
2017-08-28 10:43:42 -0400

    Update daybreak senate high email subject

--- src/main/java/gov/nysenate/openleg/model/spotcheck/daybreak/DaybreakDocType.java
+++ src/main/java/gov/nysenate/openleg/model/spotcheck/daybreak/DaybreakDocType.java
@@ -12,7 +12,7 @@ import java.util.List;
 public enum DaybreakDocType {
     PAGE_FILE       (".page_file.txt",      "Job ABPSDD - LBDC all Bills"),
     SENATE_LOW      (".senate.low.html",    "Sen Act Title Sum Spon Law 1-4000"),
-    SENATE_HIGH     (".senate.high.html",   "Sen Act Title Sum Spon Law 4001-9999"),
+    SENATE_HIGH     (".senate.high.html",   "Sen Act Title Sum Spon Law 4001-99999"),
     ASSEMBLY_LOW    (".assembly.low.html",  "Asm Act Title Sum Spon Law 1-4000"),
     ASSEMBLY_HIGH   (".assembly.high.html", "Asm Act Title Sum Spon Law 4001-99999")
     ;
