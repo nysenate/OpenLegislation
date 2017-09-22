49ed997d6126519e77ac02763af2dc8c6545655d
Anthony Calabrese <aacal050@gmail.com>
2017-09-22 15:20:59 -0400

    Implement OpenlegAgenda Report and Check Services
    The logic for the OpenlegAgenda Report and Check services has been
    implemented.
    As soon as some Id fields are added to prod, the spotcheck
    will become operational.

--- src/main/java/gov/nysenate/openleg/service/spotcheck/openleg/OpenlegAgendaCheckService.java
+++ src/main/java/gov/nysenate/openleg/service/spotcheck/openleg/OpenlegAgendaCheckService.java
@@ -3,8 +3,10 @@ package gov.nysenate.openleg.service.spotcheck.openleg;
 import gov.nysenate.openleg.client.view.agenda.AgendaCommAddendumView;
 import gov.nysenate.openleg.model.agenda.CommitteeAgendaAddendumId;
 import gov.nysenate.openleg.model.spotcheck.ReferenceDataNotFoundEx;
+import gov.nysenate.openleg.model.spotcheck.SpotCheckMismatchType;
 import gov.nysenate.openleg.model.spotcheck.SpotCheckObservation;
 import gov.nysenate.openleg.service.spotcheck.base.BaseSpotCheckService;
+import gov.nysenate.openleg.util.OutputUtils;
 import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +27,54 @@ public class OpenlegAgendaCheckService extends BaseSpotCheckService<CommitteeAge
 
     @Override
     public SpotCheckObservation<CommitteeAgendaAddendumId>  check(AgendaCommAddendumView content, AgendaCommAddendumView reference) {
-        return null;
+        final SpotCheckObservation<CommitteeAgendaAddendumId> observation = new SpotCheckObservation<>(reference.getCommitteeAgendaAddendumId());
+        checkModifiedDateTime(content,reference,observation);
+        checkChair(content,reference,observation);
+        checkLocation(content,reference,observation);
+        checkMeetingDateTime(content,reference,observation);
+        checkNotes(content,reference,observation);
+        checkBillListing(content,reference,observation);
+        checkHasVotes(content,reference,observation);
+        if(content.isHasVotes() && reference.isHasVotes()) {
+            checkAttendanceList(content,reference,observation);
+            checkVotesList(content,reference,observation);
+        }
+        return observation;
+    }
+
+    protected void checkModifiedDateTime(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation ) {
+        checkString(content.getModifiedDateTime().toString(), reference.getModifiedDateTime().toString(),observation, SpotCheckMismatchType.AGENDA_MODIFIED_DATE_TIME);
+    }
+
+    protected void checkHasVotes(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(String.valueOf(content.isHasVotes()), String.valueOf(reference.isHasVotes()), observation, SpotCheckMismatchType.AGENDA_HAS_VOTES);
+    }
+
+    protected void checkChair(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(content.getMeeting().getChair(), reference.getMeeting().getChair(), observation, SpotCheckMismatchType.AGENDA_CHAIR);
+    }
+
+    protected void checkLocation(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(content.getMeeting().getLocation(), content.getMeeting().getLocation(), observation, SpotCheckMismatchType.AGENDA_LOCATION);
+    }
+
+    protected void checkMeetingDateTime(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(content.getMeeting().getMeetingDateTime().toString(), reference.getMeeting().getMeetingDateTime().toString(),observation,SpotCheckMismatchType.AGENDA_MEETING_TIME);
+    }
+
+    protected void checkNotes(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(content.getMeeting().getNotes(), reference.getMeeting().getNotes(), observation, SpotCheckMismatchType.AGENDA_NOTES);
+    }
+
+    protected void checkBillListing(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(OutputUtils.toJson(content.getBills()),OutputUtils.toJson(reference.getBills()), observation, SpotCheckMismatchType.AGENDA_BILL_LISTING);
+    }
+
+    protected void checkAttendanceList(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(OutputUtils.toJson(content.getVoteInfo().getAttendanceList()),OutputUtils.toJson(reference.getVoteInfo().getAttendanceList()), observation, SpotCheckMismatchType.AGENDA_ATTENDANCE_LIST);
+    }
+
+    protected void checkVotesList(AgendaCommAddendumView content, AgendaCommAddendumView reference,SpotCheckObservation<CommitteeAgendaAddendumId> observation) {
+        checkString(OutputUtils.toJson(content.getVoteInfo().getVotesList()),OutputUtils.toJson(reference.getVoteInfo().getVotesList()), observation, SpotCheckMismatchType.AGENDA_VOTES_LIST);
     }
 }
--- src/main/java/gov/nysenate/openleg/service/spotcheck/openleg/OpenlegAgendaReportService.java
+++ src/main/java/gov/nysenate/openleg/service/spotcheck/openleg/OpenlegAgendaReportService.java
@@ -3,13 +3,13 @@ package gov.nysenate.openleg.service.spotcheck.openleg;
 import gov.nysenate.openleg.client.view.agenda.AgendaCommAddendumView;
 import gov.nysenate.openleg.client.view.agenda.AgendaView;
 import gov.nysenate.openleg.dao.agenda.reference.openleg.OpenlegAgendaDao;
+import gov.nysenate.openleg.dao.base.SortOrder;
 import gov.nysenate.openleg.dao.spotcheck.SpotCheckReportDao;
+import gov.nysenate.openleg.model.agenda.AgendaId;
 import gov.nysenate.openleg.model.agenda.CommitteeAgendaAddendumId;
 import gov.nysenate.openleg.model.base.SessionYear;
-import gov.nysenate.openleg.model.spotcheck.ReferenceDataNotFoundEx;
-import gov.nysenate.openleg.model.spotcheck.SpotCheckRefType;
-import gov.nysenate.openleg.model.spotcheck.SpotCheckReport;
-import gov.nysenate.openleg.model.spotcheck.SpotCheckReportId;
+import gov.nysenate.openleg.model.spotcheck.*;
+import gov.nysenate.openleg.service.agenda.data.AgendaDataService;
 import gov.nysenate.openleg.service.bill.data.BillDataService;
 import gov.nysenate.openleg.service.spotcheck.base.BaseSpotCheckReportService;
 import org.slf4j.Logger;
@@ -19,9 +19,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static gov.nysenate.openleg.model.spotcheck.SpotCheckMismatchType.OBSERVE_DATA_MISSING;
+import static gov.nysenate.openleg.model.spotcheck.SpotCheckMismatchType.REFERENCE_DATA_MISSING;
 
 @Service
 
@@ -36,6 +37,9 @@ public class OpenlegAgendaReportService extends BaseSpotCheckReportService<Commi
 
     @Autowired
     private OpenlegAgendaDao openlegAgendaDao;
+
+    @Autowired
+    private AgendaDataService agendaDataService;
 
     @Autowired
     private BillDataService billDataService;
@@ -66,17 +70,67 @@ public class OpenlegAgendaReportService extends BaseSpotCheckReportService<Commi
         logger.info("The current session year is " + SessionYear.of(start.getYear()));
 
         //Retrieve Openleg Ref Agenda data
-        List<AgendaView> referenceCalendarViews = openlegAgendaDao.getOpenlegAgendaView(String.valueOf(start.getYear()), apiSecret);
-        if (referenceCalendarViews.isEmpty()) {
+        List<AgendaView> referenceAgendaViews = openlegAgendaDao.getOpenlegAgendaView(String.valueOf(start.getYear()), apiSecret);
+        if (referenceAgendaViews.isEmpty()) {
             throw new ReferenceDataNotFoundEx("The collection of reference agendas with the given session year " + SessionYear.of(start.getYear()) + " is empty");
         }
 
         //Create Maps of content and source AgendaCommAddendumView
-        Map<CommitteeAgendaAddendumId, AgendaCommAddendumView> referenceAddendumViewMap = new HashMap<>();
         Map<CommitteeAgendaAddendumId, AgendaCommAddendumView> contentAddendumViewMap = new HashMap<>();
+        Map<CommitteeAgendaAddendumId, AgendaCommAddendumView> referenceAddendumViewMap = new HashMap<>();
 
+        //Get content data, transform it into an AgendaView then to a AgendaCommAddendumView and store in contentAddendumViewMap
+        for (AgendaId contentAgendaId : agendaDataService.getAgendaIds(start.getYear(), SortOrder.NONE) ) {
+          AgendaView contentAgendaView = new AgendaView(agendaDataService.getAgenda(contentAgendaId) ,billDataService );
+          putAgendaCommAddendumViewIntoMap(contentAgendaView , contentAddendumViewMap);
+        }
 
+        //Retrieve AgendaCommAddendumView from reference agenda views and store it in referenceAddendumViewMap
+        for(AgendaView refAgendaView: referenceAgendaViews) {
+            putAgendaCommAddendumViewIntoMap(refAgendaView , referenceAddendumViewMap);
+        }
+
+        //Put content Id's a set
+        Set<CommitteeAgendaAddendumId> remainingContentIds = new HashSet<>(contentAddendumViewMap.keySet());
+
+        //Find symmetric difference between content and reference id's
+        referenceAddendumViewMap.forEach((id, referenceAddendum) -> {
+            if (contentAddendumViewMap.containsKey(id)) {
+                //Both Calendars have the same Addendum item
+                AgendaCommAddendumView contentAddendum = contentAddendumViewMap.get(id);
+                SpotCheckObservation<CommitteeAgendaAddendumId> observation = checkService.check(contentAddendum, referenceAddendum);
+                report.addObservation(observation);
+            } else {
+                //add data missing
+                SpotCheckObservation<CommitteeAgendaAddendumId> sourceMissingObs = new SpotCheckObservation<>(reportId.getReferenceId(), id);
+                sourceMissingObs.addMismatch(new SpotCheckMismatch(OBSERVE_DATA_MISSING, id, "Missing Data from Openleg XML, ID:" + id.toString()));
+                report.addObservation(sourceMissingObs);
+            }
+            remainingContentIds.remove(id);
+        });
+
+        remainingContentIds.forEach(id -> {
+            // add ref missing
+            SpotCheckObservation<CommitteeAgendaAddendumId> refMissingObs = new SpotCheckObservation<>(reportId.getReferenceId(), id);
+            refMissingObs.addMismatch(new SpotCheckMismatch(REFERENCE_DATA_MISSING, id, "Missing Data from Openleg Ref, ID:" + id.toString()));
+            report.addObservation(refMissingObs);
+        });
+
+        logger.info("Found total number of " + report.getOpenMismatchCount(false) + " mismatches");
 
         return report;
+    }
+
+    private void putAgendaCommAddendumViewIntoMap(AgendaView agendaView,Map<CommitteeAgendaAddendumId, AgendaCommAddendumView> addendumViewMap) {
+        agendaView.getCommitteeAgendas()
+                .getItems()
+                .stream()
+                .forEach(
+                        agendaCommView -> agendaCommView.getAddenda()
+                                .getItems()
+                                .stream()
+                                .forEach(
+                                        agendaCommAddendumView ->
+                                                addendumViewMap.put(agendaCommAddendumView.getCommitteeAgendaAddendumId() , agendaCommAddendumView)));
     }
 }
