0831cdcfa72b0ded36feba0351a87c39606b6297
Anthony Calabrese <aacal050@gmail.com>
2017-09-22 12:26:09 -0400

    Roll back Agenda and Committee Ids

--- src/main/java/gov/nysenate/openleg/client/view/agenda/AgendaCommAddendumView.java
+++ src/main/java/gov/nysenate/openleg/client/view/agenda/AgendaCommAddendumView.java
@@ -2,12 +2,8 @@ package gov.nysenate.openleg.client.view.agenda;
 
 import gov.nysenate.openleg.client.view.base.ListView;
 import gov.nysenate.openleg.client.view.base.ViewObject;
-import gov.nysenate.openleg.model.agenda.AgendaId;
 import gov.nysenate.openleg.model.agenda.AgendaInfoCommittee;
 import gov.nysenate.openleg.model.agenda.AgendaVoteCommittee;
-import gov.nysenate.openleg.model.agenda.CommitteeAgendaAddendumId;
-import gov.nysenate.openleg.model.base.Version;
-import gov.nysenate.openleg.model.entity.CommitteeId;
 import gov.nysenate.openleg.service.bill.data.BillDataService;
 
 import java.time.LocalDateTime;
@@ -22,8 +18,6 @@ public class AgendaCommAddendumView implements ViewObject
     private AgendaMeetingView meeting;
     private ListView<AgendaItemView> bills;
     private AgendaVoteView voteInfo;
-    private AgendaId agendaId;
-    private CommitteeId committeeId;
 
     public AgendaCommAddendumView(String addendumId, LocalDateTime modDateTime, AgendaInfoCommittee infoComm,
                                   AgendaVoteCommittee voteComm, BillDataService billDataService) {
@@ -39,8 +33,6 @@ public class AgendaCommAddendumView implements ViewObject
             if (this.hasVotes) {
                 this.voteInfo = new AgendaVoteView(voteComm);
             }
-            this.agendaId = infoComm.getAgendaId();
-            this.committeeId = infoComm.getCommitteeId();
         }
     }
 
@@ -69,26 +61,6 @@ public class AgendaCommAddendumView implements ViewObject
 
     public AgendaVoteView getVoteInfo() {
         return voteInfo;
-    }
-
-    public AgendaId getAgendaId() {
-        return agendaId;
-    }
-
-    public void setAgendaId(AgendaId agendaId) {
-        this.agendaId = agendaId;
-    }
-
-    public CommitteeId getCommitteeId() {
-        return committeeId;
-    }
-
-    public void setCommitteeId(CommitteeId committeeId) {
-        this.committeeId = committeeId;
-    }
-
-    public CommitteeAgendaAddendumId getCommitteeAgendaAddendumId() {
-        return new CommitteeAgendaAddendumId(this.agendaId, this.committeeId, Version.of(this.addendumId) );
     }
 
     @Override
