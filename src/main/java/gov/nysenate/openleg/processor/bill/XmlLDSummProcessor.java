9ef80fe3ce01f36b960dbe3f4e4ac1f2ae6956b4
Anthony Calabrese <aacal050@gmail.com>
2017-11-27 10:04:56 -0500

    Replace law tab chars with a space character
    In an attempt to resolve the remaining spotcheck errors concerned bill
    law codes, I am now replacing all tabs with spaces, so any potential
    whitespace is ultimately being replaced with a single space char

--- src/main/java/gov/nysenate/openleg/processor/bill/XmlLDSummProcessor.java
+++ src/main/java/gov/nysenate/openleg/processor/bill/XmlLDSummProcessor.java
@@ -55,7 +55,7 @@ public class XmlLDSummProcessor extends AbstractDataProcessor implements SobiPro
             final String summary = xmlHelper.getNode("digestsummary/summary", doc) == null ? "" : xmlHelper.getNode("digestsummary/summary", doc).getTextContent().replaceAll("\n"," ").trim();
             final String amd = xmlHelper.getString("digestsummary/summaryamendment", doc);
             final Version version = Version.of(amd);
-            final String law = xmlHelper.getString("law", billTextNode).replaceAll("Â", "¶").replaceAll("º","§").replaceAll("\n"," ").replaceAll(" +"," ").trim();
+            final String law = xmlHelper.getString("law", billTextNode).replaceAll("Â", "¶").replaceAll("º","§").replaceAll("\n"," ").replaceAll("\t", " ").replaceAll(" +"," ").trim();
             final Bill baseBill = getOrCreateBaseBill(sobiFragment.getPublishedDateTime(), new BillId(billhse + billno, new SessionYear(sessionYear), version), sobiFragment);
             baseBill.setSummary(summary);
             baseBill.getAmendment(version).setLaw(law);
