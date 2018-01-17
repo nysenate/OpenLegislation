008beb330b8650ab03aa3fab6015259235ccf079
Sam Stouffer <samlthecaml@gmail.com>
2018-01-17 11:02:52 -0500

    Modify bill alt. pdf data, Add 2018 budget pdfs.
    This commit modifies the data persistence practices for alternate bill
    text pdfs and also adds pdfs for the 2018 budget bills.
    
    - Rename existing alternate pdf files to include session year in name.
    - Add alternate pdf database entries to openleg.data.sql.
    - Add patch to rename files in database and add 2018 budget entries.
    
    Fixes #11665

--- src/main/resources/sql/openleg.data.sql
+++ src/main/resources/sql/openleg.data.sql
@@ -61,6 +61,47 @@ J5165	2013	145	1	2015-03-03 16:30:28.243363	\N
 
 
 --
+-- Data for Name: bill_text_alternate_pdf; Type: TABLE DATA; Schema: master; Owner: postgres
+--
+COPY bill_text_alternate_pdf (bill_session_year, bill_print_no, bill_amend_version, active, url_path) FROM stdin;
+2015	S6400		t	/static/pdf/2015-S6400-A9000.pdf
+2015	A9000		t	/static/pdf/2015-S6400-A9000.pdf
+2015	S6401		t	/static/pdf/2015-S6401-A9001.pdf
+2015	A9001		t	/static/pdf/2015-S6401-A9001.pdf
+2015	S6402		t	/static/pdf/2015-S6402-A9002.pdf
+2015	A9002		t	/static/pdf/2015-S6402-A9002.pdf
+2015	S6403		t	/static/pdf/2015-S6403-A9003.pdf
+2015	A9003		t	/static/pdf/2015-S6403-A9003.pdf
+2015	S6404		t	/static/pdf/2015-S6404-A9004.pdf
+2015	A9004		t	/static/pdf/2015-S6404-A9004.pdf
+2017	A7067		t	/static/pdf/2017-A7067.pdf
+2017	A7068		t	/static/pdf/2017-A7068.pdf
+2017	S5491		t	/static/pdf/2017-S5491.pdf
+2017	S5492		t	/static/pdf/2017-S5492.pdf
+2017	S2000		t	/static/pdf/2017-S2000-A3000.pdf
+2017	A3000		t	/static/pdf/2017-S2000-A3000.pdf
+2017	S2001		t	/static/pdf/2017-S2001-A3001.pdf
+2017	A3001		t	/static/pdf/2017-S2001-A3001.pdf
+2017	S2002		t	/static/pdf/2017-S2002-A3002.pdf
+2017	A3002		t	/static/pdf/2017-S2002-A3002.pdf
+2017	S2003		t	/static/pdf/2017-S2003-A3003.pdf
+2017	A3003		t	/static/pdf/2017-S2003-A3003.pdf
+2017	S2004		t	/static/pdf/2017-S2004-A3004.pdf
+2017	A3004		t	/static/pdf/2017-S2004-A3004.pdf
+2017	S7500		t	/static/pdf/2017-S7500-A9500.pdf
+2017	S9500		t	/static/pdf/2017-S7500-A9500.pdf
+2017	S7501		t	/static/pdf/2017-S7501-A9501.pdf
+2017	S9501		t	/static/pdf/2017-S7501-A9501.pdf
+2017	S7502		t	/static/pdf/2017-S7502-A9502.pdf
+2017	S9502		t	/static/pdf/2017-S7502-A9502.pdf
+2017	S7503		t	/static/pdf/2017-S7503-A9503.pdf
+2017	S9503		t	/static/pdf/2017-S7503-A9503.pdf
+2017	S7504		t	/static/pdf/2017-S7504-A9504.pdf
+2017	S9504		t	/static/pdf/2017-S7504-A9504.pdf
+\.
+
+
+--
 -- Data for Name: committee; Type: TABLE DATA; Schema: master; Owner: postgres
 --
 
