b42a71e92fe68339af0d73c176486941682a1e3c
Sam Stouffer <samlthecaml@gmail.com>
2018-09-05 11:01:10 -0400

    Remove catch-and-log ex handling from MapViewSerializer

--- src/main/java/gov/nysenate/openleg/util/MapViewSerializer.java
+++ src/main/java/gov/nysenate/openleg/util/MapViewSerializer.java
@@ -16,12 +16,7 @@ public class MapViewSerializer extends JsonSerializer<MapView> {
     private static final Logger logger = LoggerFactory.getLogger(MapViewSerializer.class);
 
     @Override
-    public void serialize(MapView value, JsonGenerator gen, SerializerProvider serializers) {
-        try {
-            gen.writeObject(ListView.of(new ArrayList<ViewObject>(value.getItems().values())));
-        }
-        catch (IOException ex){
-            logger.warn("MapView not serialized.");
-        }
+    public void serialize(MapView value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeObject(ListView.of(new ArrayList<ViewObject>(value.getItems().values())));
     }
 }
