d4639efa3ea0d25f6fab623223eb18968db22b2b
Sam Stouffer <samlthecaml@gmail.com>
2019-02-08 13:36:19 -0500

    Convert 12/19 elastic api log sql script to flyway
    Converts the 12/19/18 sql script that drops the request and response
    tables to flyway.  This script was created in the elastic-api-log
    branch, which had not been merged into the dev branch when the existing
    scripts were converted to flyway.  The truncation step is removed and an
    'if exists' clause is added for compatibility with instances that have
    already upgraded to the latest dev code and run the script.
    Also, removing the temporary rules sponsor fix script.

--- src/main/resources/sql/patches/2018-12-19.remove_api_log_table.sql
+++
@@ -1,8 +0,0 @@
-
--- Removes the request and response tables in the public schema
--- Performs a TRUNCATE first to reclaim space.
-
-TRUNCATE public.response;
-DROP TABLE public.response;
-TRUNCATE public.request;
-DROP TABLE public.request;
