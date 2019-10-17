3693d76a504b8e881ed0e30d83c1fc34e3cd7191
Julien Cheng <chengj7@rpi.edu>
2019-10-17 17:29:04 -0400

    Add to BillTextUtils/Test: conversion to HTML5 with/without custom tags, final text, and text diff.
    .toHTML5(String)
    .toHTML5WithTags(String)
    .TextDiff
    .changesToFinalText(ArrayList<TextDiff>)
    .toChanges(String)
    
    private:
    .convertToHTML5(String, boolean)
    .specialMin(int, int)
    .addIndex(String)
    .removeIndex(String)
    .jsoupParsePreserveNewline(String)
    .leadingWhitespace(String)

--- src/main/java/gov/nysenate/openleg/util/BillTextUtils.java
+++ src/main/java/gov/nysenate/openleg/util/BillTextUtils.java
@@ -9,6 +9,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
 import org.jsoup.nodes.TextNode;
+import org.jsoup.safety.Whitelist;
 import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
@@ -272,4 +273,261 @@ public class BillTextUtils
             }
         }
     }
+
+    /**
+     * Converts a billtext HTML String to an HTML5 String. Retains the same formatting.
+     *
+     * @param rawHTML String
+     * @return HTML5 String
+     */
+    public static String toHTML5(String rawHTML) {
+        return convertToHTML5(rawHTML, true);
+    }
+
+    /**
+     * Converts a billtext HTML String to an HTML5 String with custom tags surrounding changed text.
+     *
+     * @param rawHTML String
+     * @return HTML5 String
+     */
+    public static String toHTML5WithTags(String rawHTML) {
+        final ArrayList<TextDiff> changes = toChanges(rawHTML);
+        String ans = "";
+        for (int i = 0; i < changes.size(); ++i) {
+            switch (changes.get(i).type) {
+                case 0:
+                    ans = ans + changes.get(i).html;
+                    break;
+                case 1:
+                    ans = ans + "<span class=\"ol-text-change ol-text-added\">" + changes.get(i).html + "</span>";
+                    break;
+                case -1:
+                    ans = ans + "<span class=\"ol-text-change ol-text-removed\">" + changes.get(i).html + "</span>";
+                    break;
+            }
+        }
+        ans = convertToHTML5(ans, false);
+        return ans;
+    }
+
+    /**
+     * TextDiff.type can be -1 (text removed), 0 (text unchanged), 1 (text added)
+     * TextDiff.text is the plain text
+     * TextDiff.html is the escaped html (contains HTML escape sequences)
+     */
+    public static class TextDiff {
+        public int type;//0 = same, 1 = add, -1 = remove
+        public String text;
+        public String html;
+        public TextDiff(final int type, final String text, final String html) {
+            this.type = type;
+            this.text = new String(text);
+            this.html = new String(html);
+        }
+
+        @Override
+        public String toString() {//meant for debug
+            return type + ": " + text + "\n" + html;
+        }
+    }
+
+    /**
+     * Converts ArrayList&lt;TextDiff&gt; to the final version, plain text.
+     *
+     * @param changes
+     * @return String
+     */
+    public static String changesToFinalText(final ArrayList<TextDiff> changes) {
+        String ans = "";
+        for (int i = 0; i < changes.size(); ++i) {
+            if (changes.get(i).type >= 0) ans = ans + changes.get(i).text;
+        }
+        return ans;
+    }
+
+    /**
+     * Accepts raw HTML string and converts it into a ArrayList&lt;TextDiff&gt;.
+     *
+     * @param rawHTML String
+     * @return ArrayList&lt;TextDiff&gt;
+     */
+    public static ArrayList<TextDiff> toChanges(final String rawHTML) {
+        ArrayList<TextDiff> ans = new ArrayList<TextDiff>();
+        String unformatted = new String(rawHTML);
+        if (unformatted.contains("</STYLE>")) unformatted = unformatted.substring(unformatted.indexOf("</STYLE>") + 8);
+        //unformatted = unformatted.trim();
+        int lastLength;
+        while (unformatted.length() > 0) {
+            lastLength = unformatted.length();
+            int[] toAdd = addIndex(unformatted);
+            int[] toRemove = removeIndex(unformatted);
+            int action = -1;//1 = add, 0 = remove
+            if (toAdd[0] == -1 && toRemove[0] == -1) {//no more text added or removed
+                ans.add(new TextDiff(0, unformatted, unformatted));
+                unformatted = "";
+            }
+            else {
+                if (toAdd[0] != -1 && toRemove[0] != -1) {
+                    if (toAdd[0] < toRemove[0]) action = 1;
+                    else action = 0;
+                }
+                else if (toAdd[0] != -1) action = 1;
+                else action = 0;
+            }
+            if (action == 1) {
+                ans.add(new TextDiff(0, unformatted.substring(0, toAdd[0]), unformatted.substring(0, toAdd[0])));
+                ans.add(new TextDiff(1, unformatted.substring(toAdd[1], toAdd[2]), unformatted.substring(toAdd[1], toAdd[2])));
+                unformatted = unformatted.substring(toAdd[3]);
+            }
+            else if (action == 0) {
+                ans.add(new TextDiff(0, unformatted.substring(0, toRemove[0]), unformatted.substring(0, toRemove[0])));
+                ans.add(new TextDiff(-1, unformatted.substring(toRemove[1], toRemove[2]), unformatted.substring(toRemove[1], toRemove[2])));
+                unformatted = unformatted.substring(toRemove[3]);
+            }
+            if (lastLength == unformatted.length()) {//unformatted remained the same 2 loop iterations in a row
+                //throw new RuntimeException("Unexpected loop break");//for testing purposes only
+                //expect to just append type 0 and continue on with execution instead of throwing exception
+                ans.add(new TextDiff(0, unformatted, unformatted));
+                unformatted = "";
+            }
+        }
+        for (int i = 0; i < ans.size(); ++i) {
+            ans.get(i).text = jsoupParsePreserveNewline(ans.get(i).text);
+            if (ans.get(i).text.equals("") && ans.get(i).html.equals("")) {
+                ans.remove(i--);
+            }
+        }
+        return ans;
+    }
+
+    /**
+     * Strips/Replaces legacy HTML formatting.
+     *
+     * @param html String
+     * @param style boolean
+     * @return String
+     */
+    private static String convertToHTML5(final String html, final boolean style) {
+        String html5 = new String(html);
+        html5 = html5.replace("<!--", "");
+        html5 = html5.replace("-->", "");
+        if (style) {
+            html5 = html5.replaceAll("<FONT SIZE=5>", "<SPAN STYLE=\"font-size: 18px;\">");
+            html5 = html5.replaceAll("</FONT>", "</SPAN>");
+            html5 = html5.replaceAll("(?i)<PRE WIDTH=\"[0-9]+\">", "<PRE>");
+            html5 = html5.replaceAll("(?i)<BASEFONT SIZE=[0-9]+>", "<STYLE> body {font-size: 12px;}</STYLE>");
+        }
+        else {
+            html5 = html5.replaceAll("<FONT SIZE=5>", "<SPAN class=\"ol-text-large\">");
+            html5 = html5.replaceAll("</FONT>", "</SPAN>");
+            html5 = html5.replaceAll("(?i)<PRE WIDTH=\"[0-9]+\">", "<PRE>");
+            html5 = html5.replaceAll("(?i)<BASEFONT SIZE=[0-9]+>", "<STYLE> body {font-size: 12px;}</STYLE>");
+            html5 = html5.replaceAll("(?i)<STYLE>[\\s\\S]*</STYLE>", "");//removes style tags
+        }
+        return html5;
+    }
+
+    /**
+     * Returns the minimum of a and b, indicating which of a or b was selected, with a default preference for b.
+     * Special case if both a and b are -1.
+     * Expects a and b to be >= -1.
+     *
+     * @param a int
+     * @param b int
+     * @return int[2], representing [minimum of a and b, which of a and b was selected]
+     */
+    private static int[] specialMin(final int a, final int b) {
+        if (a == -1 && b == -1) return new int[]{-1, 0};
+        else if (a == -1) return new int[]{b, 2};
+        else if (b == -1) return new int[]{a, 1};
+        else if (a < b) return new int[]{a, 1};
+        else if (b < a) return new int[]{b, 2};
+        else return new int[]{b, 2};
+    }
+
+    private static final String ADDTAGOPEN = "<B><U>";
+    private static final String ADDTAGCLOSE = "</U></B>";
+    /**
+     * Detects HTML text marked as an addition.
+     *
+     * @param text HTML String
+     * @return int[4], where int[0] is the index before the initial tags,
+     * int[1] is the index after the initial tags,
+     * int[2] is the index before the final tags,
+     * and int[3] is the index after the final tags.
+     */
+    private static int[] addIndex(final String text) {
+        final int startIndex = text.indexOf(ADDTAGOPEN);
+        int endIndex = text.indexOf(ADDTAGCLOSE);
+        int cpos = 0;
+        while (endIndex <= startIndex && endIndex != -1 && startIndex != -1) {
+            endIndex = text.indexOf(ADDTAGCLOSE, cpos);
+            cpos = endIndex + 8;
+        }
+        if (startIndex == -1 || endIndex == -1) return new int[]{-1, -1, -1, -1};
+        else return new int[]{startIndex, startIndex + ADDTAGOPEN.length(), endIndex, endIndex + ADDTAGCLOSE.length()};
+    }
+
+    private static final String REMOVETAGOPEN1 = "<B><S>";
+    private static final String REMOVETAGOPEN2 = "[<B><S>";
+    private static final String REMOVETAGCLOSE1 = "</S></B>";
+    private static final String REMOVETAGCLOSE2 = "</S></B>]";
+    /**
+     * Detects HTML text marked for removal.
+     *
+     * @param text HTML String
+     * @return int[4], where int[0] is the index before the initial tags,
+     * int[1] is the index after the initial tags,
+     * int[2] is the index before the final tags,
+     * and int[3] is the index after the final tags.
+     */
+    private static int[] removeIndex(final String text) {
+        final int[] start = specialMin(text.indexOf(REMOVETAGOPEN1), text.indexOf(REMOVETAGOPEN2));
+        int[] end = specialMin(text.indexOf(REMOVETAGCLOSE1), text.indexOf(REMOVETAGCLOSE2));
+        int cpos = 0;
+        while (end[0] <= start[0] && end[0] != -1 && start[0] != -1) {
+            end = specialMin(text.indexOf(REMOVETAGCLOSE1, cpos), text.indexOf(REMOVETAGCLOSE2, cpos));
+            cpos = end[0] + (end[1] == 2 ? REMOVETAGCLOSE2.length() : REMOVETAGCLOSE1.length());
+        }
+        if (start[0] == -1 || end[0] == -1) return new int[]{-1, -1, -1, -1};
+        else return new int[]{start[0],
+                start[0] + (start[1] == 2 ? REMOVETAGOPEN2.length() : REMOVETAGOPEN1.length()),
+                end[0],
+                end[0] + (end[1] == 2 ? REMOVETAGCLOSE2.length() : REMOVETAGCLOSE1.length())};
+    }
+
+    /**
+     * Converts html to plaintext while attempting to preserve all line breaks.
+     *
+     * @param html String
+     * @return String
+     */
+    private static String jsoupParsePreserveNewline(String html) {
+        if (html==null)
+            return html;
+        final String leadingNewlines = leadingWhitespace(html);
+        html = html.substring(leadingNewlines.length());
+        Document document = Jsoup.parse(html);
+        document.outputSettings(new Document.OutputSettings().prettyPrint(false));//makes html() preserve linebreaks and spacing
+        document.select("br").append("\\n");
+        document.select("p").prepend("\\n\\n");
+        String s = document.html().replaceAll("\\\\n", "\n");
+        return leadingNewlines + Jsoup.clean(s, "", Whitelist.none(), new Document.OutputSettings().prettyPrint(false));
+    }
+
+    /**
+     * Returns the leading whitespace characters of a String.
+     *
+     * @param x String
+     * @return String
+     */
+    private static String leadingWhitespace(final String x) {
+        for (int i = 0; i < x.length(); ++i) {
+            if (x.charAt(i) != '\n' && x.charAt(i) != ' ') {
+                return x.substring(0, i);
+            }
+        }
+        return x;
+    }
+
 }
--- src/test/java/gov/nysenate/openleg/processor/bill/text/BillTextUtilsTest.java
+++ src/test/java/gov/nysenate/openleg/processor/bill/text/BillTextUtilsTest.java
@@ -1,14 +1,43 @@
 package gov.nysenate.openleg.processor.bill.text;
 
 import gov.nysenate.openleg.annotation.UnitTest;
+import gov.nysenate.openleg.model.bill.Bill;
 import gov.nysenate.openleg.util.BillTextUtils;
+import gov.nysenate.openleg.util.XmlHelper;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 @Category(UnitTest.class)
 public class BillTextUtilsTest {
+    final String bill = "\n<STYLE><!--U  {color: Green}S  {color: RED} I  {color: DARKBLUE; background-color:yellow}\n" +
+            "        P.brk {page-break-before:always}--></STYLE>\n" +
+            "<BASEFONT SIZE=3>\n" +
+            "<PRE WIDTH=\"136\">\n" +
+            "\n" +
+            "<FONT SIZE=5><B>                STATE OF NEW YORK</B></FONT>\n" +
+            "                ________________________________________________________________________\n" +
+            "        155\n" +
+            "        2019-2020 Regular Sessions\n" +
+            "\n" +
+            "<FONT SIZE=5><B>                    IN SENATE</B></FONT>\n" +
+            "                                       <B><U>(Prefiled)</U></B>\n" +
+            "!!text has been abridged for test purposes!!\n" +
+            "                EXPLANATION--Matter in <B><U>italics</U></B> (underscored) is new; matter in brackets\n" +
+            "                [<B><S>removed text</S></B>] is old law to be omitted.\n" +
+            "        LBD04958-01-9\n";
+
 
     @Test
     public void senateBillTextFormatTest() {
@@ -131,4 +160,141 @@ public class BillTextUtilsTest {
         String actual = BillTextUtils.formatHtmlExtractedResoText(lrsText);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void resolutionParse() {
+        String input = "\n<PRE WIDTH=\"200\">\n" +
+                "\n" +
+                "Senate Resolution No. 485\n" +
+                "\n" +
+                "<B>BY:</B> Senator BROOKS\n" +
+                "\n" +
+                "<B>CONGRATULATING</B>  Maureen A. Griffin-Damone upon the\n" +
+                "occasion  of  being  crowned  Ms.  New  York  Senior\n" +
+                "America on July 29, 2018\n" +
+                "!!text has been abridged for test purposes!!\n" +
+                "</pre>\n";
+        String output = BillTextUtils.toHTML5(input);
+        //System.out.println("Tagless: " + output);
+        assertEquals(-1, output.indexOf("<PRE WIDTH"));
+        assertNotEquals(output.indexOf("!!text has been abridged for test purposes!!"), -1);
+        String outputWithTags = BillTextUtils.toHTML5WithTags(input);
+        //System.out.println("With tags: " + outputWithTags);
+        assertEquals(-1, outputWithTags.indexOf("<PRE WIDTH"));
+        assertNotEquals(outputWithTags.indexOf("!!text has been abridged for test purposes!!"), -1);
+    }
+
+    @Test
+    public void billParse() {
+        String output = BillTextUtils.toHTML5(bill);
+        //System.out.println("Tagless: " + output);
+        assertEquals(-1, output.indexOf("<PRE WIDTH"));
+        assertEquals(-1, output.indexOf("<FONT"));
+        assertNotEquals(output.indexOf("!!text has been abridged for test purposes!!"), -1);
+        String outputWithTags = BillTextUtils.toHTML5WithTags(bill);
+        //System.out.println("With tags: " + outputWithTags);
+        assertEquals(-1, outputWithTags.indexOf("<PRE WIDTH"));
+        assertEquals(-1, outputWithTags.indexOf("<FONT"));
+        assertNotEquals(outputWithTags.indexOf("!!text has been abridged for test purposes!!"), -1);
+        assertNotEquals(outputWithTags.indexOf("ol-text-added"), -1);
+        assertNotEquals(outputWithTags.indexOf("ol-text-removed"), -1);
+    }
+
+    @Test
+    public void billParseChanges() {
+        ArrayList<BillTextUtils.TextDiff> changes = BillTextUtils.toChanges(bill);
+        //for (int i = 0; i < changes.size(); ++i) System.out.println(changes.get(i));
+        assertEquals(7, changes.size());
+        String plain = BillTextUtils.parseHTMLtext(bill).trim();
+        String alsoPlain = BillTextUtils.changesToFinalText(changes).trim();
+        assertNotEquals(plain, alsoPlain);//they're not supposed to be the same
+        assertEquals(-1, alsoPlain.indexOf("removed text"));
+    }
+
+    @Test
+    public void textDiff() {
+        BillTextUtils.TextDiff a = new BillTextUtils.TextDiff(0, "plain", "<B><U>plain</U></B>");
+        assertEquals(0, a.type);
+        assertEquals("plain", a.text);
+        assertEquals("<B><U>plain</U></B>", a.html);
+    }
+
+    @Test
+    public void exitSuccessOnBrokenTags() {
+        ArrayList<BillTextUtils.TextDiff> changes = BillTextUtils.toChanges("<B><U>test</S></B>");
+        String result = BillTextUtils.changesToFinalText(changes);
+        //System.out.println(result);
+        changes = BillTextUtils.toChanges("[<B><S>test</U></B>");
+        result = BillTextUtils.changesToFinalText(changes);
+        //System.out.println(result);
+        changes = BillTextUtils.toChanges("</S></B>]test[<B><S>");
+        result = BillTextUtils.changesToFinalText(changes);
+        //System.out.println(result);
+    }
+
+    @Test
+    public void compareWithPOC() throws IOException, ParserConfigurationException, SAXException, XPathExpressionException {
+        String[] files = {"src/test/resources/processor/bill/text/2017-01-01-00.00.00.000000_BILLTEXT_S09999.XML",
+                            "src/test/resources/processor/bill/text/2017-02-09-14.23.24.464033_BILLTEXT_A05457.XML"};
+        for (int i = 0; i < files.length; ++i) {
+            String contents = new String(Files.readAllBytes(Paths.get(files[i])));
+            XmlHelper xmlHelper = new XmlHelper();
+            Node a = xmlHelper.parse(contents);
+            String raw = xmlHelper.getString("/billtext_html", a);
+            //System.out.println(raw);
+            String html5 = BillTextUtils.toHTML5(raw);
+            ArrayList<BillTextUtils.TextDiff> changes = BillTextUtils.toChanges(raw);
+            //System.out.println(html5);
+            String html5Tags = BillTextUtils.toHTML5WithTags(raw);
+            String finalText = BillTextUtils.changesToFinalText(changes);
+            assertNotEquals(-1, raw.indexOf("FONT"));
+            assertEquals(-1, html5.indexOf("FONT"));
+            assertEquals(-1, html5Tags.indexOf("FONT"));
+            assertNotEquals(-1, html5Tags.indexOf("ol-text"));
+            assertNotEquals(-1, raw.indexOf("STATE OF NEW YORK"));
+            assertNotEquals(-1, html5.indexOf("STATE OF NEW YORK"));
+            assertNotEquals(-1, html5Tags.indexOf("STATE OF NEW YORK"));
+        }
+        /*
+        Files.write(Paths.get("/home/aa/btp/j_output/html5.html"), html5.getBytes());
+        Files.write(Paths.get("/home/aa/btp/j_output/html5_tags.html"), html5Tags.getBytes());
+        Files.write(Paths.get("/home/aa/btp/j_output/final.txt"), finalText.getBytes());*/
+    }
+
+    /**
+     * requires BillTextUtils.jsoupParsePreserveNewline() to be public access
+     */
+    /*
+    @Test
+    public void plainTextOutput() {
+        assertEquals("", BillTextUtils.jsoupParsePreserveNewline(""));
+        assertEquals("", BillTextUtils.jsoupParsePreserveNewline("<B></B>"));
+        assertEquals("\n", BillTextUtils.jsoupParsePreserveNewline("\n<B></B>"));
+        assertEquals("\n", BillTextUtils.jsoupParsePreserveNewline("<B></B>\n"));
+        assertEquals("\n\n", BillTextUtils.jsoupParsePreserveNewline("\n\n<B></B>"));
+        assertEquals("\n \n", BillTextUtils.jsoupParsePreserveNewline("\n \n<B></B>"));
+        assertEquals(" \n", BillTextUtils.jsoupParsePreserveNewline(" \n"));
+        assertEquals("\n \n", BillTextUtils.jsoupParsePreserveNewline("\n \n"));
+        assertEquals("\nA\n", BillTextUtils.jsoupParsePreserveNewline("\nA\n"));
+        assertEquals("abcd", BillTextUtils.jsoupParsePreserveNewline("abcd"));
+        assertEquals("§", BillTextUtils.jsoupParsePreserveNewline("&#167;"));
+    }*/
+
+    /**
+     * requires BillTextUtils.leadingWhitespace() to be public access
+     */
+    /*
+    @Test
+    public void leadingWhitespace() {
+        assertEquals("", BillTextUtils.leadingWhitespace(""));
+        assertEquals("", BillTextUtils.leadingWhitespace("<B></B>"));
+        assertEquals("\n", BillTextUtils.leadingWhitespace("\n<B></B>"));
+        assertEquals("", BillTextUtils.leadingWhitespace("<B></B>\n"));
+        assertEquals("\n\n", BillTextUtils.leadingWhitespace("\n\n<B></B>"));
+        assertEquals("\n \n", BillTextUtils.leadingWhitespace("\n \n<B></B>"));
+        assertEquals(" \n", BillTextUtils.leadingWhitespace(" \n"));
+        assertEquals("\n \n", BillTextUtils.leadingWhitespace("\n \n"));
+        assertEquals("", BillTextUtils.leadingWhitespace("abcd"));
+        assertEquals("", BillTextUtils.leadingWhitespace("&#167;"));
+    }*/
 }
