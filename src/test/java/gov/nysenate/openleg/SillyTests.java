e66d375a5ae5df5e1dd6a67f3d575a5d0ea7359d
Jacob Keegan <jacobmkeegan@gmail.com>
2020-06-25 14:24:05 -0400

    Create SillyTests.java
    Adding back in this file, which is not tracked by Git.

---
+++ src/test/java/gov/nysenate/openleg/SillyTests.java
@@ -0,0 +1,31 @@
+package gov.nysenate.openleg;
+
+import gov.nysenate.openleg.annotation.SillyTest;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(SillyTest.class)
+public class SillyTests extends BaseTests {
+
+    /** --- Your silly tests go here --- */
+
+    /*
+                      __     __,
+                      \,`~"~` /
+      .-=-.           /    . .\
+     / .-. \          {  =    Y}=
+    (_/   \ \          \      /
+           \ \        _/`'`'`b
+            \ `.__.-'`        \-._
+             |            '.__ `'-;_
+             |            _.' `'-.__)
+              \    ;_..--'/     //  \
+              |   /  /   |     //    |
+              \  \ \__)   \   //    /
+               \__)        './/   .'
+                             `'-'`
+    */
+
+    @Test
+    public void sillyTest() {}
+}
