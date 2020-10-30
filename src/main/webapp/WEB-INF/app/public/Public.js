0cff803a0b534801bb52a99e4e5f23aa5f05fd1a
Kevin Caseiras <kcaseiras@gmail.com>
2020-10-30 15:57:48 -0400

    WIP testing out rem and public home header

--- src/main/webapp/WEB-INF/app/public/Public.js
+++ src/main/webapp/WEB-INF/app/public/Public.js
@@ -1,21 +1,70 @@
 import React from 'react'
-import '../app.scss'
+import styled from 'styled-components'
+import PublicHero from "./PublicHero";
+import Card from "../common/Card"
 
-export default function Public () {
+const About = styled(Card)`
+  position: relative;
+  max-width: 96rem;
+  padding: 5rem 3rem;
+  margin: -10rem auto 0 auto;
+  font-weight: 400;
+  font-size: 1.9rem;
+  text-align: center;
+  
+  & > a {
+    color: #008cba;
+  }
+`
+
+const BrowseCard = styled(Card)`
+  margin: 5rem auto 0 auto;
+  padding: 2rem 2rem;
+  max-width: 96rem;
+  
+  & > header > h3 {
+    margin: 0 auto 2rem 0;
+    font-size: 2rem;
+    text-align: center;
+  }
+  
+  & > p {
+    margin: 1rem 0;
+    text-align: center;
+    font-weight: 300;
+    font-size: 1.9rem;
+  }
+`
+
+const HomePage = styled.div`
+  background: #f1f1f1;
+  overflow: auto;
+`
+
+export default function Public() {
   return (
-    <div className={'public-home-page'}>
-      <section className={'hero-container-public'}>
-        <h1>
-          <img src="/static/img/nys_logo224x224.png"/>Open Legislation
-        </h1>
-      </section>
-      <div className="pop-out-container">
-        <div className="project-desc">
-          Developed in-house at the <a href="http://www.nysenate.gov">New York State Senate</a>, Open Legislation is a
-          public web service that provides access
-          to NYS legislative data from the Legislative Bill Drafting Commission system through a JSON API.
-        </div>
-      </div>
-    </div>
+    <HomePage>
+      <header>
+        <PublicHero/>
+      </header>
+      <main>
+        <About>
+          Developed in-house at the <a href="http://www.nysenate.gov">New York State Senate</a>,
+          Open Legislation is a public web service that provides access to NYS legislative data
+          from the Legislative Bill Drafting Commission system through a JSON API.
+        </About>
+        <BrowseCard>
+          <header>
+            <h3>Browse Legislation</h3>
+          </header>
+          <p>
+            We have an Open Legislation interface that we use to assist in our development.
+          </p>
+          <p>
+            Feel free to access it by using your API Key.
+          </p>
+        </BrowseCard>
+      </main>
+    </HomePage>
   )
 }
