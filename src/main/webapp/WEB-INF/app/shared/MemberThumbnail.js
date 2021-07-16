c5bb3614677d0bbb9462f6f81f84eade2d17241d
Kevin Caseiras <kcaseiras@gmail.com>
2021-07-16 15:25:37 -0400

    Fix missing member images taking up lots of width

--- src/main/webapp/WEB-INF/app/shared/MemberThumbnail.js
+++ src/main/webapp/WEB-INF/app/shared/MemberThumbnail.js
@@ -4,7 +4,7 @@ export default function MemberThumbnail({ member }) {
   const imgName = member ? member.imgName : "no_image.jpg"
   const alt = (member && member.fullName) ? member.fullName : "Rules Committee"
   return (
-    <img className="h-20 md:h-24 mr-3"
+    <img className="w-12 md:w-16 mr-3"
          alt={`Member ${alt}`}
          src={`/static/img/business_assets/members/mini/${imgName}`} />
   )
