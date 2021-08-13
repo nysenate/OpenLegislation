bfcadd51273f89f7906c51ca8fdf4ffc6d73fe53
Kevin Caseiras <kcaseiras@gmail.com>
2021-08-13 15:48:15 -0400

    Accordion style improvements

--- src/main/webapp/WEB-INF/app/shared/Accordion.js
+++ src/main/webapp/WEB-INF/app/shared/Accordion.js
@@ -7,12 +7,15 @@ import {
 
 export default function Accordion({ title, children }) {
   const [ isOpen, setIsOpen ] = React.useState(false);
-
+  let headerClass = "flex justify-between items-center p-3 rounded cursor-pointer hover:bg-gray-100";
+  headerClass += isOpen ? " bg-gray-100" : ""
   return (
     <div className="border-1 border-gray-300 rounded">
-      <div className="flex justify-between items-center p-3 rounded cursor-pointer hover:bg-gray-100"
+      <div className={headerClass}
            onClick={() => setIsOpen((open) => !open)}>
+        <span className={isOpen ? "text-blue-600 bg-gray-100" : ""}>
         {title}
+        </span>
         <div>
           {isOpen
             ? <CaretDown size="1.25rem" />
