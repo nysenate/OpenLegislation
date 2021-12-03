955f814a2dcd8cfc4c2c495a28fae1fe03738ba4
Kevin Caseiras <kcaseiras@gmail.com>
2021-12-03 13:53:12 -0500

    Allow viewing of ui from whitelisted ip addresses

--- src/main/java/gov/nysenate/openleg/api/auth/PermissionCtrl.java
+++ src/main/java/gov/nysenate/openleg/api/auth/PermissionCtrl.java
@@ -5,22 +5,31 @@ import gov.nysenate.openleg.api.response.BaseResponse;
 import gov.nysenate.openleg.api.response.ViewObjectResponse;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping(BaseCtrl.BASE_API_PATH + "/auth/permission")
 public class PermissionCtrl extends BaseCtrl {
 
+    @Value("${api.auth.ip.whitelist}") private String ipWhitelist;
+
     @RequestMapping(value = "/check", method = RequestMethod.GET)
-    public BaseResponse checkPermission(@RequestParam String permission) {
+    public BaseResponse checkPermission(@RequestParam String permission, HttpServletRequest request) {
         Subject subject = SecurityUtils.getSubject();
-        if (subject.isPermitted(permission)) {
+        // Allow browsing of the UI without logging in from whitelisted ip addresses.
+        if (permission.equals("ui:view") && request.getRemoteAddr().matches(ipWhitelist)) {
             return new ViewObjectResponse<>(new PermissionView(permission, true, subject.isAuthenticated()));
         }
 
+        if (subject.isPermitted(permission)) {
+            return new ViewObjectResponse<>(new PermissionView(permission, true, subject.isAuthenticated()));
+        }
         return new ViewObjectResponse<>(new PermissionView(permission, false, subject.isAuthenticated()));
     }
 }
