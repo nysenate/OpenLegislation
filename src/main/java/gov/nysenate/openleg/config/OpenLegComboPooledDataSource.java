c4d84c2a1a6da6600788a1fa395aec5bc74e5317
Jacob Keegan <jacobmkeegan@gmail.com>
2022-01-28 01:26:30 -0500

    More directly targeted a Thread that was still running at shutdown.

--- src/main/java/gov/nysenate/openleg/config/OpenLegComboPooledDataSource.java
+++ src/main/java/gov/nysenate/openleg/config/OpenLegComboPooledDataSource.java
@@ -4,6 +4,7 @@ import com.mchange.v2.c3p0.AbstractComboPooledDataSource;
 
 import javax.naming.Referenceable;
 import java.io.*;
+import java.util.Optional;
 
 /**
  * This class is copied nearly exactly from ComboPooledDataSource, except with an overridden close method.
@@ -30,15 +31,21 @@ final class OpenLegComboPooledDataSource extends AbstractComboPooledDataSource i
     }
 
     /**
-     * Intercepts an error thrown deep within the stack trace at shutdown.
-     * This error warns of a memory leak, but since we're shutting down,
-     * it can be safely ignored.
+     * There is one particular thread created by the superclass' close() that may not close before
+     * shutdown, which would cause an error message to be printed. This finds that thread,
+     * called in com.mchange.v2.resourcepool.BasicResourcePool.close(), and waits for it to finish.
      */
     @Override
     public void close() {
-        try {
-            super.close();
+        String problemThreadName = "Resource Destroyer in BasicResourcePool.close()";
+        super.close();
+        Optional<Thread> problemThread = Thread.getAllStackTraces().keySet().stream()
+                .filter(thread -> thread.getName().equals(problemThreadName)).findFirst();
+        if (problemThread.isPresent()) {
+            try {
+                problemThread.get().join();
+            }
+            catch (InterruptedException ignored) {}
         }
-        catch (Exception ignored) {}
     }
 }
