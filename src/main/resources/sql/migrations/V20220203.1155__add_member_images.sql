d09ceb5a2837f5b5404eef3602b00ccccb076808
Kevin Caseiras <kcaseiras@gmail.com>
2022-02-03 12:21:24 -0500

    Add missing Senator member images (and Eddie Gibbs)

---
+++ src/main/resources/sql/migrations/V20220203.1155__add_member_images.sql
@@ -0,0 +1,27 @@
+UPDATE public.person SET img_name = '735_Fredrick_Akshar.jpg' WHERE id = 735;
+
+UPDATE public.person SET img_name = '924_Jamaal_Bailey.jpg' WHERE id = 924;
+
+UPDATE public.person SET img_name = '1311_Jabari_Brisport.jpg' WHERE id = 1311;
+
+UPDATE public.person SET img_name = '1317_Samra_Brouk.jpg' WHERE id = 1317;
+
+UPDATE public.person SET img_name = '1318_Jeremy_Cooney.jpg' WHERE id = 1318;
+
+UPDATE public.person SET img_name = '1314_Michelle_Hinchey.jpg' WHERE id = 1314;
+
+UPDATE public.person SET img_name = '1315_John_Mannion.jpg' WHERE id = 1315;
+
+UPDATE public.person SET img_name = '1313_Mike_Martucci.jpg' WHERE id = 1313;
+
+UPDATE public.person SET img_name = '1309_Mario_Mattera.jpg' WHERE id = 1309;
+
+UPDATE public.person SET img_name = '1316_Peter_Oberacker.jpg' WHERE id = 1316;
+
+UPDATE public.person SET img_name = '1319_Edward_Rath.jpg' WHERE id = 1319;
+
+UPDATE public.person SET img_name = '1312_Elijah_Reichlin-Melnick.jpg' WHERE id = 1312;
+
+UPDATE public.person SET img_name = '1310_Alexis_Weik.jpg' WHERE id = 1310;
+
+UPDATE public.person SET img_name = '1505_Eddie_Gibbs.jpg' WHERE id = 1322;
