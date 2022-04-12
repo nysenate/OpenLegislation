6c80237c3ffcf9286811eb240fb9dc41b0cecf8e
Kevin Caseiras <kcaseiras@gmail.com>
2022-04-12 10:51:17 -0400

    Create the manage notifications page

--- src/main/webapp/WEB-INF/app/apis/adminAccountApi.js
+++ src/main/webapp/WEB-INF/app/apis/adminAccountApi.js
@@ -8,6 +8,19 @@ export function changePassword(newPassword) {
   return fetchUrl(url, options)
 }
 
+export function fetchUserNotificationSubscriptions() {
+  const url = "/api/3/admin/notifications/subscriptions"
+  return fetchUrl(url)
+}
+
+export function unsubscribeFromNotification(subscriptionId) {
+  const url = `/api/3/admin/notifications/unsubscribe/${subscriptionId}`
+  const options = {
+    method: "DELETE"
+  }
+  return fetchUrl(url, options)
+}
+
 async function fetchUrl(url, options) {
   const response = await fetch(url, options)
   const data = await response.json()
--- src/main/webapp/WEB-INF/app/views/admin/accounts/ManageNotifications.js
+++ src/main/webapp/WEB-INF/app/views/admin/accounts/ManageNotifications.js
@@ -1,10 +1,73 @@
 import React from "react"
+import {
+  fetchUserNotificationSubscriptions,
+  unsubscribeFromNotification
+} from "app/apis/adminAccountApi";
+import { Trash } from "phosphor-react";
 
 
-export default function ManageNotifications({setHeaderText}) {
+export default function ManageNotifications() {
+  const [ subscriptions, setSubscriptions ] = React.useState([])
+
+  React.useEffect(() => {
+    getUserSubscriptions()
+  }, [])
+
+  const getUserSubscriptions = () => {
+    fetchUserNotificationSubscriptions()
+      .then((res) => setSubscriptions(res.result.items))
+  }
+
+  const unsubscribe = (subscription) => {
+    unsubscribeFromNotification(subscription.id)
+      .then(() => getUserSubscriptions())
+  }
+
   return (
-    <div>
+    <div className="p-3">
       Manage notifications
+      <hr />
+      <CurrentSubscriptions subscriptions={subscriptions} onUnsubscribe={unsubscribe} />
     </div>
+  )
+}
+
+function CurrentSubscriptions({ subscriptions, onUnsubscribe }) {
+  return (
+    <div className="my-6">
+      <h3 className="h4">Current Notification Subscriptions</h3>
+      <table className="table table--stripe w-full my-3">
+        <thead>
+        <tr>
+          <th>Notification Type</th>
+          <th>Target Type</th>
+          <th>Target Address</th>
+          <th></th>
+          <th></th>
+        </tr>
+        </thead>
+        <tbody>
+        {subscriptions.map((sub) => (
+          <tr key={sub.id}>
+            <td>{sub.type}</td>
+            <td>{sub.target}</td>
+            <td>{sub.address}</td>
+            <td>
+              <DeleteIcon onUnsubscribe={() => onUnsubscribe(sub)} />
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+function DeleteIcon({ onUnsubscribe }) {
+  return (
+    <Trash onClick={() => onUnsubscribe()}
+           className="text-red-600 hover:cursor-pointer inline mr-3"
+           size="1.2rem"
+           weight="bold" />
   )
 }
