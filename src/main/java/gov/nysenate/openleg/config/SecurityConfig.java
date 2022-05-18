91cb6446c05fa4081784ccbbf97ed58138fe5532
Jacob Keegan <jacobmkeegan@gmail.com>
2022-05-18 19:11:00 -0400

    Removed an unused security class.

--- src/main/java/gov/nysenate/openleg/config/SecurityConfig.java
+++ src/main/java/gov/nysenate/openleg/config/SecurityConfig.java
@@ -2,28 +2,17 @@ package gov.nysenate.openleg.config;
 
 import org.apache.shiro.cache.MemoryConstrainedCacheManager;
 import org.apache.shiro.config.Ini;
-import org.apache.shiro.realm.Realm;
 import org.apache.shiro.spring.LifecycleBeanPostProcessor;
 import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 
-import java.util.List;
-
 @Configuration
-public class SecurityConfig
-{
-    private static final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-
-    @Autowired protected List<Realm> realms;
-
+public class SecurityConfig {
     /**
      * Shiro Filter factory that sets up the url authentication mechanism and applies the security
      * manager instance.
@@ -32,7 +21,8 @@ public class SecurityConfig
     public ShiroFilterFactoryBean shiroFilter() {
         ShiroFilterFactoryBean shiroFilter = new ShiroFilterFactoryBean();
         shiroFilter.setSecurityManager(securityManager());
-        shiroFilter.setFilterChainDefinitionMap(shiroIniConfig().getSection("urls"));
+        shiroFilter.setFilterChainDefinitionMap(Ini.fromResourcePath("classpath:shiro.ini")
+                .getSection("urls"));
         return shiroFilter;
     }
 
@@ -63,20 +53,12 @@ public class SecurityConfig
     }
 
     /**
-     * Configures the ShiroCachingService security manager with the instance of the active realm.
+     * Configures the security manager with the instance of the active realm.
      */
     @Bean(name = "securityManager")
     public DefaultWebSecurityManager securityManager() {
         DefaultWebSecurityManager defaultWebSecurityManager = new DefaultWebSecurityManager();
         defaultWebSecurityManager.setCacheManager(new MemoryConstrainedCacheManager());
         return defaultWebSecurityManager;
-    }
-
-    /**
-     * Exposes the ShiroCachingService.ini configuration file as an Ini instance that is consumed by the
-     * security filter manager when setting up the filter chains.
-     */
-    public Ini shiroIniConfig() {
-        return Ini.fromResourcePath("classpath:shiro.ini");
     }
 }
