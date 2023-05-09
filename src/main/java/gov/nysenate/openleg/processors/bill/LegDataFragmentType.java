9bbf68f230bc34040edc8b68cef591a9a11b09ab
Jacob Keegan <jacobmkeegan@gmail.com>
2023-05-09 01:50:52 -0400

    Added stubbed out code for new file type.

--- src/main/java/gov/nysenate/openleg/processors/bill/LegDataFragmentType.java
+++ src/main/java/gov/nysenate/openleg/processors/bill/LegDataFragmentType.java
@@ -29,7 +29,8 @@ public enum LegDataFragmentType
     SAMEAS          (true, "<sameas .+", "</sameas>.+"),                          // Same as
     SENMEMO         (true, "<senate_billmemo .+", "</senate_billmemo.+"),         // Memo
     VETOMSG         (true, "<veto_message .+", "</veto_message.+"),               // Veto memo
-    SENFLVOTE       (true, "<senfloorvote .+","</senfloorvote>.+");
+    SENFLVOTE       (true, "<senfloorvote .+","</senfloorvote>.+"),
+    SENFLATD        (true, "<senfloorattd .+", "</senfloorattd>.+");
 
     boolean isXml;
     String startPattern;
