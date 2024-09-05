f11f82c4604dc6b5ce4a1abe101eda4d2b16528a
JacobKeegan <jacobmkeegan@gmail.com>
2024-09-04 20:48:54 -0400

    Fixed a bug serializing empty Strings as keys.

--- src/main/java/gov/nysenate/openleg/common/util/OutputUtils.java
+++ src/main/java/gov/nysenate/openleg/common/util/OutputUtils.java
@@ -1,14 +1,16 @@
 package gov.nysenate.openleg.common.util;
 
 import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.StdKeySerializers;
 import com.fasterxml.jackson.datatype.guava.GuavaModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * OutputUtils serves as a simple utility class to convert Objects to string representations.
@@ -21,7 +23,8 @@ public final class OutputUtils {
             .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
             .registerModule(new GuavaModule()).registerModule(new JavaTimeModule());
     public static final ObjectMapper elasticsearchJsonMapper = basicJsonMapper.copy()
-            .disable(SerializationFeature.INDENT_OUTPUT);
+            .disable(SerializationFeature.INDENT_OUTPUT)
+            .registerModule(new SimpleModule().addKeySerializer(String.class, new ElasticsearchStringSerializer()));
     public static final ObjectMapper failOnUnknownMapper = basicJsonMapper.copy()
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
@@ -46,5 +49,14 @@ public final class OutputUtils {
             logger.error("ObjectMapper exception: {}", ex.getMessage());
         }
         return "";
+    }
+
+    private static class ElasticsearchStringSerializer extends StdKeySerializers.StringKeySerializer {
+        @Override
+        public void serialize(Object value, JsonGenerator g, SerializerProvider provider) throws IOException {
+            var strValue = ((String) value);
+            // Elasticsearch does not support empty Strings as keys.
+            g.writeFieldName(strValue.isEmpty() ? "BLANK" : strValue);
+        }
     }
 }
