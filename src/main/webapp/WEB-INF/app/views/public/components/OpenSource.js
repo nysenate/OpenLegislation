3db53a399f53175bcb6f9b75257b45ad852225d9
JacobKeegan <jacobmkeegan@gmail.com>
2026-06-09 11:10:15 -0400

    Fixed the publicly-listed contact email.

--- src/main/webapp/WEB-INF/app/views/public/components/OpenSource.js
+++ src/main/webapp/WEB-INF/app/views/public/components/OpenSource.js
@@ -13,7 +13,7 @@ export default function OpenSource() {
         <p className="text text-left md:text-center">
           The source code <a href="http://www.github.com/nysenate/OpenLegislation" className="link">is published on GitHub</a>.
           Feel free to open any tickets with issues you are having or contact the development
-          team at <a href="mailto:senatedev@nysenate.gov" className="link">senatedev@nysenate.gov</a>.
+          team at <a href="mailto:openlegislation@nysenate.gov" className="link">openlegislation@nysenate.gov</a>.
         </p>
       </address>
     </section>
