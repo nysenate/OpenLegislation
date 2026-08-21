3cbb984864bc0f26a7db2c6fee9d0c8df10482d4
JacobKeegan <jacobmkeegan@gmail.com>
2026-08-21 19:17:44 -0400

    Narrowed a request mapping.

--- src/main/java/gov/nysenate/openleg/api/ui/PingCtrl.java
+++ src/main/java/gov/nysenate/openleg/api/ui/PingCtrl.java
@@ -2,13 +2,14 @@ package gov.nysenate.openleg.api.ui;
 
 import gov.nysenate.openleg.api.response.BaseResponse;
 import gov.nysenate.openleg.api.response.SimpleResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ping")
 public class PingCtrl {
-    @RequestMapping("")
+    @GetMapping("")
     public BaseResponse ping() {
         return new SimpleResponse(true, "OK", "ping");
     }
